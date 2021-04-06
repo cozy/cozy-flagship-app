30e8e7534f44ee4bd70321e1a6f8ed1b1997c352
doubleface <christophe@cozycloud.cc>
2021-04-06 12:31:21 +0200

    test: bypass harvest to run the launcher. This is temporary

--- src/screens/konnectors/Container.js
+++ src/screens/konnectors/Container.js
@@ -4,7 +4,17 @@ import HarvestView from './HarvestView'
 import LauncherView from './LauncherView'
 
 const Container = () => {
-  const [launcherContext, setLauncherContext] = useState(null)
+  // const [launcherContext, setLauncherContext] = useState(null)
+  const [launcherContext, setLauncherContext] = useState({
+    job: {
+      id: 'job test id',
+      trigger_id: 'trigger test id',
+      message: {
+        konnector: 'test',
+        account: 'account test id',
+      },
+    },
+  })
   return (
     <View style={styles.container}>
       {launcherContext ? (
