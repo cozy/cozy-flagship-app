c74f73693a682472fc6d8b2fede492a061135bc0
doubleface <christophe@cozycloud.cc>
2021-04-29 11:22:14 +0200

    fix: yarn token

--- scripts/token.js
+++ scripts/token.js
@@ -1,8 +1,11 @@
 const {execSync} = require('child_process')
-const {domain, folder} = require('../config.json')
+const {name, domain} = require('../config.json')
 const path = require('path')
 
 const finalPath = path.resolve(__dirname, '..', 'token.json')
 
-execSync(`make-token ${domain} io.cozy.files | xargs -I{} cp {} ${finalPath}`)
-console.log(`Generated token for ${domain} in ${finalPath}`)
+const cozy = name + '.' + domain
+const cmd = `make-token ${cozy} io.cozy.files | xargs -I{} cp {} ${finalPath}`
+console.log(cmd + '...')
+execSync(cmd)
+console.log(`  Generated token for ${cozy} in ${finalPath}`)
