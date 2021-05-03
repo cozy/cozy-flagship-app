58f646430c5a6e695e484b29d446986c416158a3
doubleface <christophe@cozycloud.cc>
2021-05-03 17:56:50 +0200

    feat: make a konnector token
    - to have konnector specific rights
    - to have a proper createdByApp in the files cozyMetadata, which allows proper prefiltering.

--- scripts/token.js
+++ scripts/token.js
@@ -1,11 +1,15 @@
 const {execSync} = require('child_process')
 const {name, domain} = require('../config.json')
 const path = require('path')
+const fs = require('fs')
 
 const finalPath = path.resolve(__dirname, '..', 'token.json')
 
 const cozy = name + '.' + domain
-const cmd = `make-token ${cozy} io.cozy.files | xargs -I{} cp {} ${finalPath}`
+const cmd = `make-token-konnector ${cozy} template | xargs -I{} cp {} ${finalPath}`
 console.log(cmd + '...')
 execSync(cmd)
+const tokenJSON = require(finalPath)
+tokenJSON.url = 'https://' + cozy
+fs.writeFileSync(finalPath, JSON.stringify(tokenJSON, null, 2))
 console.log(`  Generated token for ${cozy} in ${finalPath}`)
