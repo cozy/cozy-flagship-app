b40190b02a0ea967a44b502c55886bab27e442f7
doubleface <christophe@cozycloud.cc>
2021-06-04 09:17:27 +0200

    feat: share content script generation script accross connectors

--- connectors/scripts/generateWebViewContentScript.js
+++ connectors/scripts/generateWebViewContentScript.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 const path = require('path')
 
-const SOURCE_PATH = path.join(__dirname, '..', 'dist/main.js')
-const OUT_PATH = path.join(__dirname, '..', 'dist/webviewScript.js')
-const MANIFEST_PATH = path.join(__dirname, '..', 'manifest.konnector')
+const [BASE_PATH] = process.argv.slice(2)
+const SOURCE_PATH = path.join(BASE_PATH, 'dist/main.js')
+const OUT_PATH = path.join(BASE_PATH, 'dist/webviewScript.js')
+const MANIFEST_PATH = path.join(BASE_PATH, 'manifest.konnector')
 
 const js = fs.readFileSync(SOURCE_PATH, 'utf8')
 const manifest = fs.readFileSync(MANIFEST_PATH, 'utf8')
@@ -13,5 +14,3 @@ const json = `module.exports = {
 }`
 fs.writeFileSync(OUT_PATH, json)
 console.log('Generated ' + OUT_PATH)
-
-
