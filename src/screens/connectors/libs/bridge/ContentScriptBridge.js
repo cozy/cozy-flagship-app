10e4b984fb8ccda9a4ebf1d43f31bcbe66de871f
doubleface <christophe@cozycloud.cc>
2021-06-18 10:37:44 +0200

    refactor: bridgeHelps -> bridgeInterfaces

--- src/screens/connectors/libs/bridge/ContentScriptBridge.js
+++ src/screens/connectors/libs/bridge/ContentScriptBridge.js
@@ -1,5 +1,5 @@
 import {ParentHandshake} from 'post-me'
-import {Bridge} from '../../../../libs/bridgeHelpers'
+import {Bridge} from '../../../../libs/bridgeInterfaces'
 import ReactNativeLauncherMessenger from './ReactNativeLauncherMessenger'
 
 /**
