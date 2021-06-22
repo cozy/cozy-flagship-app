6c50fda8947c3396d81be04e053ff87066a86e0a
doubleface <christophe@cozycloud.cc>
2021-06-22 11:15:45 +0200

    refactor: extract LauncherInterface from Launcher

--- src/screens/connectors/libs/ReactNativeLauncher.js
+++ src/screens/connectors/libs/ReactNativeLauncher.js
@@ -4,41 +4,11 @@ import Minilog from '@cozy/minilog'
 import {Q} from 'cozy-client'
 import {saveFiles, saveBills, saveIdentity} from '../../../libs/connectorLibs'
 import {dataURItoArrayBuffer} from '../../../libs/utils'
+import LauncherInterface from '../../../libs/LauncherInterface'
 
 const log = Minilog('Launcher')
 
 Minilog.enable()
-
-/**
- * All launchers are supposed to implement this interface
- *
- * @interface
- */
-class LauncherInterface {
-  /**
-   * Inject the content script and initialize the bridge to communicate it
-   *
-   * @param  {Object} options.bridgeOptions : options which will be given as is to the bridge. Bridge options depend on the environment of the launcher
-   * @param  {String} options.contentScript : source code of the content script which will be injected
-   *
-   * @return {Bridge}
-   */
-  async init({bridgeOptions, contentScript}) {}
-
-  /**
-   * Start the connector execution
-   *
-   * @return {Bridge}
-   */
-  async start() {}
-
-  /**
-   * Get content script logs. This function is called by the content script via the bridge
-   *
-   * @param  {ContentScriptLogMessage} message : log message
-   */
-  log(message) {}
-}
 
 /**
  * This is the launcher implementation for a React native application
