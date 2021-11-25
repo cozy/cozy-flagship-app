3ad51c7834f668cc8105255caf235e9996b92b4a
Antonin Cezard <acezard@gmail.com>
2021-11-25 10:57:34 +0100

    feat: Improve handling

--- src/hooks/useAuthenticate.js
+++ src/hooks/useAuthenticate.js
@@ -13,7 +13,7 @@ export const useAuthenticate = (navigation, setClient) => {
         await setClient(client)
         navigation.navigate(strings.home)
       } catch (error) {
-        console.log(error)
+        setUri('')
       }
     }
 
