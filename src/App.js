032003618ca1d24f36024199cfe4db1d790e0a70
Antonin Cezard <acezard@gmail.com>
2021-11-25 10:47:12 +0100

    feat: Init mvp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react'
-import {SafeAreaView, StyleSheet, View, Linking} from 'react-native'
+import {SafeAreaView, StyleSheet} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
-import {Provider as PaperProvider, TextInput, Button} from 'react-native-paper'
+import {Provider as PaperProvider} from 'react-native-paper'
 
 import {decode, encode} from 'base-64'
 
@@ -11,7 +11,8 @@ import {CozyProvider} from 'cozy-client'
 import {lightTheme} from './theme'
 import Connectors from './screens/connectors'
 import StoreView from './screens/store/StoreView'
-import {getClient, saveClient, initClient} from './libs/client'
+import {getClient} from './libs/client'
+import {Authenticate} from './screens/Authenticate'
 
 const Root = createStackNavigator()
 
@@ -22,33 +23,6 @@ if (!global.btoa) {
 
 if (!global.atob) {
   global.atob = decode
-}
-
-const COZY_PREFIX = 'cozy://'
-
-const Authenticate = ({setClient}) => {
-  const [uri, setUri] = useState(null)
-  const [busy, setBusy] = useState(false)
-
-  return (
-    <View>
-      <TextInput
-        label="Cozy url"
-        placeholder="https://testchristophe.cozy.works"
-        onChange={(event) => setUri(event.nativeEvent.text)}
-      />
-      <Button
-        busy={busy}
-        onPress={async () => {
-          setBusy(true)
-          const clientResult = await callInitClient(uri)
-          await setClient(clientResult)
-          setBusy(false)
-        }}>
-        Submit
-      </Button>
-    </View>
-  )
 }
 
 const App = () => {
@@ -89,37 +63,6 @@ const App = () => {
       )}
     </PaperProvider>
   )
-}
-
-const callInitClient = async (uri) => {
-  const client = await initClient(uri, {
-    scope: [
-      'io.cozy.apps',
-      'io.cozy.settings',
-      'io.cozy.konnectors',
-      'io.cozy.jobs',
-      'io.cozy.contacts',
-      'io.cozy.triggers',
-      'io.cozy.permissions',
-      'io.cozy.apps.suggestions',
-      'com.bitwarden.organizations',
-      'com.bitwarden.ciphers',
-      'io.cozy.bank.accounts',
-      'io.cozy.timeseries.geojson',
-      'io.cozy.files.*',
-      'io.cozy.bills',
-      'io.cozy.accounts',
-      'io.cozy.identities',
-    ],
-    oauth: {
-      redirectURI: COZY_PREFIX,
-      softwareID: 'amiral',
-      clientKind: 'mobile',
-      clientName: 'Amiral',
-    },
-  })
-  await saveClient(client)
-  return client
 }
 
 const AppSafeView = () => (
