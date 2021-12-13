023aece53b5ccc55b03b0266df5e6d28b2b7ed39
Antonin Cezard <acezard@gmail.com>
2021-12-13 16:34:16 +0100

    feat: Install and configure react-native-bootsplash

--- android/app/src/main/java/com/cozyreactnative/MainActivity.java
+++ android/app/src/main/java/com/cozyreactnative/MainActivity.java
@@ -3,11 +3,8 @@ package com.cozyreactnative;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
-<<<<<<< HEAD
 import com.zoontek.rnbootsplash.RNBootSplash;
-=======
 import com.documentscanner.DocumentScannerPackage;
->>>>>>> feat: [wip] document scanner
 
 public class MainActivity extends ReactActivity {
 
