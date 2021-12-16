2099c6e92cb91f08da30621fa3e05ae363361d38
paultranvan <paul@cozycloud.cc>
2021-12-16 18:09:00 +0100

    feat: [wip] document scanner

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import {CozyAppView} from './screens/routes/CozyAppView'
 import {OCR} from './screens/scanner/OCR'
 import {useSplashScreen} from './hooks/useSplashScreen'
 import {SplashScreenProvider} from './screens/providers/SplashScreenProvider'
-import {OCR} from './screens/OCR'
+import {OCR} from './screens/scanner/OCR'
 
 const Root = createStackNavigator()
 const MainStack = createStackNavigator()
