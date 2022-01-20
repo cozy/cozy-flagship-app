6d83d6b0d0ca139bf7eb537e3174544480919765
Yannick Chiron <yannick.chiron@gmail.com>
2022-01-20 19:45:12 +0100

    feat: Open apps using their native mobile version when available
    The Home view can now open apps like Cozy Banks and Cozy Pass using
    their native mobile version
    
    Only apps that describe their mobile info in their `manifest.webapp`
    file can be opened using this mechanism
    
    Old version of those apps are ported using the local `slugFallbacks`
    constant

---
+++ src/libs/functions/openApp.js
@@ -0,0 +1,115 @@
+import {Alert, Linking, Platform} from 'react-native'
+
+/**
+ * App's mobile information. Used to describe the app scheme and its store urls
+ * @typedef {object} AppManifestMobileInfo
+ * @property {string} schema - The app's URL scheme
+ * @property {string} id_playstore - The app's id on Google PlayStore
+ * @property {string} id_appstore - The app's id on Apple AppStore
+ */
+
+/**
+ * App's description resulting of its manifest.webapp file
+ * @typedef {object} AppManifest
+ * @property {string} slug - The app's slug
+ * @property {AppManifestMobileInfo} mobile - The app's mobile information
+ */
+
+/**
+ * Apps mobile information that can be used as a fallback if the Cozy
+ * still has an old version of the app which doesn't describe its mobile
+ * info on the manifest.webapp
+ * 
+ * This object is used for retro-compatibility support and may be deleted
+ * in the future
+ * 
+ * If a new schema is inserted into this array or if a new app describe its
+ * schema in its manifest.webapp, then AndroidManifest.xml and Info.plist
+ * should be updated with the scheme permission
+ * 
+ * @type {Array.<AppManifestMobileInfo>}
+ */
+const slugFallbacks = {
+  passwords: {
+    schema: 'cozypass://',
+    id_playstore: 'io.cozy.pass',
+    id_appstore: 'cozy-pass/id1502262449',
+  },
+  banks: {
+    schema: 'cozybanks://',
+    id_playstore: 'io.cozy.banks.mobile',
+    id_appstore: 'cozy-banks/id1349814380',
+  },
+}
+
+/**
+ * Open the native mobile app using app scheme passed in arguments
+ * If app is not installed, redirect the user to the mobile store
+ * @param {AppManifestMobileInfo} appNativeData - The app's mobile information
+ * @returns {Promise}
+ */
+const openAppNative = async appNativeData => {
+  const schema = appNativeData.schema
+
+  const canOpenApp = await Linking.canOpenURL(schema)
+
+  if (canOpenApp) {
+    return Linking.openURL(schema)
+  } else {
+    return new Promise(resolve => {
+      const idPlayStore = appNativeData.id_playstore
+      const idAppStore = appNativeData.id_appstore
+
+      const storeUrl =
+        Platform.OS === 'ios'
+          ? `itms-apps://apps.apple.com/id/app/${idAppStore}?l=id`
+          : `https://play.google.com/store/apps/details?id=${idPlayStore}`
+
+      const storeName = Platform.OS === 'ios' ? 'AppStore' : 'PlayStore'
+
+      Alert.alert(
+        'Open Store', // TODO: Translate
+        `You will be redirected to the ${storeName}. Do you want to continue?`, // TODO: Translate
+        [
+          {
+            text: 'Cancel', // TODO: Translate
+            onPress: resolve,
+            style: 'cancel',
+          },
+          {
+            text: `Open the ${storeName}`, // TODO: Translate
+            onPress: () => Linking.openURL(storeUrl).then(resolve),
+          },
+        ],
+        {
+          cancelable: true,
+          onDismiss: resolve,
+        },
+      )
+    })
+  }
+}
+
+/**
+ * Open the native mobile app if the app has a mobile version
+ * Otherwise open the app on a webview using href
+ * @param {any} navigation - The React navigation context
+ * @param {string} href - The app web url
+ * @param {AppManifest} app - The app information
+ * @returns {Promise}
+ */
+export const openApp = (navigation, href, app) => {
+  if (app?.mobile) {
+    return openAppNative(app.mobile)
+  }
+
+  if (app?.slug) {
+    const slugFallback = slugFallbacks[app.slug]
+
+    if (slugFallback) {
+      return openAppNative(slugFallback)
+    }
+  }
+
+  return navigation.navigate('cozyapp', {href})
+}
---
+++ src/libs/functions/openApp.spec.js
@@ -0,0 +1,180 @@
+import RN from 'react-native'
+
+import {openApp} from './openApp'
+
+jest.mock('react-native', () => ({
+  Alert: {
+    alert: jest.fn(),
+  },
+  Linking: {
+    canOpenURL: jest.fn(),
+    openURL: jest.fn(),
+  },
+  Platform: {
+    OS: 'ios',
+  },
+}))
+
+const navigation = {
+  navigate: jest.fn(),
+}
+
+describe('openApp', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('with no app manifest', () => {
+    it('should open href', async () => {
+      await openApp(navigation, 'https://appurl')
+
+      expect(navigation.navigate).toHaveBeenCalledWith('cozyapp', {
+        href: 'https://appurl',
+      })
+    })
+  })
+
+  describe('with app manifest but no mobile info nor slug fallback', () => {
+    it('should open href', async () => {
+      await openApp(navigation, 'https://appurl', {
+        slug: 'some_app_with_no_native_equivalent',
+      })
+
+      expect(navigation.navigate).toHaveBeenCalledWith('cozyapp', {
+        href: 'https://appurl',
+      })
+    })
+  })
+
+  describe(`with app manifest and mobile info`, () => {
+    it('should open native app from manifest info', async () => {
+      RN.Linking.canOpenURL.mockResolvedValue(true)
+
+      await openApp(navigation, 'https://appurl', {
+        mobile: {
+          schema: 'cozypass://',
+          id_playstore: 'io.cozy.pass',
+          id_appstore: 'cozy-pass/id1502262449',
+        },
+      })
+
+      expect(RN.Linking.canOpenURL).toHaveBeenCalledWith('cozypass://')
+      expect(RN.Linking.openURL).toHaveBeenCalledWith('cozypass://')
+    })
+
+    it('should ask to open AppStore on iOS if native app is not installed', async () => {
+      RN.Linking.canOpenURL.mockResolvedValue(false)
+      RN.Linking.openURL.mockResolvedValue(false)
+      RN.Platform.OS = 'ios'
+
+      RN.Alert.alert = jest.fn((title, message, buttons) => {
+        buttons[1].onPress()
+      })
+
+      await openApp(navigation, 'https://appurl', {
+        mobile: {
+          schema: 'cozypass://',
+          id_playstore: 'io.cozy.pass',
+          id_appstore: 'cozy-pass/id1502262449',
+        },
+      })
+
+      expect(RN.Linking.canOpenURL).toHaveBeenCalledWith('cozypass://')
+      expect(RN.Linking.openURL).toHaveBeenCalledWith(
+        'itms-apps://apps.apple.com/id/app/cozy-pass/id1502262449?l=id',
+      )
+    })
+
+    it('should ask to open PlayStore on Android if native app is not installed', async () => {
+      RN.Linking.canOpenURL.mockResolvedValue(false)
+      RN.Linking.openURL.mockResolvedValue(false)
+      RN.Platform.OS = 'android'
+
+      RN.Alert.alert = jest.fn((title, message, buttons) => {
+        buttons[1].onPress()
+      })
+
+      await openApp(navigation, 'https://appurl', {
+        mobile: {
+          schema: 'cozypass://',
+          id_playstore: 'io.cozy.pass',
+          id_appstore: 'cozy-pass/id1502262449',
+        },
+      })
+
+      expect(RN.Linking.canOpenURL).toHaveBeenCalledWith('cozypass://')
+      expect(RN.Linking.openURL).toHaveBeenCalledWith(
+        'https://play.google.com/store/apps/details?id=io.cozy.pass',
+      )
+    })
+  })
+
+  describe(`with app manifest and slug with fallback`, () => {
+    it('should open native app from fallbacks info', async () => {
+      RN.Linking.canOpenURL.mockResolvedValue(true)
+
+      await openApp(navigation, 'https://appurl', {
+        slug: 'passwords',
+      })
+
+      expect(RN.Linking.canOpenURL).toHaveBeenCalledWith('cozypass://')
+      expect(RN.Linking.openURL).toHaveBeenCalledWith('cozypass://')
+    })
+
+    it('should ask to open AppStore on iOS if native app is not installed', async () => {
+      RN.Linking.canOpenURL.mockResolvedValue(false)
+      RN.Linking.openURL.mockResolvedValue(false)
+      RN.Platform.OS = 'ios'
+
+      RN.Alert.alert = jest.fn((title, message, buttons) => {
+        buttons[1].onPress()
+      })
+
+      await openApp(navigation, 'https://appurl', {
+        slug: 'passwords',
+      })
+
+      expect(RN.Linking.canOpenURL).toHaveBeenCalledWith('cozypass://')
+      expect(RN.Linking.openURL).toHaveBeenCalledWith(
+        'itms-apps://apps.apple.com/id/app/cozy-pass/id1502262449?l=id',
+      )
+    })
+
+    it('should ask to open PlayStore on Android if native app is not installed', async () => {
+      RN.Linking.canOpenURL.mockResolvedValue(false)
+      RN.Linking.openURL.mockResolvedValue(false)
+      RN.Platform.OS = 'android'
+
+      RN.Alert.alert = jest.fn((title, message, buttons) => {
+        buttons[1].onPress()
+      })
+
+      await openApp(navigation, 'https://appurl', {
+        slug: 'passwords',
+      })
+
+      expect(RN.Linking.canOpenURL).toHaveBeenCalledWith('cozypass://')
+      expect(RN.Linking.openURL).toHaveBeenCalledWith(
+        'https://play.google.com/store/apps/details?id=io.cozy.pass',
+      )
+    })
+  })
+
+  describe(`with app manifest and mobile info and slug fallback`, () => {
+    it('should prioritize mobile info from manifest over fallback', async () => {
+      RN.Linking.canOpenURL.mockResolvedValue(true)
+
+      await openApp(navigation, 'https://appurl', {
+        slug: 'passwords',
+        mobile: {
+          schema: 'cozypassoverride://',
+          id_playstore: 'io.cozy.pass.override',
+          id_appstore: 'cozy-pass-override/id1502262449',
+        },
+      })
+
+      expect(RN.Linking.canOpenURL).toHaveBeenCalledWith('cozypassoverride://')
+      expect(RN.Linking.openURL).toHaveBeenCalledWith('cozypassoverride://')
+    })
+  })
+})
