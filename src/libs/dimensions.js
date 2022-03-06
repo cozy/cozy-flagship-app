30b850714ef680111ae33f148fe0acde39a1bdfd
Quentin Valmori <quentin.valmori@gmail.com>
2022-03-06 10:01:14 +0100

    fix: StatusBar.currentHeight can return undefined

--- src/libs/dimensions.js
+++ src/libs/dimensions.js
@@ -3,6 +3,6 @@ import {Dimensions, StatusBar} from 'react-native'
 const screenHeight = Dimensions.get('screen').height
 const windowHeight = Dimensions.get('window').height
 const navbarHeight = screenHeight - windowHeight
-const statusBarHeight = StatusBar.currentHeight
+const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight : 0
 
 export {navbarHeight, statusBarHeight}
