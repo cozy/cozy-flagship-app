52e93c83e03a6aaf403407d579e4164e94861b01
Yannick Chiron <yannick.chiron@gmail.com>
2022-03-18 18:00:01 +0100

    refactor: Move `doHashPasswords()` methods in `passwordHelper.js`

--- src/screens/login/components/PasswordView.js
+++ src/screens/login/components/PasswordView.js
@@ -1,58 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {Button, View, Text, TextInput} from 'react-native'
-import Minilog from '@cozy/minilog'
 
-import {queryResultToCrypto} from '../../../components/webviews/CryptoWebView/cryptoObservable/cryptoObservable'
-
-const log = Minilog('PasswordView')
-
-const ITERATION_NUMBER = 100_000
-
-const getSaltForInstance = instance => {
-  const domain = instance.split(':')[0]
-  const salt = `me@${domain}`
-
-  return salt
-}
-
-/**
- * Hash password data by calling CryptioWebView cryptography methods
- * @param {PasswordData} passwordData - the password data to hash
- * @param {string} instance - the Cozy instance used to generate the salt
- * @param {number} kdfIterations - the number of KDF iterations to be used for hashing the password
- * @returns {LoginData} login data containing hashed password and encryption keys
- */
-const doHashPassword = async (passwordData, fqdn, kdfIterations) => {
-  log.debug('Start hashing password')
-  try {
-    const {password} = passwordData
-
-    const salt = getSaltForInstance(fqdn)
-
-    const result = await queryResultToCrypto('computePass', {
-      pass: password,
-      salt: salt,
-      iterations: kdfIterations,
-    })
-
-    const {passwordHash, iterations, key, publicKey, privateKey, masterKey} =
-      result.param
-
-    const loginData = {
-      passwordHash,
-      iterations,
-      key,
-      publicKey,
-      privateKey,
-      masterKey,
-    }
-
-    return loginData
-  } catch (e) {
-    log.error('Error while requesting cryptography result:', e)
-    throw e
-  }
-}
+import {doHashPassword} from '../../../libs/functions/passwordHelpers'
 
 /**
  * Show a password form that asks the user their password
