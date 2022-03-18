0fd630fb7e4fe2839a159bf8d03aaa70b7f51657
Yannick Chiron <yannick.chiron@gmail.com>
2022-03-18 18:01:01 +0100

    feat: Add ErrorView to display errors in login and onboarding process
    This component displays an error message and can show/hide the json
    stringified error
    
    This component is rudimentary and should be improved later

---
+++ src/screens/login/components/ErrorView.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import {Button, View, Text} from 'react-native'
+
+/**
+ * Display an error to the user
+ *
+ * @param {object} props
+ * @param {setInstanceData} props.errorMessage - An error message that should be displayed to the user
+ * @param {Error} props.error - The error object
+ * @param {ButtonInfo} props.button - The action button to be displayed to the user (ex: to leave the error screen)
+ * @returns {import('react').ComponentClass}
+ */
+export const ErrorView = ({errorMessage, error, button}) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  return (
+    <View>
+      <Text>AN ERROR OCCURED</Text>
+      <Text>{errorMessage}</Text>
+
+      {error && (
+        <View style={{marginTop: 20}}>
+          <Button onPress={toggleDetails} title="Show details" />
+          {showDetails && <Text>{JSON.stringify(error)}</Text>}
+        </View>
+      )}
+
+      <View style={{marginTop: 20}}>
+        <Button onPress={button.callback} title={button.title} />
+      </View>
+    </View>
+  )
+}
