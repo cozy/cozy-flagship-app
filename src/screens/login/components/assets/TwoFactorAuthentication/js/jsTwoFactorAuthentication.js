08f779356911d5e6831ffdc93b54623e81f07d96
Yannick Chiron <yannick.chiron@gmail.com>
2022-04-01 17:56:20 +0200

    feat: Implement login's TwoFactorAuthenticationView UI
    This commit implements UI for the `TwoFactorAuthenticationView`
    component
    
    The UI is implemented inside of a webview using HTML/Javascript code
    
    This technical choice allows us to retrieve the same password form UI
    from `cozy-stack` and will allow us to support CSS overload from
    `cozy-stack`
    
    For now the HTML/Javascript has been copy-pasted inside of JS variables
    and has been manually modified to support sending messages to the
    native code
    
    In the future we expect to be able to use the exact same
    HTML/Javascript code on both platforms (`react-native` and
    `cozy-stack`)

---
+++ src/screens/login/components/assets/TwoFactorAuthentication/js/jsTwoFactorAuthentication.js
@@ -0,0 +1,38 @@
+/*******************************************************************/
+/* This code should reflect cozy-stack/assets/scripts/twofactor.js */
+/*******************************************************************/
+
+export const twoFactorAuthenticationJs = `
+function postMessage (message) {
+  if (window.ReactNativeWebView.postMessage === undefined) {
+    setTimeout(postMessage, 200, message)
+  } else {
+    window.ReactNativeWebView.postMessage(message)
+  }
+}
+
+;(function (w, d) {
+  if (!w.fetch || !w.Headers) return
+
+  const twofaForm = d.getElementById('two-factor-form')
+  const twofaField = d.getElementById('two-factor-field')
+  const submitButton = d.getElementById('two-factor-submit')
+  const passcodeInput = d.getElementById('two-factor-passcode')
+
+  const onSubmitTwoFactorCode = function (event) {
+    event.preventDefault()
+
+    passcodeInput.setAttribute('disabled', true)
+    submitButton.setAttribute('disabled', true)
+
+    const passcode = passcodeInput.value
+
+    postMessage(JSON.stringify({
+      message: 'setTwoFactorAuthenticationCode',
+      twoFactorAuthenticationCode: passcodeInput.value
+    }))
+  }
+
+  twofaForm.addEventListener('submit', onSubmitTwoFactorCode)
+})(window, document)
+`
