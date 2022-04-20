17faebca1a643e3876551831186ec57de8ec9fa4
Antonin Cezard <acezard@gmail.com>
2022-04-20 14:15:05 +0200

    chore: Update log forwarder
    It takes into account new cozy-intent API when detecting messages
    Also added full testing coverage

---
+++ src/components/webviews/jsInteractions/jsLogInterception.spec.js
@@ -0,0 +1,86 @@
+/* eslint-disable no-console */
+import { tryConsole } from './jsLogInterception'
+
+const logId = 'logId'
+const logger = {
+  log: jest.fn(),
+  debug: jest.fn()
+}
+const makePayload = ({ type, args }) => ({
+  nativeEvent: {
+    data: JSON.stringify({
+      type: 'Console',
+      data: { type, log: ['one', 'two', 'three', ...(args || [])] }
+    })
+  }
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+it('handles default case', () => {
+  const payload = { ...makePayload({ type: 'log' }) }
+
+  tryConsole(payload, logger, logId)
+
+  expect(logger.log).toHaveBeenNthCalledWith(
+    1,
+    '[Console logId]',
+    'one',
+    'two',
+    'three'
+  )
+})
+
+it('handles default case with another type', () => {
+  const payload = { ...makePayload({ type: 'debug' }) }
+
+  tryConsole(payload, logger, logId)
+
+  expect(logger.debug).toHaveBeenNthCalledWith(
+    1,
+    '[Console logId]',
+    'one',
+    'two',
+    'three'
+  )
+})
+
+it('handles post-me case', () => {
+  console.debug = jest.fn()
+
+  const payload = { ...makePayload({ type: 'debug', args: ['@post-me'] }) }
+
+  tryConsole(payload, logger, logId)
+
+  expect(console.debug).toHaveBeenNthCalledWith(
+    1,
+    'one',
+    'two',
+    'three',
+    '@post-me'
+  )
+})
+
+it('recovers when erroring', () => {
+  console.error = jest.fn()
+
+  tryConsole()
+
+  expect(console.error).toHaveBeenCalledTimes(1)
+})
+
+it('ignores other events', () => {
+  console.error = jest.fn()
+
+  const payload = {
+    nativeEvent: { data: JSON.stringify({ foo: 'bar', type: 'baz' }) }
+  }
+
+  tryConsole(payload, logger, logId)
+
+  expect(console.error).not.toHaveBeenCalled()
+  expect(logger.log).not.toHaveBeenCalled()
+  expect(logger.debug).not.toHaveBeenCalled()
+})
