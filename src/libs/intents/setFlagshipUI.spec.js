d4767cfd8c3f2f387515257c4cbd330430b7eb7f
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-04-20 18:32:36 +0200

    test: Refactor tests where mocks did not follow Jest convention
    A mock should rather not call another mock if possible

--- src/libs/intents/setFlagshipUI.spec.js
+++ src/libs/intents/setFlagshipUI.spec.js
@@ -1,99 +1,108 @@
 import { flagshipUI, setFlagshipUI } from './setFlagshipUI'
+import { changeBarColors } from 'react-native-immersive-bars'
 
-const mockOnChange = jest.fn()
-const mockChangeBarColors = jest.fn()
+jest.mock('react-native-immersive-bars')
 
-jest.mock('react-native-immersive-bars', () => ({
-  changeBarColors: jest.fn().mockImplementation(v => mockChangeBarColors(v))
-}))
-
-it('should parse topTheme light', () => {
-  flagshipUI.on('change', mockOnChange)
-
-  setFlagshipUI({
-    topTheme: 'light'
+describe('setFlagshipUI', () => {
+  let mockOnChange
+  beforeEach(() => {
+    mockOnChange = jest.fn()
   })
 
-  expect(mockOnChange).toHaveBeenNthCalledWith(1, { topTheme: 'light-content' })
-  expect(mockChangeBarColors).not.toHaveBeenCalled()
-})
+  it('should parse topTheme light', () => {
+    flagshipUI.on('change', mockOnChange)
 
-it('should parse topTheme dark', () => {
-  flagshipUI.on('change', mockOnChange)
+    setFlagshipUI({
+      topTheme: 'light'
+    })
 
-  setFlagshipUI({
-    topTheme: 'dark'
+    expect(mockOnChange).toHaveBeenNthCalledWith(1, {
+      topTheme: 'light-content'
+    })
+    expect(changeBarColors).not.toHaveBeenCalled()
   })
 
-  expect(mockOnChange).toHaveBeenNthCalledWith(1, { topTheme: 'dark-content' })
-  expect(mockChangeBarColors).not.toHaveBeenCalled()
-})
+  it('should parse topTheme dark', () => {
+    flagshipUI.on('change', mockOnChange)
 
-it('should parse incomplete object', () => {
-  flagshipUI.on('change', mockOnChange)
+    setFlagshipUI({
+      topTheme: 'dark'
+    })
 
-  setFlagshipUI({
-    topTheme: undefined,
-    topBackground: '#fff'
+    expect(mockOnChange).toHaveBeenNthCalledWith(1, {
+      topTheme: 'dark-content'
+    })
+    expect(changeBarColors).not.toHaveBeenCalled()
   })
 
-  expect(mockOnChange).toHaveBeenNthCalledWith(1, { topBackground: '#fff' })
-  expect(mockChangeBarColors).not.toHaveBeenCalled()
-})
+  it('should parse incomplete object', () => {
+    flagshipUI.on('change', mockOnChange)
 
-it('should parse padded values', () => {
-  flagshipUI.on('change', mockOnChange)
+    setFlagshipUI({
+      topTheme: undefined,
+      topBackground: '#fff'
+    })
 
-  setFlagshipUI({
-    bottomBackground: '    #fff    '
+    expect(mockOnChange).toHaveBeenNthCalledWith(1, { topBackground: '#fff' })
+    expect(changeBarColors).not.toHaveBeenCalled()
   })
 
-  expect(mockOnChange).toHaveBeenNthCalledWith(1, { bottomBackground: '#fff' })
-  expect(mockChangeBarColors).not.toHaveBeenCalled()
-})
+  it('should parse padded values', () => {
+    flagshipUI.on('change', mockOnChange)
 
-it('should parse full objects with bottomTheme special case dark', () => {
-  flagshipUI.on('change', mockOnChange)
+    setFlagshipUI({
+      bottomBackground: '    #fff    '
+    })
 
-  setFlagshipUI({
-    bottomBackground: 'white',
-    bottomTheme: 'dark',
-    bottomOverlay: 'transparent',
-    topBackground: 'white',
-    topTheme: 'dark',
-    topOverlay: 'transparent'
+    expect(mockOnChange).toHaveBeenNthCalledWith(1, {
+      bottomBackground: '#fff'
+    })
+    expect(changeBarColors).not.toHaveBeenCalled()
   })
 
-  expect(mockOnChange).toHaveBeenNthCalledWith(1, {
-    bottomBackground: 'white',
-    bottomOverlay: 'transparent',
-    topBackground: 'white',
-    topOverlay: 'transparent',
-    topTheme: 'dark-content'
+  it('should parse full objects with bottomTheme special case dark', () => {
+    flagshipUI.on('change', mockOnChange)
+
+    setFlagshipUI({
+      bottomBackground: 'white',
+      bottomTheme: 'dark',
+      bottomOverlay: 'transparent',
+      topBackground: 'white',
+      topTheme: 'dark',
+      topOverlay: 'transparent'
+    })
+
+    expect(mockOnChange).toHaveBeenNthCalledWith(1, {
+      bottomBackground: 'white',
+      bottomOverlay: 'transparent',
+      topBackground: 'white',
+      topOverlay: 'transparent',
+      topTheme: 'dark-content'
+    })
+
+    expect(changeBarColors).toHaveBeenNthCalledWith(1, false)
   })
 
-  expect(mockChangeBarColors).toHaveBeenNthCalledWith(1, false)
-})
+  it('should emit parse full objects with bottomTheme special case light', () => {
+    flagshipUI.on('change', mockOnChange)
 
-it('should emit parse full objects with bottomTheme special case light', () => {
-  flagshipUI.on('change', mockOnChange)
+    setFlagshipUI({
+      bottomBackground: 'white',
+      bottomTheme: 'light',
+      bottomOverlay: 'transparent',
+      topBackground: 'white',
+      topTheme: 'dark',
+      topOverlay: 'transparent'
+    })
 
-  setFlagshipUI({
-    bottomBackground: 'white',
-    bottomTheme: 'light',
-    bottomOverlay: 'transparent',
-    topBackground: 'white',
-    topTheme: 'dark',
-    topOverlay: 'transparent'
+    expect(mockOnChange).toHaveBeenNthCalledWith(1, {
+      bottomBackground: 'white',
+      bottomOverlay: 'transparent',
+      topBackground: 'white',
+      topOverlay: 'transparent',
+      topTheme: 'dark-content'
+    })
+
+    expect(changeBarColors).toHaveBeenNthCalledWith(1, true)
   })
-
-  expect(mockOnChange).toHaveBeenNthCalledWith(1, {
-    bottomBackground: 'white',
-    bottomOverlay: 'transparent',
-    topBackground: 'white',
-    topOverlay: 'transparent',
-    topTheme: 'dark-content'
-  })
-
-  expect(mockChangeBarColors).toHaveBeenNthCalledWith(1, true)
 })
