d2d76d81a7446b4df818e038e37c49c058fd7e94
Crash-- <quentin.valmori@gmail.com>
2022-04-27 10:22:51 +0200

    fix: Disable zoom
    There is no props to disable zoom. So let's add the meta attribute
    to the HTML to tell the webview to not allow the scroll

--- src/components/webviews/jsInteractions/jsCozyInjection.js
+++ src/components/webviews/jsInteractions/jsCozyInjection.js
@@ -28,5 +28,10 @@ export const jsCozyGlobal = routeName => `
       'flagship-os-${Platform.OS}',
       'flagship-route-${routeName}'
     )
+    // make the webapp non-zoomable
+    var meta = document.createElement('meta')
+    meta.setAttribute('name', 'viewport')
+    meta.setAttribute('content', 'width = device-width, initial-scale = 1.0, minimum-scale = 1.0, maximum-scale = 1.0, user-scalable = no')
+    document.getElementsByTagName('head')[0].appendChild(meta)
   })
 `
