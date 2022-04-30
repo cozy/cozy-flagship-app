28f89ac883dfee2fce7e1ea97a0bb68413aa10e4
Yannick Chiron <yannick.chiron@gmail.com>
2022-04-30 20:10:25 +0200

    refactor: Remove unused code from `HttpServer`
    In previous commit we retrieved HttpServer.js from
    react-native-static-server project
    
    Our usage of HttpServer is simpler than what is authorized by its
    constructor
    
    So to improve code readability and maintainability we chose to simplify
    this code

--- src/libs/httpserver/HttpServer.js
+++ src/libs/httpserver/HttpServer.js
@@ -8,49 +8,13 @@ const { HttpServer: NativeHttpServer } = NativeModules;
 
 const PORT = '';
 const ROOT = null;
-const LOCALHOST = 'http://127.0.0.1:';
 
 class HttpServer {
 	constructor(port, root, opts) {
-		switch (arguments.length) {
-			case 3:
-				this.port = `${port}` || PORT;
-				this.root = root || ROOT;
-				this.localOnly = (opts && opts.localOnly) || false;
-				this.keepAlive = (opts && opts.keepAlive) || false;
-				break;
-			case 2:
-				this.port = `${port}`;
-				if (typeof(arguments[1]) === 'string') {
-					this.root = root;
-					this.localOnly = false;
-					this.keepAlive = false;
-				} else {
-					this.root = ROOT;
-					this.localOnly = (arguments[1] && arguments[1].localOnly) || false;
-					this.keepAlive = (arguments[1] && arguments[1].keepAlive) || false;
-				}
-				break;
-			case 1:
-				if (typeof(arguments[0]) === 'number') {
-					this.port = `${port}`;
-					this.root = ROOT;
-					this.localOnly = false;
-					this.keepAlive = false;
-				} else {
-					this.port = PORT;
-					this.root = ROOT;
-					this.localOnly = (arguments[0] && arguments[0].localOnly) || false;
-					this.keepAlive = (arguments[0] && arguments[0].keepAlive) || false;
-				}
-				break;
-			default:
-				this.port = PORT;
-				this.root = ROOT;
-				this.localOnly = false;
-				this.keepAlive = false;
-		}
-
+		this.port = `${port}` || PORT;
+		this.root = root || ROOT;
+		this.localOnly = (opts && opts.localOnly) || false;
+		this.keepAlive = (opts && opts.keepAlive) || false;
 
 		this.started = false;
 		this._origin = undefined;
