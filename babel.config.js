3634b879d8f16664bbaecb3b81417d662ef7f723
Antonin Cezard <acezard@gmail.com>
2022-05-03 08:41:48 +0200

    build: Add root alias for src directories
    Also add it to jsconfig for Electron based IDE.
    Can be used later for TSConfig if needed.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['optional-require'],
+  plugins: [
+    'optional-require',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '^/(.+)': './src/\\1'
+        },
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.json',
+          '.tsx',
+          '.ts',
+          '.native.js'
+        ]
+      }
+    ]
+  ]
 }
