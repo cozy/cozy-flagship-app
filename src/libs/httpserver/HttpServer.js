6122e94857511674dd5b79a1ab5c197eb64bd3e6
Yannick Chiron <yannick.chiron@gmail.com>
2022-05-04 16:23:43 +0200

    chore: Fix lint on `HttpServer.js`
    In previous commit we retrieved HttpServer.js from
    react-native-static-server project
    
    This code did not fit our lint rules, so this commit fixes the lint
    errors

--- src/libs/httpserver/HttpServer.js
+++ src/libs/httpserver/HttpServer.js
@@ -1,89 +1,87 @@
-import {
-	NativeModules,
-	AppState,
-	Platform
- } from 'react-native';
+import { NativeModules, AppState, Platform } from 'react-native'
 
-const { HttpServer: NativeHttpServer } = NativeModules;
+const { HttpServer: NativeHttpServer } = NativeModules
 
-const PORT = '';
-const ROOT = null;
+const PORT = ''
+const ROOT = null
 
 class HttpServer {
-	constructor(port, root, opts) {
-		this.port = `${port}` || PORT;
-		this.root = root || ROOT;
-		this.localOnly = (opts && opts.localOnly) || false;
-		this.keepAlive = (opts && opts.keepAlive) || false;
+  constructor(port, root, opts) {
+    this.port = `${port}` || PORT
+    this.root = root || ROOT
+    this.localOnly = (opts && opts.localOnly) || false
+    this.keepAlive = (opts && opts.keepAlive) || false
 
-		this.started = false;
-		this._origin = undefined;
-		this._handleAppStateChangeFn = this._handleAppStateChange.bind(this);
-	}
+    this.started = false
+    this._origin = undefined
+    this._handleAppStateChangeFn = this._handleAppStateChange.bind(this)
+  }
 
-	start() {
-		if( this.running ){
-			return Promise.resolve(this.origin);
-		}
+  start() {
+    if (this.running) {
+      return Promise.resolve(this.origin)
+    }
 
-		this.started = true;
-		this.running = true;
+    this.started = true
+    this.running = true
 
-		if (!this.keepAlive && (Platform.OS === 'android')) {
-			AppState.addEventListener('change', this._handleAppStateChangeFn);
-		}
+    if (!this.keepAlive && Platform.OS === 'android') {
+      AppState.addEventListener('change', this._handleAppStateChangeFn)
+    }
 
-		return NativeHttpServer.start(this.port, this.root, this.localOnly, this.keepAlive)
-			.then((origin) => {
-				this._origin = origin;
-				return origin;
-			});
-	}
+    return NativeHttpServer.start(
+      this.port,
+      this.root,
+      this.localOnly,
+      this.keepAlive
+    ).then(origin => {
+      this._origin = origin
+      return origin
+    })
+  }
 
-	stop() {
-		this.running = false;
+  stop() {
+    this.running = false
 
-		return NativeHttpServer.stop();
-	}
+    return NativeHttpServer.stop()
+  }
 
-	kill() {
-		this.stop();
-		this.started = false;
-		this._origin = undefined;
-		AppState.removeEventListener('change', this._handleAppStateChangeFn);
-	}
+  kill() {
+    this.stop()
+    this.started = false
+    this._origin = undefined
+    AppState.removeEventListener('change', this._handleAppStateChangeFn)
+  }
 
-	_handleAppStateChange(appState) {
-		if (!this.started) {
-			return;
-		}
+  _handleAppStateChange(appState) {
+    if (!this.started) {
+      return
+    }
 
-		if (appState === "active" && !this.running) {
-			this.start();
-		}
+    if (appState === 'active' && !this.running) {
+      this.start()
+    }
 
-		if (appState === "background" && this.running) {
-			this.stop();
-		}
+    if (appState === 'background' && this.running) {
+      this.stop()
+    }
 
-		if (appState === "inactive" && this.running) {
-			this.stop();
-		}
-	}
+    if (appState === 'inactive' && this.running) {
+      this.stop()
+    }
+  }
 
-	get origin() {
-		return this._origin;
-	}
+  get origin() {
+    return this._origin
+  }
 
-	isRunning() {
-		return NativeHttpServer.isRunning()
-			.then(running => {
-				this.running = running;
+  isRunning() {
+    return NativeHttpServer.isRunning().then(running => {
+      this.running = running
 
-				return this.running;
-			})
-	}
-
+      return this.running
+    })
+  }
 }
 
-export default HttpServer;
+export default HttpServer
