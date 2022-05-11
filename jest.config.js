299a50bfb8d26edfdbe57c6110579611f2421a63
Antonin Cezard <acezard@gmail.com>
2022-05-11 08:29:37 +0200

    test: Improve jest config
    Added jsx support to Jest with babel-jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 const config = {
   clearMocks: true,
-  setupFiles: ['<rootDir>/__tests__/jestSetupFile.js'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   preset: 'react-native',
+  setupFiles: ['<rootDir>/__tests__/jestSetupFile.js'],
+  transform: { '^.+\\.jsx$': 'babel-jest' },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?)/)'
-  ],
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}']
+  ]
 }
 
 module.exports = config
