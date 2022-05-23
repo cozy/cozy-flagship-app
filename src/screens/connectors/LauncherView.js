cozy/cozy-flagship-app

commit ab87de2453f8fc60ee5e4f881c023063fbce6093

feat(CCC): Overload previous worker state
Instead of crushing it with setWorkerState.

This will allow functions like ContentScript.goto to work without changing the visibility of the worker for example

src/screens/connectors/LauncherView.js
@@ -84,7 +84,7 @@ class LauncherView extends Component {
     const initConnectorError = await this.initConnector()
 
     this.launcher.on('SET_WORKER_STATE', options => {
-      this.setState({ worker: options })
+      this.setState({ worker: { ...this.state.worker, options } })
     })
 
     this.launcher.on('SET_USER_AGENT', userAgent => {

commit 20b77fbc7a9398ae486e4d664fa2c6c3b3ef2eac

fix(CCC): Regression after setWorkerState overload
Regression introduced in
https://github.com/cozy/cozy-react-native/commit/ab87de2453f8fc60ee5e4f881c023063fbce6093

src/screens/connectors/LauncherView.js
@@ -84,7 +84,7 @@ class LauncherView extends Component {
     const initConnectorError = await this.initConnector()
 
     this.launcher.on('SET_WORKER_STATE', options => {
-      this.setState({ worker: { ...this.state.worker, options } })
+      this.setState({ worker: { ...this.state.worker, ...options } })
     })
 
     this.launcher.on('SET_USER_AGENT', userAgent => {