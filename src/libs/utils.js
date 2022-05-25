531790c101a916c9d09f31bad98c6aa959170436
Antonin Cezard <acezard@gmail.com>
2022-05-25 10:00:29 +0200

    feat: Allow on demand Sentry logging
    This PR will allow a manual toggle to send Logs to sentry even in dev.
    It is disabled by default and for now can only be activated by modifying
    L26 of EnvService to `if (true)`
    
    Also added same functionality for LogBox

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -22,13 +22,3 @@ export const dataURItoArrayBuffer = dataURI => {
   }
   return { contentType, arrayBuffer }
 }
-
-export const isDevMode = () => {
-  try {
-    return !!__DEV__
-  } catch {
-    return false
-  }
-}
-
-export const isBuildMode = () => !isDevMode()
