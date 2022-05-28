ce5b7a5f29c301815d50f88481a42a7606ba4d4b
Yannick Chiron <yannick.chiron@gmail.com>
2022-05-28 16:03:29 +0200

    feat: Skip authorization step when possible
    The cozy-stack implements a new `auth/login/flagship` endpoint that
    allows to obtain OAuth access and register tokens without having to
    make the OAuth dance
    
    This allows to login the app without having to call the inAppBrowser
    with Authorize url
    
    This new scenario is more natural for the user as no extra
    authorisation step is required
    
    However, if the app fails to register as a `flagship` app, then the old
    scenario is still executed and the user will have to accept
    Authorizations from the inAppBrowser view
    
    The new `loginFlagship` method replaces the old `fetchSessionCode`
    method which is removed
    
    Related PR: cozy/cozy-stack#3400
    Related PR: cozy/cozy-client#1181

---
+++ src/libs/clientHelpers/loginFlagship.js
@@ -0,0 +1,47 @@
+/**
+ * Try to login cozy-client using Flagship dedicated route
+ *
+ * Errors are handled to detect when 2FA is needed and when password is invalid
+ *
+ * @param {object} param
+ * @param {object} param.client - the CozyClient instance that will be authenticated through OAuth
+ * @param {object} param.loginData - login data containing hashed password
+ * @param {object} [param.twoFactorAuthenticationData] - the 2FA data containing a token and a passcode
+ * @returns {SessionCodeResult} The query result with session_code, or 2FA token, or invalid password error
+ * @throws
+ */
+export const loginFlagship = async ({
+  client,
+  loginData,
+  twoFactorAuthenticationData = undefined
+}) => {
+  const stackClient = client.getStackClient()
+
+  try {
+    const loginResult = await stackClient.loginFlagship({
+      passwordHash: loginData.passwordHash,
+      twoFactorToken: twoFactorAuthenticationData
+        ? twoFactorAuthenticationData.token
+        : undefined,
+      twoFactorPasscode: twoFactorAuthenticationData
+        ? twoFactorAuthenticationData.passcode
+        : undefined
+    })
+
+    return loginResult
+  } catch (e) {
+    if (e.status === 401) {
+      if (twoFactorAuthenticationData) {
+        return {
+          two_factor_token: twoFactorAuthenticationData.token
+        }
+      } else {
+        return {
+          invalidPassword: true
+        }
+      }
+    } else {
+      throw e
+    }
+  }
+}
