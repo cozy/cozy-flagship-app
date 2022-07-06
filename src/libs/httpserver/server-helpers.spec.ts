ebb6396259fc843196aa1aae88dd5013348990a4
Antonin Cezard <acezard@gmail.com>
2022-07-06 15:43:14 +0200

    feat: Enable Typescript support
    Enable Typescript support as per official documentation.
    
    Also converted a js file to a ts file to provide an example.

--- src/libs/httpserver/server-helpers.spec.ts
+++ src/libs/httpserver/server-helpers.spec.ts
@@ -2,9 +2,9 @@ import { compose } from '/libs/functions/compose'
 import { statusBarHeight, getNavbarHeight } from '/libs/dimensions'
 import {
   addBodyClasses,
-  addCSS,
+  addBarStyles,
   addMetaAttributes
-} from '/libs/httpserver/helpers'
+} from './server-helpers'
 
 jest.mock('/libs/RootNavigation.js', () => ({
   navigationRef: {
@@ -16,12 +16,19 @@ it('should return a stringified HTML with added CSS', () => {
   const html = `<html><head></head><body></body></html>`
   const expected = `<html><head><style>body {--flagship-top-height: ${statusBarHeight}px; --flagship-bottom-height: ${getNavbarHeight()}px;}</style></head><body></body></html>`
 
-  expect(addCSS(html)).toEqual(expected)
+  expect(addBarStyles(html)).toEqual(expected)
 })
 
 it('should return a stringified HTML with added body classes', () => {
   const html = `<html><head></head><body></body></html>`
   const expected = `<html><head></head><body class="flagship-app flagship-os-ios flagship-route-home"></body></html>`
+
+  expect(addBodyClasses(html)).toEqual(expected)
+})
+
+it('should return a stringified HTML with appended body classes', () => {
+  const html = `<html class="it"><head class="works"></head><body id="baz" class="foo bar" data-class="test"></body></html>`
+  const expected = `<html class="it"><head class="works"></head><body id="baz" class="foo bar flagship-app flagship-os-ios flagship-route-home" data-class="test"></body></html>`
 
   expect(addBodyClasses(html)).toEqual(expected)
 })
@@ -37,7 +44,7 @@ it('should be able to handle composition', () => {
   const html = `<html><head></head><body></body></html>`
   const expected = `<html><head><style>body {--flagship-top-height: 20px; --flagship-bottom-height: 0px;}</style><meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" /></head><body class="flagship-app flagship-os-ios flagship-route-home"></body></html>`
 
-  expect(compose(addCSS, addBodyClasses, addMetaAttributes)(html)).toEqual(
-    expected
-  )
+  expect(
+    compose(addBarStyles, addBodyClasses, addMetaAttributes)(html)
+  ).toEqual(expected)
 })
