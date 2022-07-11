f0f031bad8de0739137094b386fc90b2b13cf782
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-07-11 12:12:19 +0200

    feat: Create Red connector

---
+++ connectors/red/src/index.js
@@ -0,0 +1,30 @@
+import ContentScript from '../../connectorLibs/ContentScript'
+import Minilog from '@cozy/minilog'
+const log = Minilog('ContentScript')
+Minilog.enable()
+
+const baseUrl = 'https://www.red-by-sfr.fr/'
+
+class TemplateContentScript extends ContentScript {
+  async ensureAuthenticated() {
+    await this.goto(baseUrl)
+  }
+
+  async checkAuthenticated() {
+
+  }
+
+  async fetch(context) {
+  }
+
+  async getUserDataFromWebsite() {
+    return {
+      sourceAccountIdentifier: 'defaultTemplateSourceAccountIdentifier'
+    }
+  }
+}
+
+const connector = new TemplateContentScript()
+connector.init({ additionalExposedMethodsNames: [] }).catch(err => {
+  console.warn(err)
+})
