8e91691d4a1fc6a2a66c8f4ce16a2756776855e0
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-07-21 11:57:50 +0200

    feat(client): set oauth client settings name as Personal Cloud
    In order to avoid users seeing Amiral connected into their Cozy.
    
    react-native-device-info needs to use JDK 11+, not JDK 8.
    
    To install Open JDK11 on OSX:
    `brew install --cask adoptopenjdk11`
    
    To use correct PATH, before running `yarn android`
    `PATH="/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin:$PATH"`

--- __tests__/jestSetupFile.js
+++ __tests__/jestSetupFile.js
@@ -1,6 +1,8 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
 import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js'
+import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock'
 
+jest.mock('react-native-device-info', () => mockRNDeviceInfo)
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
 jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo)
 
