120e5c2d469e443db26b49113193a175a4367b3f
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-07-22 17:19:51 +0200

    feat: user login and catching user credentials

--- connectors/red/src/index.js
+++ connectors/red/src/index.js
@@ -4,24 +4,72 @@ const log = Minilog('ContentScript')
 Minilog.enable()
 
 const baseUrl = 'https://www.red-by-sfr.fr/'
+const HOMEPAGE_URL = 'https://www.red-by-sfr.fr/mon-espace-client/?casforcetheme=espaceclientred#sfrclicid=EC_mire_Me-Connecter'
 
 class TemplateContentScript extends ContentScript {
+  //////////
+  //PILOT //
+  //////////
   async ensureAuthenticated() {
-    await this.goto(baseUrl)
-    await this.waitForElementInWorker('[pause]')
+    const credentials = await this.getCredentials()
+    if (credentials) {
+      await this.waitForElementInWorker('[pauseWithCred]')
+    }
+    if(!credentials){
+      await this.goto(baseUrl)
+      await this.waitForElementInWorker('a[href="//www.red-by-sfr.fr/mon-espace-client/?casforcetheme=espaceclientred#redclicid=X_Menu_EspaceClient"]')
+      await this.clickAndWait('a[href="//www.red-by-sfr.fr/mon-espace-client/?casforcetheme=espaceclientred#redclicid=X_Menu_EspaceClient"]', '#username')
+      await this.waitForUserAuthentication()
+      await this.waitForElementInWorker('[pause]')
+      return true
+    }
   }
 
-  async checkAuthenticated() {
-
+  async waitForUserAuthentication() {
+    this.log('waitForUserAuthentication starts')
+    await this.setWorkerState({visible: true})
+    await this.runInWorkerUntilTrue({method: 'waitForAuthenticated'})
+    await this.setWorkerState({visible: false})
   }
 
+  
+  async getUserDataFromWebsite() {
+    
+  }
+  
   async fetch(context) {
   }
-
-  async getUserDataFromWebsite() {
-    return {
-      sourceAccountIdentifier: 'defaultTemplateSourceAccountIdentifier'
+  
+  //////////
+  //WORKER//
+  //////////
+  
+  async checkAuthenticated() {
+    const loginField = document.querySelector('#username')
+    const passwordField = document.querySelector('#password')
+    if (loginField && passwordField) {
+      const userCredentials = await this.findAndSendCredentials.bind(this)(loginField, passwordField)
+      this.log('Sendin userCredentials to Pilot')
+      this.sendToPilot({
+        userCredentials
+      })
     }
+    if(document.location.href === HOMEPAGE_URL && document.querySelector('a[href="https://www.sfr.fr/cas/logout?red=true&url=https://www.red-by-sfr.fr"]')){
+      this.log('Auth Check succeeded')
+      return true
+    }
+    return false
+  }
+
+  async findAndSendCredentials(login, password){
+    this.log('findAndSendCredentials starts')
+    let userLogin = login.value
+    let userPassword = password.value
+    const userCredentials = {
+      login : userLogin,
+      password : userPassword
+    }
+    return userCredentials
   }
 }
 
