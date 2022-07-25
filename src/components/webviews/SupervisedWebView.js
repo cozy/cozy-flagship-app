64d2106460ff2b9706813c1c2ad3cb035fbca6d8
Yannick Chiron <yannick.chiron@gmail.com>
2022-07-25 17:25:10 +0200

    fix: Improve the `remount` mechanism that reload terminated iOS WebViews
    When a WKWebView process is terminated by iOS, sometime just remounting
    the WebView is not enough and it does not reload correctly
    
    This may be due to a race condition during the WebView rendering due to
    how iOS handle processes priority and memory management
    
    To mitigate this, `SuperviserWebView` now tries a new render if the
    component takes too long to render
    
    More info: https://nevermeant.dev/handling-blank-wkwebviews/#killed-before-being-rendered

--- src/components/webviews/SupervisedWebView.js
+++ src/components/webviews/SupervisedWebView.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Platform } from 'react-native'
 import { WebView } from 'react-native-webview'
 
@@ -7,6 +7,8 @@ import Minilog from '@cozy/minilog'
 const log = Minilog('SupervisedWebView')
 
 Minilog.enable()
+
+const RELOAD_DELAY_IN_MS = 10000
 
 /**
  * Display a react-native WebView that is supervised in order to reload it
@@ -25,17 +27,65 @@ export const SupervisedWebView =
   Platform.OS !== 'ios'
     ? WebView
     : React.forwardRef((props, ref) => {
-        const [webViewKey, setWebViewKey] = useState(1)
+        const [state, setState] = useState({
+          isLoaded: false,
+          shouldBeLoaded: false,
+          key: 0
+        })
+
+        const { onLoad, ...otherProps } = props
+        const { isLoaded, shouldBeLoaded, key } = state
+
+        useEffect(
+          function verifyLoadSuccess() {
+            if (shouldBeLoaded) {
+              log.debug('Wait for loading ' + key)
+              let timeout = setTimeout(() => {
+                log.debug('Finished waiting for loading ' + key)
+                setState(oldState => ({ ...oldState, shouldBeLoaded: true }))
+              }, RELOAD_DELAY_IN_MS)
+
+              return () => clearTimeout(timeout)
+            }
+          },
+          [shouldBeLoaded, key]
+        )
+
+        useEffect(
+          function reloadIfLoadFailed() {
+            if (shouldBeLoaded && !isLoaded) {
+              log.debug('WebView failed to load')
+              reloadWebView()
+            } else if (shouldBeLoaded && isLoaded) {
+              log.debug('WebView did successfuly load')
+            }
+          },
+          [shouldBeLoaded, isLoaded]
+        )
+
+        const reloadWebView = () => {
+          log.debug('Trying to reload the WebView')
+          setState(oldState => ({
+            ...oldState,
+            key: oldState.key + 1,
+            isLoaded: false,
+            shouldBeLoaded: false
+          }))
+        }
 
         return (
           <WebView
-            {...props}
+            {...otherProps}
             ref={ref}
             key={key}
             onContentProcessDidTerminate={syntheticEvent => {
               const { nativeEvent } = syntheticEvent
               log.warn('WebView terminated, reloading', nativeEvent)
-              setWebViewKey(oldKey => oldKey + 1)
+              reloadWebView()
+            }}
+            onLoad={syntheticEvent => {
+              setState(oldState => ({ ...oldState, isLoaded: true }))
+              onLoad?.(syntheticEvent)
             }}
           />
         )
