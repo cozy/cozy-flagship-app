ba192048484160ce7ad440dc595f4711b50f2c0c
Yannick Chiron <yannick.chiron@gmail.com>
2022-07-27 09:44:53 +0200

    fix: Make SupervisedWebView work with Android
    Like for iOS, the Android's WebView may be terminated by the OS
    
    This can happen when the WebView renderer crashes
    
    So we want the `SupervisedWebView` component to also work on Android
    
    More info: https://developer.android.com/reference/android/webkit/WebViewClient#onRenderProcessGone(android.webkit.WebView,%20android.webkit.RenderProcessGoneDetail)

--- src/components/webviews/SupervisedWebView.js
+++ src/components/webviews/SupervisedWebView.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Platform, View } from 'react-native'
+import { View } from 'react-native'
 import { WebView } from 'react-native-webview'
 
 import Minilog from '@cozy/minilog'
@@ -43,92 +43,101 @@ const initialState = {
 
 /**
  * Display a react-native WebView that is supervised in order to reload it
- * when terminated by iOS
+ * when terminated
  *
- * More info: https://nevermeant.dev/handling-blank-wkwebviews/
+ * More info (iOS): https://nevermeant.dev/handling-blank-wkwebviews/
+ * More info (Android): https://developer.android.com/reference/android/webkit/WebViewClient#onRenderProcessGone(android.webkit.WebView,%20android.webkit.RenderProcessGoneDetail)
  *
- * In order to test this component:
+ * In order to test this component on iOS:
  * - open the app in an iOS simulator
  * - open the MacOS Activity Monitor and look for `com.apple.WebKit.WebContent` processes
  * - kill the WKWebView you want to test
  *   - the higher the PID is, the later the WKWebView was instanciated
  *   - so first process should be the CryptoWebView, second one should be the HomeView and later should be CozyAppViews
+ *
+ * In order to test this component on Android:
+ * - edit CozyWebView to call `webviewRef.injectJavaScript('javascript:(function() { txt = \"a\"; while(1){ txt += \"a\"; } })();')` with a button
+ * - open the app in an Android emulator
+ * - click the button and wait for the infinite loop to crash the webview
+ * - note: do not inject this code from a Chrome inspector. This won't work as fast and this will make your computer's Chrome unresponsive too
+ * - note: navigating to `chrome://crash` as stated in the Android documentation won't work as the WebView would prevent the navitation to any local resource
  */
-export const SupervisedWebView =
-  Platform.OS !== 'ios'
-    ? WebView
-    : React.forwardRef((props, ref) => {
-        const [state, setState] = useState({
-          ...initialState,
-          key: 0
-        })
+export const SupervisedWebView = React.forwardRef((props, ref) => {
+  const [state, setState] = useState({
+    ...initialState,
+    key: 0
+  })
 
-        const { onLoad, supervisionShowProgress = true, ...otherProps } = props
-        const { isReloading, isLoaded, shouldBeLoaded, key, reloadDelay } =
-          state
+  const { onLoad, supervisionShowProgress = true, ...otherProps } = props
+  const { isReloading, isLoaded, shouldBeLoaded, key, reloadDelay } = state
 
-        useEffect(
-          function verifyLoadSuccess() {
-            if (isReloading) {
-              log.debug('Wait for loading ' + key)
-              let timeout = setTimeout(() => {
-                log.debug('Finished waiting for loading ' + key)
-                setState(oldState => ({ ...oldState, shouldBeLoaded: true }))
-              }, reloadDelay)
+  useEffect(
+    function verifyLoadSuccess() {
+      if (isReloading) {
+        log.debug('Wait for loading ' + key)
+        let timeout = setTimeout(() => {
+          log.debug('Finished waiting for loading ' + key)
+          setState(oldState => ({ ...oldState, shouldBeLoaded: true }))
+        }, reloadDelay)
 
-              return () => clearTimeout(timeout)
-            }
-          },
-          [isReloading, key, reloadDelay]
-        )
+        return () => clearTimeout(timeout)
+      }
+    },
+    [isReloading, key, reloadDelay]
+  )
 
-        useEffect(
-          function reloadIfLoadFailed() {
-            if (shouldBeLoaded && !isLoaded) {
-              log.debug('WebView failed to load')
-              reloadWebView()
-            } else if (shouldBeLoaded && isLoaded) {
-              log.debug('WebView did successfuly load')
-              setState(oldState => ({ ...oldState, ...initialState }))
-            }
-          },
-          [shouldBeLoaded, isLoaded]
-        )
+  useEffect(
+    function reloadIfLoadFailed() {
+      if (shouldBeLoaded && !isLoaded) {
+        log.debug('WebView failed to load')
+        reloadWebView()
+      } else if (shouldBeLoaded && isLoaded) {
+        log.debug('WebView did successfuly load')
+        setState(oldState => ({ ...oldState, ...initialState }))
+      }
+    },
+    [shouldBeLoaded, isLoaded]
+  )
 
-        const reloadWebView = () => {
-          log.debug('Trying to reload the WebView')
-          setState(oldState => ({
-            ...oldState,
-            key: oldState.key + 1,
-            isLoaded: false,
-            shouldBeLoaded: false,
-            isReloading: true,
-            reloadDelay: oldState.shouldBeLoaded
-              ? RELOAD_MAX_DELAY_IN_MS
-              : RELOAD_DELAY_IN_MS
-          }))
-        }
+  const reloadWebView = () => {
+    log.debug('Trying to reload the WebView')
+    setState(oldState => ({
+      ...oldState,
+      key: oldState.key + 1,
+      isLoaded: false,
+      shouldBeLoaded: false,
+      isReloading: true,
+      reloadDelay: oldState.shouldBeLoaded
+        ? RELOAD_MAX_DELAY_IN_MS
+        : RELOAD_DELAY_IN_MS
+    }))
+  }
 
-        return (
-          <>
-            <WebView
-              {...otherProps}
-              ref={ref}
-              key={key}
-              onContentProcessDidTerminate={syntheticEvent => {
-                const { nativeEvent } = syntheticEvent
-                log.warn('WebView terminated, reloading', nativeEvent)
-                reloadWebView()
-              }}
-              onLoad={syntheticEvent => {
-                setState(oldState => ({ ...oldState, isLoaded: true }))
-                onLoad?.(syntheticEvent)
-              }}
-            />
-            {!isLoaded && supervisionShowProgress && <RemountProgress />}
-          </>
-        )
-      })
+  return (
+    <>
+      <WebView
+        {...otherProps}
+        ref={ref}
+        key={key}
+        onContentProcessDidTerminate={syntheticEvent => {
+          const { nativeEvent } = syntheticEvent
+          log.warn('WebView terminated, reloading', nativeEvent)
+          reloadWebView()
+        }}
+        onRenderProcessGone={syntheticEvent => {
+          const { nativeEvent } = syntheticEvent
+          log.warn('WebView crashed, reloading', nativeEvent)
+          reloadWebView()
+        }}
+        onLoad={syntheticEvent => {
+          setState(oldState => ({ ...oldState, isLoaded: true }))
+          onLoad?.(syntheticEvent)
+        }}
+      />
+      {!isLoaded && supervisionShowProgress && <RemountProgress />}
+    </>
+  )
+})
 
 SupervisedWebView.displayName = 'SupervisedWebView'
 
