6319e77ca6f7c438289fe01e04db712b934f1003
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-08-16 13:02:31 +0200

    feat: Fetching bills

--- connectors/totalenergies/src/index.js
+++ connectors/totalenergies/src/index.js
@@ -49,19 +49,35 @@ class TemplateContentScript extends ContentScript {
   }
   
   async fetch(context) {
+    await this.clickAndWait('a[href="/clients/mes-factures/mes-factures-et-paiements"]', 'a[href="/clients/mes-factures/mes-factures-electricite/mon-historique-de-factures"]')
+    await this.clickAndWait('a[href="/clients/mes-factures/mes-factures-electricite/mon-historique-de-factures"]', '.detail-facture')
+    await this.runInWorker('getBills')
+    await this.saveBills(this.store.allDocuments, {
+      context,
+      fileIdAttributes: ['vendorRef', 'filename'],
+      contentType: 'application/pdf',
+      qualificationLabel: 'energy_invoice'
+    })
     // await this.waitForElementInWorker('[pause]')
   }
 
   async authWithCredentials(){
+    this.log('auth with credentials starts')
     await this.goto(baseUrl)
     await this.waitForElementInWorker('a[class="menu-p-btn-ec"]')
     await this.runInWorker('clickLoginPage')
     if (await this.checkAuthenticated()){
       return true
     }
+    // else {
+    //   //Auto-login coming
+    //   await this.waitForUserAuthentication()
+    //   return true
+    // }
   }
 
   async authWithoutCredentials(){
+    this.log('auth without credentials starts')
     await this.goto(baseUrl)
     await this.waitForElementInWorker('a[class="menu-p-btn-ec"]')
     await this.runInWorker('clickLoginPage')
@@ -145,13 +161,174 @@ class TemplateContentScript extends ContentScript {
     if(userMail) return userMail
     return 'UNKNOWN_ERROR'
   }
+
+  async getBills(){
+    this.log('getBills starts')
+    const invoices = await this.getInvoices()
+    const schedules = await this.getSchedules()
+    const allDocuments = await this.computeInformations(invoices, schedules)
+    await this.sendToPilot({allDocuments}) 
+  }
+
+  getInvoices() {
+    const invoices = document.querySelectorAll('div[class="detail-facture"]')
+    return invoices
+  }
+
+  getSchedules() {
+    const schedulesInfos = document.querySelectorAll('.action__condition-conteneur-label')
+    // const schedulesUrl = document.querySelectorAll('.action__display-zone > div[class="text-center mt-std"] > a')
+    let schedules = []
+    for (let i = 0; i < schedulesInfos.length; i++) {
+      const schedulesObject = {
+        element: schedulesInfos[i],
+        downloadButton : schedulesInfos[i].nextElementSibling.children[1].children[0]
+      }
+      schedules.push(schedulesObject)
+    }
+    return schedules
+  }
+
+  computeInformations(invoices, schedules) {
+    let computedInvoices = []
+    for (let i = 0; i < invoices.length; i++){
+      const vendorRef = invoices[i].children[0].children[2].innerHTML.match(/N° (.*)/)[1]
+      const docTitle = invoices[i].children[0].children[0].innerHTML
+      const rawDate = invoices[i].children[1].innerHTML
+      const splitDate = rawDate.split('/')
+      const day = splitDate[0]
+      const month = splitDate[1]
+      const year = splitDate[2]
+      const rawPaymentStatus = invoices[i].children[2].innerHTML
+      const paymentStatus = this.findBillStatus(rawPaymentStatus)
+      const rawAmount = invoices[i].children[3].innerHTML.match(/([0-9]){1,},([0-9]){1,2}/g)
+      const rawCurrency =  invoices[i].children[3].innerHTML.match(/€|\$|£/g)
+      const currency = rawCurrency === '€' ? 'EUR' : rawCurrency[0]
+      const href = invoices[i].children[4].getAttribute('href')
+      const fileurl = `https://www.totalenergies.fr${href}`
+      const amount = parseFloat(rawAmount[0].replace(',', '.'))
+      const date = new Date(`${month}/${day}/${year}`)
+      let invoice = {
+        docTitle,
+        filename : `${day}-${month}-${year}_TotalEnergies_${docTitle.replace(/ /g,'-')}_${amount}${currency}.pdf`,
+        vendorRef,
+        amount,
+        date,
+        currency,
+        fileurl,
+        fileIdAttributes: ['vendorRef'],
+        vendor: 'Total Energies',
+        fileAttributes: {
+          metadata: {
+            contentAuthor: 'totalenergies.fr',
+            issueDate: new Date(),
+            datetime: date,
+            datetimeLabel: `issueDate`,
+            invoiceNumber: `${vendorRef}`,
+            isSubscription: true,
+            carbonCopy: true
+          }
+        }
+      }
+      switch (paymentStatus) {
+        case 'Paid':
+          invoice.paymentStatus = paymentStatus
+          invoice.paymentStatusDate = rawPaymentStatus.match(/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/g)[0]
+          break
+        case 'Refunded':
+          invoice.paymentStatus = paymentStatus
+          invoice.paymentStatusDate = rawPaymentStatus.match(/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/g)[0]
+          invoice.isRefund = true
+          break
+        default:
+          invoice.paymentStatus = paymentStatus
+          break
+      }
+      computedInvoices.push(invoice)
+    }
+    let computedSchedules = []
+    for (let j = 0; j < schedules.length; j++) {
+      const vendorRef = schedules[j].element.children[0].children[2].innerHTML.match(/N° (.*)/)[1]
+      const docTitle = schedules[j].element.children[0].children[0].innerHTML
+      const rawDate = schedules[j].element.children[1].innerHTML
+      const splitDate = rawDate.split('/')
+      const day = splitDate[0]
+      const month = splitDate[1]
+      const year = splitDate[2]
+      const rawPaymentStatus = schedules[j].element.children[2].innerHTML
+      const paymentStatus = this.findBillStatus(rawPaymentStatus)
+      const rawAmount = schedules[j].element.children[3].innerHTML.match(/([0-9]){1,},([0-9]){1,2}/g)
+      const rawCurrency =  schedules[j].element.children[3].innerHTML.match(/€|\$|£/g)
+      const currency = rawCurrency === '€' ? 'EUR' : rawCurrency[0]
+      const href = schedules[j].downloadButton.getAttribute('href')
+      const fileurl = `https://www.totalenergies.fr${href}`
+      const amount = parseFloat(rawAmount[0].replace(',', '.'))
+      const date = new Date(`${month}/${day}/${year}`)
+      let schedule = {
+        docTitle,
+        filename : `${day}-${month}-${year}_TotalEnergies_${docTitle.replace(/ /g,'-')}_${amount}${currency}.pdf`,
+        vendorRef,
+        amount,
+        date,
+        currency,
+        fileurl,
+        fileIdAttributes: ['vendorRef'],
+        vendor: 'Total Energies',
+        fileAttributes: {
+          metadata: {
+            contentAuthor: 'totalenergies.fr',
+            issueDate: new Date(),
+            datetime: date,
+            datetimeLabel: `issueDate`,
+            invoiceNumber: `${vendorRef}`,
+            isSubscription: true,
+            carbonCopy: true
+          }
+        }
+      }
+      switch (paymentStatus) {
+        case 'Paid':
+          schedule.paymentStatus = paymentStatus
+          schedule.paymentStatusDate = rawPaymentStatus.match(/([0-9]{2}\/[0-9]{2}\/[0-9]{4})/)
+          break
+        case 'Refunded':
+          schedule.paymentStatus = paymentStatus
+          schedule.paymentStatusDate = rawPaymentStatus.match(/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/)
+          schedule.isRefunded = true
+          break
+        default:
+          schedule.paymentStatus = paymentStatus
+          break
+      }
+      computedSchedules.push(schedule)
+    }
+    const computedDocs = computedInvoices.concat(computedSchedules)
+    return computedDocs
+  }
+
+  findBillStatus(rawPaymentStatus){
+    if(rawPaymentStatus.match('Payée')){
+      return 'Paid'
+    }
+    else if(rawPaymentStatus.match('Terminé')){
+      return 'Ended'
+    }
+    else if(rawPaymentStatus.match('Remboursée')){
+      return 'Refunded'
+    }
+    else {
+      this.log('Unknown status, returning as it is')
+      return rawPaymentStatus
+    }  
+  }
 }
 
 const connector = new TemplateContentScript()
 connector.init({ additionalExposedMethodsNames: [
   'clickLoginPage',
   'checkMaintenanceStatus',
-  'getUserMail'
+  'getUserMail',
+  'getBills',
 ] }).catch(err => {
   console.warn(err)
 })
