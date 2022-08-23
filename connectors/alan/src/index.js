f128855c41478c864cc04d2de36d098a5aff3352
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-08-23 18:33:31 +0200

    feat: Login without credentials

--- connectors/alan/src/index.js
+++ connectors/alan/src/index.js
@@ -1,9 +1,11 @@
 import ContentScript from '../../connectorLibs/ContentScript'
-import {kyScraper as ky, blobToBase64} from '../../connectorLibs/utils'
+// import {kyScraper as ky, blobToBase64} from '../../connectorLibs/utils'
 import Minilog from '@cozy/minilog'
 const log = Minilog('ContentScript')
-Minilog.enable('sfrCCC')
+Minilog.enable('alanCCC')
 
+const BASE_URL = 'https://alan.com/fr-fr'
+const HOMEPAGE_URL = 'https://alan.com/app/dashboard'
 class TemplateContentScript extends ContentScript {
   //////////
   //PILOT //
@@ -11,10 +13,18 @@ class TemplateContentScript extends ContentScript {
   async ensureAuthenticated() {
     const credentials = await this.getCredentials()
     if (credentials) {
-
+      const auth = await this.authWithCredentials()
+      if(auth) {
+        return true
+      }
+      return false
     }
     if(!credentials){
-
+      const auth = await this.authWithoutCredentials()
+      if(auth) {
+        return true
+      }
+      return false
     }
   }
 
@@ -27,11 +37,29 @@ class TemplateContentScript extends ContentScript {
 
 
   async getUserDataFromWebsite() {
-
+    await this.waitForElementInWorker('[pauseGetData]')
   }
 
   async fetch(context) {
 
+  }
+
+  async authWithCredentials(){
+    await this.goto(BASE_URL)
+    await this.waitForElementInWorker('[pauseWithCred]')
+  }
+
+  async authWithoutCredentials(){
+    await this.goto(BASE_URL)
+    await this.waitForElementInWorker('a[href="/login"]')
+    await this.clickAndWait('a[href="/login"]', 'input[name="password"]' )
+    await this.waitForUserAuthentication()
+    await this.saveCredentials(this.store.userCredentials)
+    const isAskingForDownload = await this.runInWorker('checkAskForAppDowload')
+    if(isAskingForDownload){
+      await this.clickAndWait('a[href="#"]','div[class="murray__NavList"]')
+    }
+    return true
   }
 
 
@@ -41,11 +69,39 @@ class TemplateContentScript extends ContentScript {
 
 
   async checkAuthenticated() {
-
+    const loginField = document.querySelector('input[name="email"]')
+    const passwordField = document.querySelector('input[name="password"]')
+    if (loginField && passwordField) {
+      const userCredentials = await this.findAndSendCredentials.bind(this)(loginField, passwordField)
+      this.log('Sendin userCredentials to Pilot')
+      this.sendToPilot({
+        userCredentials
+      })
+    }
+    if(document.location.href.includes(`${HOMEPAGE_URL}`) && document.querySelector('a[href="#"]') || document.querySelector('div[class="murray__NavList"]')){
+      this.log('Auth Check succeeded')
+      return true
+    }
+    return false
   }
 
   async findAndSendCredentials(login, password){
+    this.log('findAndSendCredentials starts')
+    let userLogin = login.value
+    let userPassword = password.value
+    const userCredentials = {
+      login : userLogin,
+      password : userPassword
+    }
+    return userCredentials
+  }
 
+  checkAskForAppDowload() {
+    if(document.querySelector('a[href="#"]')){
+      return true
+    }else{
+      return false
+    }
   }
 
   async getUserMail() {    
@@ -54,6 +110,8 @@ class TemplateContentScript extends ContentScript {
 }
 
 const connector = new TemplateContentScript()
-connector.init({ additionalExposedMethodsNames: [] }).catch(err => {
+connector.init({ additionalExposedMethodsNames: [
+  'checkAskForAppDowload',
+] }).catch(err => {
   console.warn(err)
 })
