c3abba0533c5ab029e650252e2109a030276f0a9
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-08-24 18:54:08 +0200

    feat: Adding saveIdentity

--- connectors/alan/src/index.js
+++ connectors/alan/src/index.js
@@ -5,12 +5,19 @@ const log = Minilog('ContentScript')
 Minilog.enable('alanCCC')
 
 const BASE_URL = 'https://alan.com/fr-fr'
+const LOGIN_URL = 'https://alan.com/login'
 const HOMEPAGE_URL = 'https://alan.com/app/dashboard'
+const PERSONAL_INFOS_URL = 'https://alan.com/app/dashboard#individualProfile/home'
 class TemplateContentScript extends ContentScript {
   //////////
   //PILOT //
   //////////
   async ensureAuthenticated() {
+    // We need to force Desktop version of the website, otherwise some pages won't be accessible on mobile
+    await this.bridge.call(
+      'setUserAgent',
+      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0',
+    )
     const credentials = await this.getCredentials()
     if (credentials) {
       const auth = await this.authWithCredentials()
@@ -37,16 +44,35 @@ class TemplateContentScript extends ContentScript {
 
 
   async getUserDataFromWebsite() {
-    await this.waitForElementInWorker('[pauseGetData]')
+    await this.goto(PERSONAL_INFOS_URL)
+    await this.waitForElementInWorker('.email-wrapper')
+    const sourceAccountId = await this.runInWorker('getUserMail')
+    await this.waitForElementInWorker('.address-wrapper')
+    await this.runInWorker('getUserIdentity')
+    if (sourceAccountId === 'UNKNOWN_ERROR') {
+      this.log("Couldn't get a sourceAccountIdentifier, using default")
+      return { sourceAccountIdentifier: DEFAULT_SOURCE_ACCOUNT_IDENTIFIER }
+    }
+    return {
+      sourceAccountIdentifier: sourceAccountId
+    }
   }
-
+  
   async fetch(context) {
+    await this.clickAndWait('#individualProfile-invoices', '.coverage-wrapper')
+    await this.waitForElementInWorker('[pause]')
 
   }
 
   async authWithCredentials(){
-    await this.goto(BASE_URL)
+    await this.goto(LOGIN_URL)
+    await this.waitForElementInWorker('.HelpButton')
+    const isLogged = await this.runInWorker('checkIfLogged')
+    if(isLogged){
+      return true
+    }
     await this.waitForElementInWorker('[pauseWithCred]')
+    await this.clickAndWait('a[href="/login"]', 'input[name="password"]' )
   }
 
   async authWithoutCredentials(){
@@ -104,7 +130,56 @@ class TemplateContentScript extends ContentScript {
     }
   }
 
-  async getUserMail() {    
+  checkIfLogged(){
+    if(document.location.href === HOMEPAGE_URL){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  getUserMail() { 
+    const userInfosElements = document.querySelectorAll('.value-box-value')
+    const userMail = userInfosElements[3].innerHTML
+    if (userMail) {
+      return userMail
+    }
+    return 'UNKNOWN_ERROR'
+  }
+
+  async getUserIdentity(){
+    const nameElement = document.querySelector('h4').innerHTML.split('&nbsp;')
+    const nameString = nameElement[0]
+    console.log(nameElement)
+    const givenName = nameString.split(' ')[0]
+    const familyName = nameString.split(' ')[1]
+    const userInfosElements = document.querySelectorAll('.value-box-value')
+    const birthDate = userInfosElements[0].textContent.split('M')[0]
+    const socialSecurityNumber = userInfosElements[1].children[0].children[1].children[0].textContent
+    const email = userInfosElements[3].innerHTML
+    const address = userInfosElements[5].innerHTML.split(', ')
+    const street = address[0]
+    const postCode = address[1]
+    const city = address[2]
+    const userIdentity = {
+      email,
+      birthDate,
+      socialSecurityNumber,
+      name : {
+        givenName,
+        familyName,
+        fullname : `${givenName} ${familyName}`
+      },
+      address: [
+        {
+          formattedAddress : `${street} ${postCode} ${city}`,
+          postCode,
+          city,
+          street
+        }
+      ]
+    }
+    await this.sendToPilot({userIdentity})
   }
 
 }
@@ -112,6 +187,9 @@ class TemplateContentScript extends ContentScript {
 const connector = new TemplateContentScript()
 connector.init({ additionalExposedMethodsNames: [
   'checkAskForAppDowload',
+  'checkIfLogged',
+  'getUserMail',
+  'getUserIdentity',
 ] }).catch(err => {
   console.warn(err)
 })
