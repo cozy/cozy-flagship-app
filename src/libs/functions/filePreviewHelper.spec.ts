c332a71a225956a9174c339216191f59972d7e6a
Yannick Chiron <yannick.chiron@gmail.com>
2022-08-31 17:01:34 +0200

    feat: Intercept downloads on iOS and open QuickLook instead
    On iOS, webViews do not handle downloads, so we have to handle them by
    ourselves
    
    Instead of implementing a download scenario for the user, we want to
    download the file in the local storage and send it to QuickLook through
    react-native-file-viewer
    
    This would allow the user to preview the file when possible and also to
    download the file using the QuickLook share feature

---
+++ src/libs/functions/filePreviewHelper.spec.ts
@@ -0,0 +1,139 @@
+import FileViewer from 'react-native-file-viewer'
+import CozyClient from 'cozy-client'
+import RNFS, { DownloadResult } from 'react-native-fs'
+
+import {
+  checkIsPreviewableLink,
+  previewFileFromDownloadUrl,
+  PUBLIC_BY_SECRET_DOWNLOAD_LINK,
+  PRIVATE_BY_PATH_DOWNLOAD_LINK
+} from './filePreviewHelper'
+
+jest.mock('react-native-file-viewer', () => ({
+  open: jest.fn()
+}))
+
+type DownloadFileT = {
+  promise: Promise<{ statusCode: number }>
+}
+const mockDownloadFile = jest.fn() as jest.MockedFunction<
+  typeof RNFS.downloadFile
+>
+jest.mock('react-native-fs', () => ({
+  downloadFile: (args: RNFS.DownloadFileOptions): DownloadFileT =>
+    mockDownloadFile(args),
+  DocumentDirectoryPath: '/app'
+}))
+
+describe('filePreviewHelper', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('checkIsPreviewableLink', () => {
+    it.each([
+      [
+        `Should return 'PUBLIC_BY_SECRET_DOWNLOAD_LINK' for a '/files/downloads/:secret/:name' link`,
+        PUBLIC_BY_SECRET_DOWNLOAD_LINK,
+        'https://claude.mycozy.cloud/files/downloads/SOME_SECRET/2022-08-29_some_connector.pdf',
+        'https://claude.mycozy.cloud'
+      ],
+      [
+        `Should return 'PUBLIC_BY_SECRET_DOWNLOAD_LINK' for a '/files/downloads/:secret/:name' link from another Cozy`,
+        PUBLIC_BY_SECRET_DOWNLOAD_LINK,
+        'https://claude.mycozy.cloud/files/downloads/SOME_SECRET/2022-08-29_some_connector.pdf',
+        'https://alice.mycozy.cloud'
+      ],
+      [
+        `Should return 'PRIVATE_BY_PATH_DOWNLOAD_LINK' for a 'files/download?Path=' link`,
+        PRIVATE_BY_PATH_DOWNLOAD_LINK,
+        'https://claude.mycozy.cloud/files/download?Path=/Documents/SOME_FILE.pdf&Dl=1',
+        'https://claude.mycozy.cloud'
+      ],
+      [
+        `Should return false for a 'files/download/:file-id' link`,
+        false,
+        'https://claude.mycozy.cloud/files/download/SOME_ID',
+        'https://claude.mycozy.cloud'
+      ],
+      [
+        `Should return false for a 'files/download?Path=' link from another Cozy`,
+        false,
+        'https://claude.mycozy.cloud/files/download?Path=/Documents/SOME_FILE.pdf&Dl=1',
+        'https://alice.mycozy.cloud'
+      ]
+    ])('%p', (reason, result, url, cozyUrl) => {
+      const client = {
+        getStackClient: jest.fn(() => ({
+          uri: cozyUrl
+        }))
+      } as unknown as CozyClient
+
+      expect(checkIsPreviewableLink(url, client)).toEqual(result)
+    })
+  })
+
+  describe('previewFileFromDownloadUrl', () => {
+    it(`should preview '/files/downloads/:secret/:name' links`, async () => {
+      const client = {
+        getStackClient: jest.fn(() => ({
+          uri: 'https://claude.mycozy.cloud',
+          getAuthorizationHeader: jest
+            .fn()
+            .mockReturnValue('SOME_AUTHORIZATION_TOKEN')
+        }))
+      } as unknown as CozyClient
+
+      mockSuccessFileDownload()
+
+      await previewFileFromDownloadUrl({
+        downloadUrl:
+          'https://claude.mycozy.cloud/files/downloads/SOME_SECRET/SOME_FILE.pdf?Dl=1',
+        client
+      })
+
+      expect(mockDownloadFile).toHaveBeenCalledWith({
+        fromUrl:
+          'https://claude.mycozy.cloud/files/downloads/SOME_SECRET/SOME_FILE.pdf?Dl=1',
+        toFile: '/app/SOME_FILE.pdf'
+      })
+      expect(FileViewer.open).toHaveBeenCalledWith('/app/SOME_FILE.pdf')
+    })
+
+    it(`should preview '/files/download?Path=' links`, async () => {
+      const client = {
+        getStackClient: jest.fn(() => ({
+          uri: 'https://claude.mycozy.cloud',
+          getAuthorizationHeader: jest
+            .fn()
+            .mockReturnValue('SOME_AUTHORIZATION_TOKEN')
+        }))
+      } as unknown as CozyClient
+
+      mockSuccessFileDownload()
+
+      await previewFileFromDownloadUrl({
+        downloadUrl:
+          'https://claude.mycozy.cloud/files/download?Path=/Documents/SOME_FILE.pdf&Dl=1',
+        client
+      })
+
+      expect(mockDownloadFile).toHaveBeenCalledWith({
+        fromUrl:
+          'https://claude.mycozy.cloud/files/download?Path=/Documents/SOME_FILE.pdf&Dl=1',
+        headers: {
+          Authorization: 'SOME_AUTHORIZATION_TOKEN'
+        },
+        toFile: '/app/SOME_FILE.pdf'
+      })
+      expect(FileViewer.open).toHaveBeenCalledWith('/app/SOME_FILE.pdf')
+    })
+  })
+})
+
+const mockSuccessFileDownload = (): void => {
+  mockDownloadFile.mockReturnValue({
+    jobId: 1,
+    promise: Promise.resolve({ statusCode: 200 } as unknown as DownloadResult)
+  })
+}
---
+++ src/libs/functions/filePreviewHelper.tsx
@@ -0,0 +1,172 @@
+import Minilog from '@cozy/minilog'
+import CozyClient from 'cozy-client'
+import FileViewer from 'react-native-file-viewer'
+import RNFS from 'react-native-fs'
+
+const log = Minilog('ReloadInterceptorWebView')
+
+/**
+ * Represents a link like `/files/downloads/:secret/:name`
+ */
+export const PUBLIC_BY_SECRET_DOWNLOAD_LINK = 'PUBLIC_BY_SECRET_DOWNLOAD_LINK'
+
+/**
+ * Represents a link like `/files/download?Path=XXX`
+ */
+export const PRIVATE_BY_PATH_DOWNLOAD_LINK = 'PRIVATE_BY_PATH_DOWNLOAD_LINK'
+
+type PreviewType =
+  | typeof PUBLIC_BY_SECRET_DOWNLOAD_LINK
+  | typeof PRIVATE_BY_PATH_DOWNLOAD_LINK
+  | false
+
+/**
+ * Check if the providen link is a download link from the Cozy
+ *
+ * Following links are previewable:
+ * - `/files/downloads/:secret/:name` (see [documentation](https://docs.cozy.io/en/cozy-stack/files/#get-filesdownloadssecretname))
+ * - `/files/download?Path=XXX` (see [documentation](https://docs.cozy.io/en/cozy-stack/files/#get-filesdownload))
+ *
+ * Following links are not implemented:
+ * - `/files/download/:file-id` (see [documentation](https://docs.cozy.io/en/cozy-stack/files/#get-filesdownloadfile-id))
+ * - `/files/archive/:key/:name` (see [documentation](https://docs.cozy.io/en/cozy-stack/files/#get-filesarchivekeyname))
+ *
+ * @param link - the link to be analyzed
+ * @param client - CozyClient instance
+ * @returns false if the link is not previewable, or the link type
+ */
+export const checkIsPreviewableLink = (
+  link: string,
+  client: CozyClient
+): PreviewType => {
+  const cozyUrl = new URL(client.getStackClient().uri)
+
+  const previewLink = new URL(link)
+
+  if (previewLink.pathname.startsWith('/files/downloads/')) {
+    return PUBLIC_BY_SECRET_DOWNLOAD_LINK
+  }
+
+  if (
+    previewLink.host === cozyUrl.host &&
+    previewLink.pathname === '/files/download' &&
+    previewLink.searchParams.has('Path')
+  ) {
+    return PRIVATE_BY_PATH_DOWNLOAD_LINK
+  }
+
+  return false
+}
+
+/**
+ * Download the specified file and display it through native file viewer
+ */
+export const previewFileFromDownloadUrl = async ({
+  downloadUrl,
+  client
+}: {
+  downloadUrl: string
+  client: CozyClient
+}): Promise<void> => {
+  try {
+    const previewType = checkIsPreviewableLink(downloadUrl, client)
+
+    if (previewType === false) {
+      throw new Error('Think link cannot be previewed')
+    }
+
+    const fileName = getFileNameFromCozyDownloadUrl(downloadUrl, previewType)
+
+    if (!fileName) {
+      throw new Error('Impossible to extract fileName from download URL')
+    }
+
+    const filePath = await downloadFile({
+      fileName,
+      downloadUrl,
+      previewType,
+      client
+    })
+
+    await FileViewer.open(filePath)
+  } catch (err) {
+    if (err instanceof Error) {
+      log.error(`Error while downloading file for preview: ${err.message}`)
+    }
+    throw err
+  }
+}
+
+const getFileNameFromPrivateByPathDownloadLink = (
+  downloadUrl: string
+): string => {
+  const url = new URL(downloadUrl)
+
+  if (!url.searchParams.has('Path')) {
+    throw new Error(
+      'PUBLIC_BY_SECRET_DOWNLOAD_LINK links should contain a Path parameter'
+    )
+  }
+
+  return url.searchParams.get('Path')?.split('/').pop() || ''
+}
+
+const getFileNameFromPublicBySecretDownloadLink = (
+  downloadUrl: string
+): string => {
+  const url = new URL(downloadUrl)
+
+  return url.pathname.split('/').pop() || ''
+}
+
+const getFileNameFromCozyDownloadUrl = (
+  downloadUrl: string,
+  previewType: PreviewType
+): string | undefined => {
+  if (previewType === PUBLIC_BY_SECRET_DOWNLOAD_LINK) {
+    return getFileNameFromPublicBySecretDownloadLink(downloadUrl)
+  }
+
+  if (previewType === PRIVATE_BY_PATH_DOWNLOAD_LINK) {
+    return getFileNameFromPrivateByPathDownloadLink(downloadUrl)
+  }
+
+  throw new Error(`No fileName can be extracted from ${downloadUrl}`)
+}
+
+const downloadFile = async ({
+  fileName,
+  downloadUrl,
+  previewType,
+  client
+}: {
+  fileName: string
+  downloadUrl: string
+  previewType: PreviewType
+  client: CozyClient
+}): Promise<string> => {
+  const stackClient = client.getStackClient()
+  const headers = stackClient.getAuthorizationHeader()
+
+  const destinationPath = `${RNFS.DocumentDirectoryPath}/${fileName}`
+  const result = await RNFS.downloadFile({
+    fromUrl: downloadUrl,
+    toFile: destinationPath,
+    headers:
+      previewType === PRIVATE_BY_PATH_DOWNLOAD_LINK
+        ? {
+            Authorization: headers
+          }
+        : undefined
+  }).promise
+
+  log.debug(`Donload result is ${JSON.stringify(result)}`)
+
+  const { statusCode } = result
+
+  if (statusCode < 200 || statusCode >= 300) {
+    throw new Error(`Status code: ${statusCode}`)
+  }
+
+  return destinationPath
+}
