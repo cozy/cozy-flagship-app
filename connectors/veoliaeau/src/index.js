04425c37cd6b33e865e695a1d612dd1d79fe1980
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-09-22 11:09:10 +0200

    feat: Creating VeoliaEau connector

---
+++ connectors/veoliaeau/src/index.js
@@ -0,0 +1,59 @@
+import ContentScript from '../../connectorLibs/ContentScript'
+import {kyScraper as ky, blobToBase64} from '../../connectorLibs/utils'
+import Minilog from '@cozy/minilog'
+const log = Minilog('ContentScript')
+Minilog.enable('veoliaeauCCC')
+
+class TemplateContentScript extends ContentScript {
+  //////////
+  //PILOT //
+  //////////
+  async ensureAuthenticated() {
+    const credentials = await this.getCredentials()
+    if (credentials) {
+
+    }
+    if(!credentials){
+
+    }
+  }
+
+  async waitForUserAuthentication() {
+    this.log('waitForUserAuthentication starts')
+    await this.setWorkerState({visible: true})
+    await this.runInWorkerUntilTrue({method: 'waitForAuthenticated'})
+    await this.setWorkerState({visible: false})
+  }
+
+
+  async getUserDataFromWebsite() {
+
+  }
+
+  async fetch(context) {
+
+  }
+
+
+  //////////
+  //WORKER//
+  //////////
+
+
+  async checkAuthenticated() {
+
+  }
+
+  async findAndSendCredentials(login, password){
+
+  }
+
+  async getUserMail() {    
+  }
+
+}
+
+const connector = new TemplateContentScript()
+connector.init({ additionalExposedMethodsNames: [] }).catch(err => {
+  console.warn(err)
+})
