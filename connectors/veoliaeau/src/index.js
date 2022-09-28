879e7474941dc951791d2808d01d64ec70b75921
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-09-28 09:51:28 +0200

    feat: Getting user datas

--- connectors/veoliaeau/src/index.js
+++ connectors/veoliaeau/src/index.js
@@ -4,6 +4,7 @@ import Minilog from '@cozy/minilog'
 const log = Minilog('ContentScript')
 Minilog.enable('veoliaeauCCC')
 
+const DEFAULT_SOURCE_ACCOUNT_IDENTIFIER = 'veolia eau'
 const BASE_URL = 'https://www.service.eau.veolia.fr/home.html'
 const HOMEPAGE_URL = 'https://www.service.eau.veolia.fr/home/espace-client.html#inside-space'
 
@@ -52,7 +53,6 @@ class TemplateContentScript extends ContentScript {
     await this.waitForElementInWorker('#veolia_username')
     await this.waitForUserAuthentication()
     await this.saveCredentials(this.store.userCredentials)
-    await this.waitForElementInWorker('[pause]')
   }
 
   async waitForUserAuthentication() {
@@ -64,11 +64,26 @@ class TemplateContentScript extends ContentScript {
 
   
   async getUserDataFromWebsite() {
-    await this.waitForElementInWorker('[pause]')
-
+    await this.clickAndWait('a[href="/home/espace-client/vos-contrats.html"]', '.bloc_princ')
+    await this.runInWorker('getUserBillingInfos')
+    await this.clickAndWait('a[href="/home/espace-client/gerer-votre-espace-personnel.html"]', '.fiche-client')
+    await this.runInWorker('getUserPersonnalInfos')
+    await this.runInWorker('computeIdentity', this.store)
+    const sourceAccountId = this.store.userIdentity.email ? this.store.userIdentity.email : 'UNKNOWN_ERROR'
+    if (sourceAccountId === 'UNKNOWN_ERROR') {
+      this.log("Couldn't get a sourceAccountIdentifier, using default")
+      return { sourceAccountIdentifier: DEFAULT_SOURCE_ACCOUNT_IDENTIFIER }
+    }
+    return {
+      sourceAccountIdentifier: sourceAccountId
+    }
   }
   
   async fetch(context) {
+    await this.clickAndWait('a[href="/home/espace-client/vos-factures-et-correspondances.html"]', '.liste-table')
+    await this.runInWorker('getBills')
+    await this.waitForElementInWorker('[pause]')
+    await this.saveIdentity(this.store.userIdentity)
     
   }
 
@@ -165,7 +180,80 @@ class TemplateContentScript extends ContentScript {
     return true
   }
 
-  async getUserMail() {    
+  async getUserPersonnalInfos() {   
+    const clientInfos = document.querySelectorAll('.ligne-info')
+    const email = clientInfos[0].innerHTML.split(': ')[1]
+    const homePhoneNumber = clientInfos[2].innerHTML.split(': ')[1]
+    const mobilePhoneNumber = clientInfos[3].innerHTML.split(': ')[1]
+    let userPersonalInfos = {
+      email,
+      phone: []
+    }
+    if(homePhoneNumber && homePhoneNumber !== ''){
+      userPersonalInfos.phone.push({
+        type: 'home',
+        number: homePhoneNumber
+      })
+    }
+    if(mobilePhoneNumber && mobilePhoneNumber !== ''){
+      userPersonalInfos.phone.push({
+        type: 'home',
+        number: homePhoneNumber
+      })
+    }
+    await this.sendToPilot({userPersonalInfos})
+  }
+
+  async getUserBillingInfos(){
+    const billingInfosElements = document.querySelectorAll('div[class="bloc_ct bloc_ct_1 bloc_contrat"]')
+    const rawContent = billingInfosElements[1].children[1].textContent
+    const userBillingInfosArray = rawContent.match(/([A-Z0-9 -]{1,})/g)
+    const [firstName, lastName] = userBillingInfosArray[1].split(' ')
+    const street = `${userBillingInfosArray[2]} ${userBillingInfosArray[3]}`
+    const postCode = userBillingInfosArray[4]
+    const city = userBillingInfosArray[5]
+    const userBillingInfos = {
+      name : {
+        firstName,
+        lastName,
+        fullName : `${firstName} ${lastName}`
+      },
+      address : [
+        {
+          street,
+          postCode,
+          city,
+          formattedAddress : `${street} ${postCode} ${city}`
+        }
+      ]
+    }
+    await this.sendToPilot({userBillingInfos})
+  }
+
+  async computeIdentity(store){
+    const userIdentity = {
+      ...store.userBillingInfos,
+      ...store.userPersonalInfos
+    }
+    await this.sendToPilot({userIdentity})
+  }
+
+  async getBills(){
+    const buttonIsAvailable = await this.checkMoreBillsButton()
+    if(buttonIsAvailable){
+      buttonIsAvailable.click()
+    }
+    // Trouver l'equivalent de ça dans le worker
+    await this.waitForElementNoReload('table')
+    
+  }
+  
+  checkMoreBillsButton(){
+    const moreBillsButton = document.querySelector('a[href="/home/espace-client/vos-factures-et-correspondances.html?voirPlus"]')
+    if(moreBillsButton){
+      return moreBillsButton
+    }
+    return false
   }
 
 }
@@ -174,6 +262,10 @@ const connector = new TemplateContentScript()
 connector.init({ additionalExposedMethodsNames: [
   'checkIfLogged',
   'handleForm',
+  'getUserPersonnalInfos',
+  'getUserBillingInfos',
+  'computeIdentity',
+  'getBills'
 ] }).catch(err => {
   console.warn(err)
 })
