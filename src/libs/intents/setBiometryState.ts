cacde37c2d854c6e1b9df18364e4ee29f2c0fa1f
Antonin Cezard <acezard@gmail.com>
2022-11-08 17:00:25 +0100

    fix: Link autolock and biometry only on activation

--- src/libs/intents/setBiometryState.ts
+++ src/libs/intents/setBiometryState.ts
@@ -13,7 +13,8 @@ const BIOMETRY_DENIED_BY_USER_IOS_ERROR = /.*User.*denied.*/
 
 const updateBiometrySetting = async (activated: boolean): Promise<boolean> => {
   await storeData(StorageKeys.BiometryActivated, activated)
-  await storeData(StorageKeys.AutoLockEnabled, activated)
+  if (!activated && !(await getData(StorageKeys.AutoLockEnabled)))
+    await storeData(StorageKeys.AutoLockEnabled, true)
   const newData = Boolean(await getData(StorageKeys.BiometryActivated))
 
   BiometryEmitter.emit('change', newData)
