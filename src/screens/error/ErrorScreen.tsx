6aa4911e62800a96164d3b47bad4343e5ac6c68c
Antonin Cezard <acezard@gmail.com>
2022-11-29 16:31:27 +0100

    feat: handle mailto error
    Also didn't see conflicts with Supervised or Interceptor WebView

--- src/screens/error/ErrorScreen.tsx
+++ src/screens/error/ErrorScreen.tsx
@@ -1,30 +1,78 @@
 import React from 'react'
 import { Linking } from 'react-native'
+import { WebViewMessageEvent } from 'react-native-webview/lib/WebViewTypes'
+
+import Minilog from '@cozy/minilog'
 
 import { CozyNotFoundPage } from '/components/webviews/CozyNotFoundPage'
 import { OfflinePage } from '/components/webviews/OfflinePage'
 import { SupervisedWebView } from '/components/webviews/SupervisedWebView'
 import { goBack } from '/libs/RootNavigation'
 
-const HTML = {
+const log = Minilog('ErrorScreen')
+
+/**
+ * Typings
+ */
+interface ErrorScreenProps {
+  route: { params: { type: string } }
+}
+
+interface Source {
+  html: ReturnType<typeof HTML[keyof typeof HTML]>
+}
+
+type Handlers = Record<
+  string,
+  { call: (() => void) | (() => Promise<void>); shouldCatch: boolean }
+>
+
+/**
+ * Implementations
+ */
+const HTML: Record<string, () => string> = {
   cozyNotFound: CozyNotFoundPage,
   offline: OfflinePage
 }
 
-const handlers = {
-  backButton: () => goBack(),
-  mailto: () => Linking.openURL('mailto:contact@cozycloud.cc')
+const handlers: Handlers = {
+  backButton: { call: goBack, shouldCatch: false },
+  mailto: {
+    call: () => Linking.openURL('mailto:contact@cozycloud.cc'),
+    shouldCatch: true
+  }
 }
 
-const handleMessage = ({ nativeEvent: { data } }) =>
-  Object.keys(handlers).forEach(
-    eventId => data?.includes(eventId) && handlers[eventId]()
+const handleMessage = async (
+  event: WebViewMessageEvent
+): Promise<undefined[]> =>
+  await Promise.all(
+    Object.keys(handlers).map(async eventId => {
+      if (!event.nativeEvent.data.includes(eventId)) return undefined
+
+      if (handlers[eventId].shouldCatch) {
+        try {
+          await handlers[eventId].call()
+        } catch (error) {
+          log.error(error)
+        }
+      }
+
+      /**
+       * We don't want to catch errors here hence the void return.
+       * Doesn't even matter if the function is a Promise or not.
+       */
+      if (!handlers[eventId].shouldCatch) void handlers[eventId].call()
+    })
   )
 
-const makeSource = route => ({
-  html: HTML[route?.params?.type]?.() ?? OfflinePage
-})
+const makeSource = (route: ErrorScreenProps['route']): Source => {
+  return { html: HTML[route.params.type]() }
+}
 
-export const ErrorScreen = ({ route }) => (
-  <SupervisedWebView onMessage={handleMessage} source={makeSource(route)} />
+export const ErrorScreen = (props: ErrorScreenProps): JSX.Element => (
+  <SupervisedWebView
+    onMessage={handleMessage}
+    source={makeSource(props.route)}
+  />
 )
