75558c6efdc14d889b6c6242e97cd103250fd9b9
Antonin Cezard <acezard@gmail.com>
2022-12-12 09:45:56 +0100

    feat: fix Jest config in tsx tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 const config = {
   clearMocks: true,
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  moduleNameMapper: {
+    '^/(.*)$': '<rootDir>/src/$1'
+  },
   preset: 'react-native',
   setupFiles: ['<rootDir>/__tests__/jestSetupFile.js'],
-  transform: { '^.+\\.jsx$': 'babel-jest' },
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?)/)'
   ]
