e1d49e864dd9844a4b18815b0bfe21196ac07088
Antonin Cezard <acezard@gmail.com>
2022-12-14 16:15:05 +0100

    refactor: convert splashscreen files to TS

--- src/hooks/useSplashScreen.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-import { SplashScreenContext } from '../libs/contexts/SplashScreenContext'
-
-export const useSplashScreen = () => ({
-  hideSplashScreen: useContext(SplashScreenContext).hide,
-  showSplashScreen: useContext(SplashScreenContext).show
-})
