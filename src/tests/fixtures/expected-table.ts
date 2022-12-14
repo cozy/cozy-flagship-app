69165d4a3ddfa66e9cdcfe7be6284406c196691f
Antonin Cezard <acezard@gmail.com>
2022-12-14 15:15:06 +0100

    refactor: migrate iconTable to TS
    Rename files to TypeScript

---
+++ src/tests/fixtures/expected-table.ts
@@ -0,0 +1,13 @@
+export const expectedTable = {
+  banks: { version: '1.43.0', xml: '<svg></svg>' },
+  coachco2: { version: '0.4.0', xml: '<svg></svg>' },
+  contacts: { version: '1.1.0', xml: '<svg></svg>' },
+  drive: { version: '1.41.0', xml: '<svg></svg>' },
+  home: { version: '1.46.0', xml: '<svg></svg>' },
+  mespapiers: { version: '0.1.0', xml: '<svg></svg>' },
+  notes: { version: '1.20.0', xml: '<svg></svg>' },
+  passwords: { version: '2.0.5', xml: '<svg></svg>' },
+  photos: { version: '1.41.0', xml: '<svg></svg>' },
+  settings: { version: '1.17.0', xml: '<svg></svg>' },
+  store: { version: '1.9.11', xml: '<svg></svg>' }
+}
