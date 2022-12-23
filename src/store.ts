ae3bd59bbcdea53a618e5ebad59348ad72197ca5
Ldoppea <yannick.chiron@gmail.com>
2022-12-23 18:40:12 +0100

    fix: Prevent non-serializable exception when using redux-persist
    When using redux-persist with redux-toolkit some non-serialisable
    actions may be dispatched to the store. This can be the case on app
    start when redux-persist tries to rehydrate the store from AsyncStorage
    
    When this happens, an exception is thrown by redux-persist
    
    To prevent this we want to ignore those actions from the store's
    middleware
    
    More info:
    https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,14 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from 'redux-persist'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import currentConnectorSlice from '/redux/ConnectorState/CurrentConnectorSlice'
 import connectorLogsSlice from '/redux/ConnectorState/ConnectorLogsSlice'
@@ -17,7 +26,13 @@ const rootReducter = combineReducers({
 const persistedReducer = persistReducer(persistConfig, rootReducter)
 
 export const store = configureStore({
-  reducer: persistedReducer
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 })
 
 export const persistor = persistStore(store)
