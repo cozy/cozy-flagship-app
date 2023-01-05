da56b450d9987d216ed2ffd449c00d5ef91c13c0
Antonin Cezard <acezard@gmail.com>
2023-01-05 11:06:40 +0100

    refactor: improve RNFS mockfile

--- __tests__/jestSetupFile.js
+++ __tests__/jestSetupFile.js
@@ -1,7 +1,7 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
 import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js'
 import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock'
-import mockRNFS from '../__mocks__/react-native-fs-mock'
+import { mockRNFS } from '../__mocks__/react-native-fs-mock'
 
 jest.mock('react-native-device-info', () => mockRNDeviceInfo)
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
