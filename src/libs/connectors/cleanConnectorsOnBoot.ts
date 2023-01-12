3987d8b49c40733e57c227a40c793710eaf0429e
Ldoppea <yannick.chiron@gmail.com>
2023-01-12 17:54:28 +0100

    feat: Clean connectors state on App boot
    If the App crashes during a connector execution, some connector logs
    may remain in the AsyncStorage
    
    We want to send those logs (if any) on next App boot
    
    Also we can detect if a connector crashed by reading the
    currentConnector state. This commit add an entry point for this check,
    but we still have to set this state when running a connector and then
    replace the `console.error` with a cleaning action (ex: stop jobs)

---
+++ src/libs/connectors/cleanConnectorsOnBoot.ts
@@ -0,0 +1,60 @@
+import Minilog from '@cozy/minilog'
+import CozyClient from 'cozy-client'
+
+import { getErrorMessage } from '/libs/functions/getErrorMessage'
+import { sendConnectorsLogs } from '/libs/connectors/sendConnectorsLogs'
+import { CurrentConnectorState } from '/redux/ConnectorState/CurrentConnectorSlice'
+import { store } from '/store'
+
+const log = Minilog('cleanConnectorsOnBoot')
+
+const CLEAN_DELAY_IN_MS = 3000
+
+const getRunningConnector = (state: {
+  currentConnector: CurrentConnectorState
+}): string | undefined => {
+  return state.currentConnector.currentRunningConnector
+}
+
+/**
+ * Check for still-running connectors, clean them and
+ * send remaining connectors' logs on cozy-stack
+ */
+export const cleanConnectorsOnBoot = async (
+  client: CozyClient
+): Promise<void> => {
+  const state = store.getState()
+
+  const runningConnector = getRunningConnector(state)
+  if (runningConnector !== undefined) {
+    log.error(
+      `A running connector is tagged as running. This means that the app may have previously crashed. Related connector: ${runningConnector}`
+    )
+  }
+
+  await sendConnectorsLogs(client, 4)
+}
+
+/**
+ * After the specified delay, check for still-running connectors, clean them and
+ * send remaining connectors' logs on cozy-stack
+ *
+ * This method runs in background and should not be awaited
+ */
+export const cleanConnectorsOnBootInBackground = (
+  client: CozyClient,
+  delayInMs = CLEAN_DELAY_IN_MS
+): Promise<void> => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      cleanConnectorsOnBoot(client)
+        .then(resolve)
+        .catch(err =>
+          log.error(
+            `Something went wront while sending logs: ${getErrorMessage(err)}`,
+            resolve()
+          )
+        )
+    }, delayInMs)
+  })
+}
--- src/redux/ConnectorState/CurrentConnectorSlice.ts
+++ src/redux/ConnectorState/CurrentConnectorSlice.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 import type { RootState } from '/store'
 
-interface CurrentConnectorState {
+export interface CurrentConnectorState {
   currentRunningConnector?: string
 }
 
