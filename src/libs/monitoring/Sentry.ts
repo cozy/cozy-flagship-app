b0a7764f5399302b4b7a0b9d16b86ec411312f6e
Antonin Cezard <acezard@gmail.com>
2023-01-16 11:03:35 +0100

    refactor: typify and reorganize Sentry files
    Putting them in a "monitoring" directory for now.

--- src/libs/monitoring/Sentry.ts
+++ src/libs/monitoring/Sentry.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/react-native'
 import { CaptureConsole } from '@sentry/integrations'
 
 import { EnvService } from '/libs/services/EnvService'
-import { version } from '../../package.json'
+import { version } from '../../../package.json'
 
 // Sentry Data Source Name
 // A DSN tells a Sentry SDK where to send events so the events are associated with the correct project.
@@ -12,8 +12,8 @@ const SentryDsn =
 
 // Available custom tags as enum-like object.
 export const SentryTags = {
-  Version: 'cozy-version',
-  Instance: 'cozy-instance'
+  Instance: 'cozy-instance',
+  Version: 'cozy-version'
 }
 
 // Runtime initialisation.
@@ -30,8 +30,9 @@ Sentry.setTag(SentryTags.Version, version)
 // Public interface as functions.
 export const withSentry = Sentry.wrap
 
-export const setSentryTag = (tag, value) => Sentry.setTag(tag, value)
+export const setSentryTag = (tag: string, value: string): void =>
+  Sentry.setTag(tag, value)
 
-export const logToSentry = error => {
+export const logToSentry = (error: unknown): void => {
   Sentry.captureException(error)
 }
