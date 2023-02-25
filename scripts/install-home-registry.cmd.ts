71ac2ba46636cee515796143609a07a8815b7adf
Antonin Cezard <acezard@gmail.com>
2023-02-25 09:31:30 +0100

    fix: update install home script
    It used the 'iOS' dir, but the correct casing is 'ios'.
    It seems it still worked for some reason, but it's best to be safe

--- scripts/install-home-registry.cmd.ts
+++ scripts/install-home-registry.cmd.ts
@@ -4,6 +4,6 @@ void installHomeRegistry(
   'https://apps-registry.cozycloud.cc/registry/home/stable/latest',
   [
     'android/app/src/main/assets/cozy-home/build',
-    'iOS/assets/resources/cozy-home/build'
+    'ios/assets/resources/cozy-home/build'
   ]
 )
