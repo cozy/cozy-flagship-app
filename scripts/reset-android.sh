b0bd4ab51d56305f77ccee511127b903026be09d
Antonin Cezard <acezard@gmail.com>
2023-03-03 16:58:05 +0100

    Update reset-android.sh

--- scripts/reset-android.sh
+++ scripts/reset-android.sh
@@ -1,6 +1,7 @@
 rm -rf $HOME/.gradle/caches/
 cd android && ./gradlew clean && ./gradlew cleanBuildCache
 rm -rf app/src/main/assets/cozy-home
+rm -rf app/build
 cd ..
 rm -rf node_modules
 yarn cache clean --force
