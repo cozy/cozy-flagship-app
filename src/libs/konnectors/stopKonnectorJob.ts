154f538dfb377ba2c0f96e3d5af65bef22bb7d70
doubleface <christophe@cozycloud.cc>
2023-03-07 18:45:52 +0100

    feat: Remove the stop job service trigger
    Since the job is already stopped

--- src/libs/konnectors/stopKonnectorJob.ts
+++ src/libs/konnectors/stopKonnectorJob.ts
@@ -1,6 +1,6 @@
 import Minilog from '@cozy/minilog'
 
-import CozyClient from 'cozy-client'
+import CozyClient, { Q } from 'cozy-client'
 
 import { TIMEOUT_KONNECTOR_ERROR } from '/libs/Launcher'
 
@@ -11,19 +11,51 @@ export const stopKonnectorJob = async (
   jobId: string
 ): Promise<void> => {
   try {
-    await client.save({
-      _type: 'io.cozy.jobs',
-      _id: jobId,
-      _rev: true, // to force an update on Job collection
-      worker: 'client',
-      attributes: {
-        state: 'errored',
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        error: TIMEOUT_KONNECTOR_ERROR
-      }
-    })
+    await stopJob(client, jobId)
+    await findJobStopTriggerAndRemove(client, jobId)
   } catch (e) {
     log.error('Error while trying to stop job ${jobId}')
     log.error(e)
+  }
+}
+
+/**
+ * Mark the given job as "context deadline exceeded"
+ */
+const stopJob = (client: CozyClient, jobId: string): Promise<unknown> => {
+  return client.save({
+    _type: 'io.cozy.jobs',
+    _id: jobId,
+    _rev: true, // to force an update on Job collection
+    worker: 'client',
+    attributes: {
+      state: 'errored',
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      error: TIMEOUT_KONNECTOR_ERROR
+    }
+  })
+}
+
+/**
+ * Remove the @at trigger which will mark the given job as context deadline exceeded since this work is already done
+ */
+const findJobStopTriggerAndRemove = async (
+  client: CozyClient,
+  jobId: string
+): Promise<void> => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { data: triggers } = await client.query(
+    Q('io.cozy.triggers').where({
+      worker: 'service',
+      type: '@at',
+      'message.fields.cliskJobId': jobId
+    })
+  )
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  if (triggers.length > 0) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+    await client.destroy(triggers[0])
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access
+    log.info(`Removed a job stop trigger : ${triggers[0]._id}`)
   }
 }
