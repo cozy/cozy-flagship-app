2d88884340d32ca2999e14396a70943f22aaf25a
doubleface <christophe@cozycloud.cc>
2023-03-09 15:03:45 +0100

    test: Add launcher test
    Test that Launcher.ensureAccountTriggerAndLaunch does not hide start
    context parameters

---
+++ src/libs/Launcher.spec.js
@@ -0,0 +1,36 @@
+import Launcher from './Launcher'
+
+jest.mock('./folder')
+
+describe('Launcher', () => {
+  describe('ensureAccountTriggerAndLaunch', () => {
+    it('should not remove startContext attributes', async () => {
+      const launcher = new Launcher()
+      const account = {
+        _id: 'testaccount'
+      }
+      const trigger = { _id: 'testtrigger' }
+      const job = { _id: 'testjob' }
+      const client = {}
+      const konnector = {}
+      launcher.ensureAccountName = jest.fn().mockResolvedValue(account)
+      launcher.setStartContext({
+        client,
+        konnector,
+        account,
+        trigger,
+        job,
+        manifest: { name: 'konnector' }
+      })
+      await launcher.ensureAccountTriggerAndLaunch()
+      expect(launcher.getStartContext()).toStrictEqual({
+        account,
+        trigger,
+        job,
+        client,
+        konnector,
+        manifest: { name: 'konnector' }
+      })
+    })
+  })
+})
