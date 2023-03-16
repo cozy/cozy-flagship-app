06a477b4adb7618394af7a8dc065300bf330e103
doubleface <christophe@cozycloud.cc>
2023-03-16 15:25:29 +0100

    feat: Display konnector post-me logs when enableKonnectorPostMeLogger
    is set in dev config.
    
    Now that we have timer logs, we can have a view of most of exchanges
    between pilot, worker and launcher and it may be enough most of the time

--- src/constants/dev-config.ts
+++ src/constants/dev-config.ts
@@ -7,6 +7,7 @@ export const devConfig = {
   disableGetIndex: false, // Bypass HTTPServer HTML generation which will fallback to local stack, useful for webpack-dev-server
   enableLocalSentry: false, // Be warned that it will send actual logs to Sentry on the "test" environment, use sparingly
   enableReduxLogger: false, // Outputs to console every Redux action with payload, prev and next state
+  enableKonnectorPostMeLogger: false, // Outputs every post-me exchanges between launcher and pilot and worker
   forceHideSplashScreen: false, // Hide react-native splash screen renders, useful for debugging in case of a webview crash
   forceOffline: false, // Force offline mode by overwriting the NetInfo module and returning a fake offline state,
   ignoreLogBox: false, // Hide react-native LogBox renders but still display logs to the console,
