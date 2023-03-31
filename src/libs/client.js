1b6c04202d4fa51bd536890bb24b71391a21c6df
Ldoppea <yannick.chiron@gmail.com>
2023-03-31 17:20:11 +0200

    feat: Add OIDC support to LoginScreen and ClouderyView
    Cozy Cloudery will implement OIDC login and onboarding using our
    partners OIDC systems
    
    ClouderyView now intercepts navigation to URLs containing `oidc=true`
    and then starts an OIDC process to the given URL using an InAppBrowser
    
    Two scenario are implemented following the InAppBrowser OIDC process:
    one for login an existing user, and one for onboarding a non-existing
    user

--- src/libs/client.js
+++ src/libs/client.js
@@ -464,3 +464,48 @@ export const removeNotificationDeviceToken = async client => {
 
   await saveClient(client)
 }
+
+export const connectOidcClient = async (client, oidcCode) => {
+  const stackClient = client.getStackClient()
+
+  let oauthOptions = stackClient.oauthOptions
+  const data = {
+    code: oidcCode,
+    client_id: oauthOptions.clientID,
+    client_secret: oauthOptions.clientSecret,
+    scope: '*'
+  }
+
+  const {
+    two_factor_token: twoFactorToken,
+    session_code: sessionCode,
+    ...token
+  } = await stackClient.fetchJSON('POST', '/oidc/access_token', data)
+
+  const need2FA = twoFactorToken !== undefined
+
+  if (need2FA) {
+    return {
+      client,
+      state: STATE_2FA_NEEDED,
+      twoFactorToken: twoFactorToken
+    }
+  }
+
+  const needFlagshipVerification = sessionCode !== undefined
+
+  if (needFlagshipVerification) {
+    return {
+      client: client,
+      state: STATE_AUTHORIZE_NEEDED,
+      sessionCode: sessionCode
+    }
+  }
+
+  stackClient.setToken(token)
+
+  return {
+    client: client,
+    state: STATE_CONNECTED
+  }
+}
