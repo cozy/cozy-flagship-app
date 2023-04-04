59917935296f4ea120bffcdbd3748a0cd4ece933
Ldoppea <yannick.chiron@gmail.com>
2023-04-04 17:08:31 +0200

    feat: Intercept OnboardedRedirection on Onboarding from email scenario
    Until now the ClouderyCreateInstanceView only had to handle redirection
    to `https://claude.mycozy.cloud/?registerToken=SOME_TOKEN` like URLs
    
    With the new OIDC Onboarding scenario, after the InAppBrowser OIDC
    flow, if the user change their email, then the Cloudery will send an
    email and the onboarding scenario should continue through
    ClouderyCreateInstanceView
    
    With that scenario, we can now have a new `onboardedRedirection` in
    the intercepted URL. Its new form should be
    `https://claude.mycozy.cloud/?onboarding=true&redirection=mespapiers&registerToken=SOME_TOKEN`
    (note the new `onboarding=true` param that allow to verify we are
    handling a correct url format)

--- src/libs/functions/getOnboardingDataFromRequest.js
+++ src/libs/functions/getOnboardingDataFromRequest.js
@@ -5,13 +5,21 @@ import strings from '/constants/strings.json'
 const getInstanceAndRegisterToken = uri => {
   const url = new URL(window.decodeURIComponent(uri))
 
+  const isOnboarding = url.searchParams.get('onboarding')
+
+  if (!isOnboarding) {
+    return null
+  }
+
   const registerToken = url.searchParams.get(strings.registerToken)
+  const onboardedRedirection = url.searchParams.get('redirection')
 
   const fqdn = url.host
 
   return {
     fqdn,
-    registerToken
+    registerToken,
+    onboardedRedirection
   }
 }
 
