ee1ccbbbd80fc1c8dc9a76ea7adadb037fe6d8a8
Ldoppea <yannick.chiron@gmail.com>
2023-04-06 11:23:18 +0200

    refactor: Convert keyboardHelper to Typescript

--- src/libs/functions/keyboardHelper.ts
+++ src/libs/functions/keyboardHelper.ts
@@ -1,10 +1,18 @@
 import { NativeModules, Platform } from 'react-native'
+import type { WebView } from 'react-native-webview'
 
 import log from 'cozy-logger'
 
-const Keyboard = NativeModules.Keyboard
+interface NativeKeyboard {
+  forceKeyboard: () => void
+}
 
-export const setFocusOnWebviewField = (webview, fieldId) => {
+const Keyboard = NativeModules.Keyboard as NativeKeyboard
+
+export const setFocusOnWebviewField = (
+  webview: WebView,
+  fieldId: string
+): void => {
   try {
     if (Platform.OS === 'android') {
       webview.requestFocus()
