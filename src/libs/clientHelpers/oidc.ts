601d73e361f6438c2a71b892505aea70a9b521e5
Ldoppea <yannick.chiron@gmail.com>
2023-04-10 19:37:50 +0200

    refactor: Extract OIDC functions from client.js

---
+++ src/libs/clientHelpers/oidc.ts
@@ -0,0 +1,61 @@
+import type CozyClient from 'cozy-client'
+
+import {
+  ConnectClientResult,
+  FetchAccessTokenResult,
+  STATE_2FA_NEEDED,
+  STATE_AUTHORIZE_NEEDED,
+  STATE_CONNECTED
+} from '/libs/clientHelpers/types'
+
+export const connectOidcClient = async (
+  client: CozyClient,
+  oidcCode: string
+): Promise<ConnectClientResult> => {
+  const stackClient = client.getStackClient()
+
+  const oauthOptions = stackClient.oauthOptions
+  const data = {
+    code: oidcCode,
+    client_id: oauthOptions.clientID,
+    client_secret: oauthOptions.clientSecret,
+    scope: '*'
+  }
+
+  const {
+    two_factor_token: twoFactorToken,
+    session_code: sessionCode,
+    ...token
+  } = await stackClient.fetchJSON<FetchAccessTokenResult>(
+    'POST',
+    '/oidc/access_token',
+    data
+  )
+
+  const need2FA = twoFactorToken !== undefined
+
+  if (need2FA) {
+    return {
+      client,
+      state: STATE_2FA_NEEDED,
+      twoFactorToken: twoFactorToken
+    }
+  }
+
+  const needFlagshipVerification = sessionCode !== undefined
+
+  if (needFlagshipVerification) {
+    return {
+      client: client,
+      state: STATE_AUTHORIZE_NEEDED,
+      sessionCode: sessionCode
+    }
+  }
+
+  stackClient.setToken(token)
+
+  return {
+    client: client,
+    state: STATE_CONNECTED
+  }
+}
