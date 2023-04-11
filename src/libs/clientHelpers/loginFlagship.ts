987ef7a99565defaf3f8f3675c9b5c78eaae0c2b
Ldoppea <yannick.chiron@gmail.com>
2023-04-11 12:10:36 +0200

    refactor: Convert LoginFlagship.js file to Typescript

---
+++ src/libs/clientHelpers/loginFlagship.ts
@@ -0,0 +1,100 @@
+import type CozyClient from 'cozy-client'
+import type {
+  LoginFlagship2faNeededResult,
+  LoginFlagshipResult as CozyClientLoginFlagshipResult,
+  LoginFlagshipVerificationNeededResult
+} from 'cozy-client'
+
+import { isFetchError } from '/libs/clientHelpers/types'
+import type {
+  LoginData,
+  TwoFactorAuthenticationData
+} from '/screens/login/components/types'
+
+interface LoginFlagshipParams {
+  client: CozyClient
+  loginData: LoginData
+  twoFactorAuthenticationData?: TwoFactorAuthenticationData
+}
+
+interface LoginFlagshipInvalidPasswordResult {
+  invalidPassword: true
+}
+
+type LoginFlagshipResult =
+  | CozyClientLoginFlagshipResult
+  | LoginFlagshipInvalidPasswordResult
+
+/**
+ * Try to login cozy-client using Flagship dedicated route
+ *
+ * Errors are handled to detect when 2FA is needed and when password is invalid
+ *
+ * @param {object} param
+ * @param {object} param.client - the CozyClient instance that will be authenticated through OAuth
+ * @param {object} param.loginData - login data containing hashed password
+ * @param {object} [param.twoFactorAuthenticationData] - the 2FA data containing a token and a passcode
+ * @returns {LoginFlagshipResult} The query result with session_code, or 2FA token, or invalid password error
+ * @throws
+ */
+export const loginFlagship = async ({
+  client,
+  loginData,
+  twoFactorAuthenticationData = undefined
+}: LoginFlagshipParams): Promise<LoginFlagshipResult> => {
+  const stackClient = client.getStackClient()
+
+  try {
+    const loginResult = await stackClient.loginFlagship({
+      passwordHash: loginData.passwordHash,
+      twoFactorToken: twoFactorAuthenticationData
+        ? twoFactorAuthenticationData.token
+        : undefined,
+      twoFactorPasscode: twoFactorAuthenticationData
+        ? twoFactorAuthenticationData.passcode
+        : undefined
+    })
+
+    return loginResult
+  } catch (e: unknown) {
+    if (!isFetchError(e)) {
+      throw e
+    }
+
+    if (e.status === 401) {
+      if (e.reason?.two_factor_token) {
+        return {
+          two_factor_token: e.reason.two_factor_token
+        }
+      } else {
+        return {
+          invalidPassword: true
+        }
+      }
+    } else if (e.status === 403 && twoFactorAuthenticationData) {
+      return {
+        two_factor_token: twoFactorAuthenticationData.token
+      }
+    } else {
+      throw e
+    }
+  }
+}
+
+export const isInvalidPasswordResult = (
+  result: LoginFlagshipResult
+): result is LoginFlagshipInvalidPasswordResult => {
+  return 'invalidPassword' in result
+}
+
+export const is2faNeededResult = (
+  result: LoginFlagshipResult
+): result is LoginFlagship2faNeededResult => {
+  return 'two_factor_token' in result && result.two_factor_token !== undefined
+}
+
+export const isFlagshipVerificationNeededResult = (
+  result: LoginFlagshipResult
+): result is LoginFlagshipVerificationNeededResult => {
+  return 'session_code' in result && result.session_code !== undefined
+}
