322885b69ba3f64b83cb2252f586d54533761832
Ldoppea <yannick.chiron@gmail.com>
2023-04-12 17:49:02 +0200

    feat: Handle password based 2FA on Magic Link login scenario
    On Magic Link scenario, if the user enabled 2FA, then then 2FA should
    be done using the user's password instead of the usual code sent by
    email
    
    This choice has been made because the Magic Link scenario already uses
    the email channel, so a 2FA code sent through the same channel would
    add no benefits to security. So instead we ask to the user for their
    password
    
    To make this possible, we will have to modify cozy-settings so the 2FA
    feature requires a password to be set on the Cozy

--- src/libs/clientHelpers/magicLink.ts
+++ src/libs/clientHelpers/magicLink.ts
@@ -1,5 +1,4 @@
 import type CozyClient from 'cozy-client'
-import type { LoginFlagshipResult } from 'cozy-client'
 
 import { authorizeClient } from '/libs/clientHelpers/authorizeClient'
 import { createClient } from '/libs/clientHelpers/createClient'
@@ -10,38 +9,93 @@ import {
 import {
   CozyClientCreationContext,
   is2faNeededResult,
+  is2faPasswordNeededResult,
   isAccessToken,
+  isFetchError,
   isFlagshipVerificationNeededResult,
-  STATE_2FA_NEEDED,
+  isInvalidPasswordResult,
+  LoginFlagshipResult,
+  STATE_2FA_PASSWORD_NEEDED,
   STATE_AUTHORIZE_NEEDED,
-  STATE_CONNECTED
+  STATE_CONNECTED,
+  STATE_INVALID_PASSWORD
 } from '/libs/clientHelpers/types'
+import { translation } from '/locales'
+
+const ERROR_2FA_PASSWORD_NEEDED =
+  'passphrase is required as second authentication factor'
+
+const ERROR_INVALID_MAGIC_CODE = 'invalid magic code'
+
+const loginMagicLink = async (
+  client: CozyClient,
+  magicCode: string,
+  passwordHash?: string
+): Promise<LoginFlagshipResult> => {
+  const stackClient = client.getStackClient()
+
+  try {
+    const oauthOptions = stackClient.oauthOptions
+    const data = {
+      magic_code: magicCode,
+      passphrase: passwordHash,
+      client_id: oauthOptions.clientID,
+      client_secret: oauthOptions.clientSecret,
+      scope: '*'
+    }
+
+    const loginResult = await stackClient.fetchJSON<LoginFlagshipResult>(
+      'POST',
+      '/auth/magic_link/flagship',
+      data
+    )
+
+    return loginResult
+  } catch (e: unknown) {
+    if (!isFetchError(e)) {
+      throw e
+    }
+
+    if (e.status === 401) {
+      if (e.reason?.error === ERROR_2FA_PASSWORD_NEEDED) {
+        return {
+          twoFactorPasswordNeeded: true
+        }
+      } else if (e.reason?.error === ERROR_INVALID_MAGIC_CODE) {
+        throw new Error(translation.screens.login.invalidMagicCode, e)
+      } else {
+        throw new Error('Error while calling loginMagicLink', e)
+      }
+    } else {
+      throw e
+    }
+  }
+}
 
 export const connectMagicLinkClient = async (
   client: CozyClient,
-  magicCode: string
+  magicCode: string,
+  passwordHash?: string
 ): Promise<CozyClientCreationContext> => {
   const stackClient = client.getStackClient()
 
-  const oauthOptions = stackClient.oauthOptions
-  const data = {
-    magic_code: magicCode,
-    client_id: oauthOptions.clientID,
-    client_secret: oauthOptions.clientSecret,
-    scope: '*'
-  }
+  const result = await loginMagicLink(client, magicCode, passwordHash)
 
-  const result = await stackClient.fetchJSON<LoginFlagshipResult>(
-    'POST',
-    '/auth/magic_link/flagship',
-    data
-  )
-
-  if (is2faNeededResult(result)) {
+  if (isInvalidPasswordResult(result)) {
     return {
       client,
-      state: STATE_2FA_NEEDED,
-      twoFactorToken: result.two_factor_token
+      state: STATE_INVALID_PASSWORD
+    }
+  }
+
+  if (is2faNeededResult(result)) {
+    throw new Error('Magic login should never return is2faNeededResult result')
+  }
+
+  if (is2faPasswordNeededResult(result)) {
+    return {
+      client,
+      state: STATE_2FA_PASSWORD_NEEDED
     }
   }
 
