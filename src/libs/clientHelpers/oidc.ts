aa3ff3da6194e43ee40ffdf04b2775a8ff758f1a
Ldoppea <yannick.chiron@gmail.com>
2023-04-13 17:26:36 +0200

    feat: Handle email based 2FA on OIDC login scenario
    On OIDC scenario, if the user enabled 2FA, then then 2FA should be done
    like in the non-OIDC scenario (email based 2FA)
    
    However the `continueOauth` method should be adapted to call the
    correct OIDC endpoint in that scenario

--- src/libs/clientHelpers/oidc.ts
+++ src/libs/clientHelpers/oidc.ts
@@ -1,34 +1,85 @@
 import type CozyClient from 'cozy-client'
-import type { LoginFlagshipResult } from 'cozy-client'
 
 import {
   CozyClientCreationContext,
   is2faNeededResult,
+  is2faPasswordNeededResult,
+  isFetchError,
   isFlagshipVerificationNeededResult,
+  isInvalidPasswordResult,
+  LoginFlagshipResult,
   STATE_2FA_NEEDED,
   STATE_AUTHORIZE_NEEDED,
   STATE_CONNECTED
 } from '/libs/clientHelpers/types'
+import { TwoFactorAuthenticationData } from '/screens/login/components/types'
+
+const loginOidc = async (
+  client: CozyClient,
+  oidcCode: string,
+  twoFactorAuthenticationData?: TwoFactorAuthenticationData
+): Promise<LoginFlagshipResult> => {
+  const stackClient = client.getStackClient()
+
+  try {
+    const oauthOptions = stackClient.oauthOptions
+    const data = {
+      code: oidcCode,
+      client_id: oauthOptions.clientID,
+      client_secret: oauthOptions.clientSecret,
+      scope: '*',
+      two_factor_token: twoFactorAuthenticationData
+        ? twoFactorAuthenticationData.token
+        : undefined,
+      two_factor_passcode: twoFactorAuthenticationData
+        ? twoFactorAuthenticationData.passcode
+        : undefined
+    }
+
+    const loginResult = await stackClient.fetchJSON<LoginFlagshipResult>(
+      'POST',
+      '/oidc/access_token',
+      data
+    )
+
+    return loginResult
+  } catch (e: unknown) {
+    if (!isFetchError(e)) {
+      throw e
+    }
+
+    if (e.status === 401) {
+      if (e.reason?.two_factor_token) {
+        return {
+          two_factor_token: e.reason.two_factor_token
+        }
+      } else {
+        return {
+          invalidPassword: true
+        }
+      }
+    } else if (e.status === 403 && twoFactorAuthenticationData) {
+      return {
+        two_factor_token: twoFactorAuthenticationData.token
+      }
+    } else {
+      throw e
+    }
+  }
+}
 
 export const connectOidcClient = async (
   client: CozyClient,
-  oidcCode: string
+  oidcCode: string,
+  twoFactorAuthenticationData?: TwoFactorAuthenticationData
 ): Promise<CozyClientCreationContext> => {
-  const stackClient = client.getStackClient()
+  const result = await loginOidc(client, oidcCode, twoFactorAuthenticationData)
 
-  const oauthOptions = stackClient.oauthOptions
-  const data = {
-    code: oidcCode,
-    client_id: oauthOptions.clientID,
-    client_secret: oauthOptions.clientSecret,
-    scope: '*'
+  if (isInvalidPasswordResult(result)) {
+    throw new Error(
+      'Oidc login should never return isInvalidPasswordResult result'
+    )
   }
-
-  const result = await stackClient.fetchJSON<LoginFlagshipResult>(
-    'POST',
-    '/oidc/access_token',
-    data
-  )
 
   if (is2faNeededResult(result)) {
     return {
@@ -36,6 +87,12 @@ export const connectOidcClient = async (
       state: STATE_2FA_NEEDED,
       twoFactorToken: result.two_factor_token
     }
+  }
+
+  if (is2faPasswordNeededResult(result)) {
+    throw new Error(
+      'Oidc login should never return is2faPasswordNeededResult result'
+    )
   }
 
   if (isFlagshipVerificationNeededResult(result)) {
@@ -46,6 +103,7 @@ export const connectOidcClient = async (
     }
   }
 
+  const stackClient = client.getStackClient()
   stackClient.setToken(result)
 
   return {
