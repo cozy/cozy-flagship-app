2778a18a7abce260cf678e7efad209097e43eb6e
Théo Poizat <hello@zatteo.com>
2023-04-21 15:20:02 +0200

    fix: Check if icon is the same before changing it
    react-native-change-icon changes icon in iOS at the
    beginning of the app, even if the icon is the same as the default
    one. Now we check before changing it. It was not possible to check before
    because I did not configured properly activities on Android and
    getIcon method was crashing. But this was fix in a previous commit.

--- src/libs/icon/config.ts
+++ src/libs/icon/config.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_ICON = 'cozy'
 export const ALLOWED_ICONS = ['mespapiers']
+export const DEFAULT_VALUE = 'default'
--- src/libs/icon/icon.ts
+++ src/libs/icon/icon.ts
@@ -1,9 +1,27 @@
-import { changeIcon as RNChangeIcon } from 'react-native-change-icon'
+import {
+  changeIcon as RNChangeIcon,
+  getIcon as RNGetIcon
+} from 'react-native-change-icon'
 
-import { DEFAULT_ICON, ALLOWED_ICONS } from './config'
+import { DEFAULT_ICON, ALLOWED_ICONS, DEFAULT_VALUE } from './config'
+
+// Our default icon is called 'cozy' but react-native-change-icon return 'default' if default icon is selected...
+const isSameIcon = (firstIconName: string, secondIconName: string): boolean => {
+  return (
+    firstIconName === secondIconName ||
+    (firstIconName === DEFAULT_VALUE && secondIconName === DEFAULT_ICON) ||
+    (firstIconName === DEFAULT_ICON && secondIconName === DEFAULT_VALUE)
+  )
+}
 
 export const changeIcon = async (slug: string): Promise<string> => {
   const iconName = ALLOWED_ICONS.includes(slug) ? slug : DEFAULT_ICON
+
+  const currentIconName = await RNGetIcon()
+
+  if (isSameIcon(iconName, currentIconName)) {
+    return iconName
+  }
 
   try {
     return await RNChangeIcon(iconName)
