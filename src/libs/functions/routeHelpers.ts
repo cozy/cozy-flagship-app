74c8c984823a68a10e6304bb5fb53eb7b52d5192
Ldoppea <yannick.chiron@gmail.com>
2023-04-27 13:27:47 +0200

    fix: Prevent initial route params to be reused on HomeView
    When opening a konnector from cozy-store, we want the HomeView to open
    the expected konnector
    
    Current router implementation has a navigation bug that re-apply the
    route's initial params every time we navigate to `routes.home`
    
    This would prevent the queried konnector to be displayed because in
    that scenario the HomeView initial param would override the konnector's
    URI with the cozy-home initial URI
    
    Until we fix this navigation bug, we want to secure the way HomeView
    handle initial params so they would never be re-applied again after
    first handling
    
    If the param re-appear on the router params, then we want a warning to
    be displayed on the debug terminal to remind us to fix the root issue

--- src/libs/functions/routeHelpers.ts
+++ src/libs/functions/routeHelpers.ts
@@ -1,9 +1,13 @@
+import Minilog from '@cozy/minilog'
 import {
   NavigationProp,
   ParamListBase,
   RouteProp
 } from '@react-navigation/native'
 import { get } from 'lodash'
+import { useEffect, useState } from 'react'
+
+const log = Minilog('routeHelpers')
 
 /**
  * Retrieve the specified route parameter and remove it from the navigation state
@@ -24,4 +28,55 @@ export const consumeRouteParameter = <T>(
   }
 
   return param
+}
+
+interface UseClouderyUrlHook<T> {
+  consume: () => T | undefined
+}
+
+/**
+ * Hook that return the specified route parameter and ensure it is read only one single time
+ *
+ * @param paramName - Name of the parameter to retrieve
+ * @param route - Application's route
+ * @param navigation - Application's navigation
+ * @returns an initial parameter to be `.consume()`
+ */
+export const useInitialParam = <T>(
+  paramName: string,
+  route: RouteProp<ParamListBase>,
+  navigation: NavigationProp<ParamListBase>
+): UseClouderyUrlHook<T> => {
+  const [paramValue, setParamValue] = useState<T | undefined>(undefined)
+  const [handledInitialParams, setHandledInitialParams] = useState(false)
+
+  const consume = (): T | undefined => {
+    const value = paramValue
+
+    setParamValue(undefined)
+
+    return value
+  }
+
+  useEffect(
+    function checkInitialParam() {
+      const value = consumeRouteParameter<T>(paramName, route, navigation)
+
+      if (handledInitialParams) {
+        if (value) {
+          log.warn(
+            `🚨 useInitialParam detected new value for already consumed ${paramName} param, this should not happens, please verify your Router initialization`
+          )
+        }
+        return
+      }
+
+      setParamValue(value)
+
+      setHandledInitialParams(true)
+    },
+    [handledInitialParams, navigation, paramName, paramValue, route]
+  )
+
+  return { consume }
 }
