77d9df41ff9dda30c3cb7c936e4a4570b6d5f104
doubleface <christophe@cozycloud.cc>
2023-05-05 12:03:19 +0200

    feat: Allow to run clisk konnector without trigger

--- src/screens/konnectors/CliskDevView.jsx
+++ src/screens/konnectors/CliskDevView.jsx
@@ -8,52 +8,70 @@ const CliskDevView = ({ setLauncherContext }) => {
   const client = useClient()
 
   const [triggers, setTriggers] = useState([])
+  const [konnectors, setKonnectors] = useState([])
   const [selected, setSelected] = useState(null)
 
   const handleRun = async () => {
-    const triggerId = selected
-    const { data: trigger } = await client.query(
-      Q('io.cozy.triggers').getById(triggerId)
-    )
-    const slug = trigger?.message?.konnector
-    const { data: konnector } = await client.query(
-      Q('io.cozy.konnectors').getById('io.cozy.konnectors/' + slug)
-    )
-    const accountId = trigger?.message?.account
-    const { data: account } = await client.query(
-      Q('io.cozy.accounts').getById(accountId)
-    )
+    if (selected?._type === 'io.cozy.triggers') {
+      const triggerId = selected._id
+      const { data: trigger } = await client.query(
+        Q('io.cozy.triggers').getById(triggerId)
+      )
+      const slug = trigger?.message?.konnector
+      const { data: konnector } = await client.query(
+        Q('io.cozy.konnectors').getById('io.cozy.konnectors/' + slug)
+      )
+      const accountId = trigger?.message?.account
+      const { data: account } = await client.query(
+        Q('io.cozy.accounts').getById(accountId)
+      )
 
-    setLauncherContext({
-      state: 'launch',
-      value: { konnector, account, trigger: triggers[0], DEBUG: true }
-    })
+      setLauncherContext({
+        state: 'launch',
+        value: { konnector, account, trigger: triggers[0], DEBUG: true }
+      })
+    } else if (selected?._type === 'io.cozy.konnectors') {
+      setLauncherContext({
+        state: 'launch',
+        value: { konnector: selected, DEBUG: true }
+      })
+    }
   }
 
   useEffect(() => {
     const doEffect = async () => {
-      const { data } = await client.query(
+      const { data: clientTriggers } = await client.query(
         Q('io.cozy.triggers').where({
           type: '@client'
         })
       )
+      const triggerSlugs = clientTriggers.map(t => t.message.konnector)
+      const { data: clientKonnectors } = await client.query(
+        Q('io.cozy.konnectors').where({
+          clientSide: true
+        })
+      )
+      const filteredKonnectors = clientKonnectors.filter(
+        k => !triggerSlugs.includes(k.slug)
+      )
 
-      return data
+      return { clientTriggers, clientKonnectors: filteredKonnectors }
     }
     doEffect()
-      .then(data => {
-        return setTriggers(data)
+      .then(({ clientTriggers, clientKonnectors }) => {
+        setKonnectors(clientKonnectors)
+        return setTriggers(clientTriggers)
       })
       .catch(err => {
         console.error('doEffect error : ', err.message)
       })
   }, [client])
 
-  const Item = ({ trigger }) => {
-    const fontWeight = trigger._id === selected ? '800' : '400'
+  const TriggerItem = ({ trigger }) => {
+    const fontWeight = trigger._id === selected?._id ? '800' : '400'
     return (
       <TouchableOpacity
-        onPress={() => setSelected(trigger._id)}
+        onPress={() => setSelected(trigger)}
         style={{ padding: 10 }}
       >
         <Text style={{ fontSize: 20, fontWeight }}>
@@ -65,13 +83,31 @@ const CliskDevView = ({ setLauncherContext }) => {
     )
   }
 
-  // todo lancer l'item sé
+  const KonnectorItem = ({ konnector }) => {
+    const fontWeight = konnector._id === selected?._id ? '800' : '400'
+    return (
+      <TouchableOpacity
+        onPress={() => setSelected(konnector)}
+        style={{ padding: 10 }}
+      >
+        <Text style={{ fontSize: 20, fontWeight }}>
+          {konnector.slug + ': create'}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
 
   return (
     <View style={{ padding: 20, backGroundColor: '#6e3b6e' }}>
       <FlatList
         data={triggers}
-        renderItem={({ item }) => <Item trigger={item} key={item._id} />}
+        renderItem={({ item }) => <TriggerItem trigger={item} key={item._id} />}
+      ></FlatList>
+      <FlatList
+        data={konnectors}
+        renderItem={({ item }) => (
+          <KonnectorItem konnector={item} key={item._id} />
+        )}
       ></FlatList>
       <Button title="run" onPress={handleRun} />
     </View>
