da34b9761067641f5a21451c5c38e97520f95729
Théo Poizat <hello@zatteo.com>
2023-05-16 12:01:12 +0200

    feat: Add userPersistedStorage
    Add a wrapper to AsyncStorage supporting storage by user. So for a UserPersistedStorageKey, we can store a value for different user.
    
    Every keys are prefixed with @user_<user uri>_ so we can easily find every keys for an user.
    
    It will be used in the backup feature to keep backup config when the user log out.

---
+++ src/libs/localStore/userPersistedStorage.spec.ts
@@ -0,0 +1,73 @@
+import CozyClient from 'cozy-client'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import {
+  getUserPersistedData as initialGetUserPersistedData,
+  storeUserPersistedData as initialStoreUserPersistedData
+} from '/libs/localStore/userPersistedStorage'
+
+const getUserPersistedData = initialGetUserPersistedData as (
+  client: CozyClient,
+  name: string
+) => Promise<unknown>
+const storeUserPersistedData = initialStoreUserPersistedData as (
+  client: CozyClient,
+  name: string,
+  value: unknown
+) => Promise<unknown>
+
+describe('userPersistedStorage', () => {
+  const aliceClient = {
+    getStackClient: () => ({ uri: 'https://alice.mycozy.cloud' })
+  } as CozyClient
+
+  const aliceClientWithPort = {
+    getStackClient: () => ({ uri: 'https://alice.mycozy.cloud:8080' })
+  } as CozyClient
+
+  const bobClient = {
+    getStackClient: () => ({ uri: 'https://bob.mycozy.cloud' })
+  } as CozyClient
+
+  beforeEach(async () => {
+    await AsyncStorage.clear()
+  })
+
+  it('should store as alice and retrieve as alice', async () => {
+    const key = 'string'
+    const value = 'test'
+    await storeUserPersistedData(aliceClient, key, value)
+    const result = await getUserPersistedData(aliceClient, key)
+    expect(result).toStrictEqual(value)
+  })
+
+  it('should store as alice and not retrieve as alice with port', async () => {
+    const key = 'string'
+    const value = 'test'
+    await storeUserPersistedData(aliceClient, key, value)
+    const result = await getUserPersistedData(aliceClientWithPort, key)
+    expect(result).toStrictEqual(null)
+  })
+
+  it('should store as alice and bob and retrieve as alice and bob', async () => {
+    const aliceKey = 'string'
+    const bobKey = 'string'
+    const aliceValue = 'testAlice'
+    const bobValue = 'testAlice'
+    await storeUserPersistedData(aliceClient, aliceKey, aliceValue)
+    await storeUserPersistedData(bobClient, bobKey, bobValue)
+    const aliceResult = await getUserPersistedData(aliceClient, aliceKey)
+    const bobResult = await getUserPersistedData(bobClient, bobKey)
+    expect(aliceResult).toStrictEqual(aliceValue)
+    expect(bobResult).toStrictEqual(bobValue)
+  })
+
+  it('should store as alice and not retrieve as bob', async () => {
+    const key = 'string'
+    const value = 'test'
+    await storeUserPersistedData(aliceClient, key, value)
+    const result = await getUserPersistedData(bobClient, key)
+    expect(result).toStrictEqual(null)
+  })
+})
---
+++ src/libs/localStore/userPersistedStorage.ts
@@ -0,0 +1,73 @@
+import CozyClient, { useClient } from 'cozy-client'
+
+import { normalizeFqdn } from '/libs/functions/stringHelpers'
+
+import { storeData, getData } from './storage'
+
+export enum UserPersistedStorageKeys {
+  BackupConfig = 'AmiralAppBackupConfig'
+}
+
+interface UserPersistedStorageItems {
+  backupConfig: object
+}
+
+const formatKey = (
+  client: CozyClient,
+  name: UserPersistedStorageKeys
+): string => {
+  const rootURL = client.getStackClient().uri
+
+  const { host: fqdn } = new URL(rootURL)
+
+  const normalizedFqdn = normalizeFqdn(fqdn)
+
+  return `@user_${normalizedFqdn}_${name}`
+}
+
+export const storeUserPersistedData = async (
+  client: CozyClient | null,
+  name: UserPersistedStorageKeys,
+  value: UserPersistedStorageItems[keyof UserPersistedStorageItems]
+): Promise<void> => {
+  if (!client) return
+
+  // @ts-expect-error Keys and values are already checked here as an UserPersistedStorageKeys
+  await storeData(formatKey(client, name), value)
+}
+
+export const getUserPersistedData = async <T>(
+  client: CozyClient | null,
+  name: UserPersistedStorageKeys
+): Promise<T | null> => {
+  if (!client) return null
+
+  // @ts-expect-error Keys and values are already checked here as an UserPersistedStorageKeys
+  return await getData(formatKey(client, name))
+}
+
+interface UseUserPersistedStorage {
+  storeUserPersistedData: (
+    name: UserPersistedStorageKeys,
+    value: UserPersistedStorageItems[keyof UserPersistedStorageItems]
+  ) => Promise<void>
+  getUserPersistedData: <T>(name: UserPersistedStorageKeys) => Promise<T | null>
+}
+
+export const useUserPersistedStorage = (): UseUserPersistedStorage => {
+  const client = useClient()
+
+  const storeUserPersistedDataWrapper = async (
+    name: UserPersistedStorageKeys,
+    value: UserPersistedStorageItems[keyof UserPersistedStorageItems]
+  ): Promise<void> => storeUserPersistedData(client, name, value)
+
+  const getUserPersistedDataWrapper = async <T>(
+    name: UserPersistedStorageKeys
+  ): Promise<T | null> => getUserPersistedData(client, name)
+
+  return {
+    storeUserPersistedData: storeUserPersistedDataWrapper,
+    getUserPersistedData: getUserPersistedDataWrapper
+  }
+}
