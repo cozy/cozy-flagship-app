ef2895cd669fc95ba6ada78a8e2313c34defad14
Théo Poizat <hello@zatteo.com>
2023-05-25 21:13:01 +0200

    feat(backup): Add upload medias service

---
+++ src/app/domain/backup/services/uploadMedias.ts
@@ -0,0 +1,183 @@
+/* eslint-disable promise/always-return */
+import Minilog from '@cozy/minilog'
+import { CameraRoll } from '@react-native-camera-roll/camera-roll'
+import { Platform } from 'react-native'
+import RNBackgroundUpload, {
+  UploadOptions
+} from 'react-native-background-upload'
+import RNFileSystem from 'react-native-fs'
+
+import { setMediaAsBackuped } from '/app/domain/backup/services/manageLocalBackupConfig'
+import { Media } from '/app/domain/backup/models/Media'
+
+import type CozyClient from 'cozy-client'
+
+const log = Minilog('💿 Backup')
+
+interface UploadMediaResult {
+  success: boolean
+  data?: object
+}
+
+export const uploadMedias = async (
+  client: CozyClient,
+  mediasToUpload: Media[]
+): Promise<boolean> => {
+  for (const mediaToUpload of mediasToUpload) {
+    try {
+      const { success } = await uploadMedia(client, mediaToUpload)
+
+      if (success) {
+        log.debug(`✅ ${mediaToUpload.name} uploaded`)
+        await setMediaAsBackuped(client, mediaToUpload)
+      } else {
+        log.debug(`❌ ${mediaToUpload.name} not uploaded`)
+      }
+    } catch (e) {
+      log.debug(`❌ ${mediaToUpload.name} not uploaded`)
+      log.debug(e)
+    }
+  }
+
+  return true
+}
+
+const uploadMedia = async (
+  client: CozyClient,
+  media: Media
+): Promise<UploadMediaResult> => {
+  if (Platform.OS === 'android') {
+    return await uploadMediaAndroid(client, media)
+  } else if (Platform.OS === 'ios') {
+    return await uploadMediaIOS(client, media)
+  }
+
+  return { success: false }
+}
+
+const uploadMediaAndroid = async (
+  client: CozyClient,
+  media: Media
+): Promise<UploadMediaResult> => {
+  const filepath = media.path.replace('file://', '')
+
+  return new Promise((resolve, reject) => {
+    const options = {
+      url: getDriveUrl(client, media),
+      path: filepath,
+      method: 'POST',
+      type: 'raw',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': media.type,
+        Authorization: `Bearer ${
+          // @ts-expect-error Type issue which will be fixed in another PR
+          client.getStackClient().token.accessToken as string
+        }`
+      },
+      notification: {
+        enabled: false
+      }
+    } as UploadOptions
+
+    RNBackgroundUpload.startUpload(options)
+      .then(uploadId => {
+        RNBackgroundUpload.addListener('error', uploadId, error => {
+          reject({ success: false, data: error })
+        })
+        RNBackgroundUpload.addListener('cancelled', uploadId, data => {
+          reject({ success: false, data })
+        })
+        RNBackgroundUpload.addListener('completed', uploadId, data => {
+          if (data.responseCode === 201) {
+            resolve({ success: true, data })
+          } else {
+            reject({ success: false, data })
+          }
+        })
+      })
+      .catch(e => {
+        reject({ status: false, data: e as unknown })
+      })
+  })
+}
+
+const uploadMediaIOS = async (
+  client: CozyClient,
+  media: Media
+): Promise<UploadMediaResult> => {
+  let filepath: string
+
+  if (media.type === 'image') {
+    const data = await CameraRoll.iosGetImageDataById(media.path)
+
+    if (data.node.image.filepath === null) {
+      return { success: false }
+    }
+
+    filepath = data.node.image.filepath.replace('file://', '')
+  } else {
+    try {
+      await RNFileSystem.unlink(
+        RNFileSystem.TemporaryDirectoryPath + media.name
+      )
+    } catch {
+      // we try to remove temporary file just in case it exists
+    }
+
+    filepath = await RNFileSystem.copyAssetsVideoIOS(
+      media.path,
+      RNFileSystem.TemporaryDirectoryPath + media.name
+    )
+  }
+
+  return new Promise((resolve, reject) => {
+    RNFileSystem.uploadFiles({
+      toUrl: getDriveUrl(client, media),
+      files: [
+        {
+          name: media.name,
+          filename: media.name,
+          filetype: media.type,
+          filepath
+        }
+      ],
+      binaryStreamOnly: true,
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': media.type,
+        Authorization: `Bearer ${
+          // @ts-expect-error Type issue which will be fixed in another PR
+          client.getStackClient().token.accessToken as string
+        }`
+      }
+    })
+      .promise.then(response => {
+        if (response.statusCode == 201) {
+          resolve({ success: true, data: response })
+        } else {
+          reject({ success: false, data: response })
+        }
+      })
+      .catch(e => {
+        reject({ status: false, data: e as unknown })
+      })
+  })
+}
+
+const getDriveUrl = (client: CozyClient, media: Media): string => {
+  const createdAt = new Date(media.creationDate).toISOString()
+
+  const toURL =
+    client.getStackClient().uri +
+    '/files/io.cozy.files.root-dir' +
+    '?Name=' +
+    encodeURIComponent(media.name) +
+    '&Type=file&Tags=library&Executable=false&CreatedAt=' +
+    createdAt +
+    '&UpdatedAt=' +
+    createdAt
+
+  return toURL
+}
