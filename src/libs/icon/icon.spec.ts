f1f2acb39e80996461a88089e6c67c2f949b0a9a
Théo Poizat <hello@zatteo.com>
2023-05-31 20:21:48 +0200

    feat: Use flags to allow icon change and set default icon

--- src/libs/icon/icon.spec.ts
+++ src/libs/icon/icon.spec.ts
@@ -1,21 +1,50 @@
 import { changeIcon as RNChangeIcon } from 'react-native-change-icon'
 
+import flag from 'cozy-flags'
+
 import { changeIcon } from './icon'
 
 jest.mock('react-native-change-icon')
+jest.mock('cozy-flags')
+
+const mockFlag = flag as jest.MockedFunction<typeof flag>
 
 const mockedRNChangeIcon = RNChangeIcon as jest.MockedFunction<
   typeof RNChangeIcon
 >
 
+const ICON_CHANGE_ALLOWED_FLAGS = {
+  'flagship.icon.changeAllowed': true,
+  'flagship.icon.defaultIcon': 'cozy'
+}
+
+const ICON_CHANGE_NOT_ALLOWED_FLAGS = {
+  'flagship.icon.changeAllowed': false
+}
+
+const ICON_CHANGE_UNDEFINED_FLAGS = {
+  'flagship.icon.changeAllowed': false
+}
+
+type Flags = Record<string, boolean | string>
+
 describe('icon', () => {
+  const mockFlags = (flags: Flags): void => {
+    // @ts-expect-error flag is not correctly typed for the moment
+    mockFlag.mockImplementation(flagName => {
+      return flags[flagName]
+    })
+  }
+
   it('should change icon if icon allowed', async () => {
+    mockFlags(ICON_CHANGE_ALLOWED_FLAGS)
     await changeIcon('mespapiers')
 
     expect(mockedRNChangeIcon).toHaveBeenCalledWith('mespapiers')
   })
 
   it('should change icon with default icon if icon not allowed', async () => {
+    mockFlags(ICON_CHANGE_ALLOWED_FLAGS)
     await changeIcon('drive')
 
     expect(mockedRNChangeIcon).toHaveBeenCalledWith('cozy')
@@ -35,5 +64,19 @@ describe('icon', () => {
     })
 
     await expect(changeIcon('mespapiers')).rejects.toThrow('ICON_INVALID')
+  })
+
+  it('should not change icon if icon change not allowed', async () => {
+    mockFlags(ICON_CHANGE_NOT_ALLOWED_FLAGS)
+    await changeIcon('drive')
+
+    expect(mockedRNChangeIcon).not.toHaveBeenCalled()
+  })
+
+  it('should not change icon if icon change undefined', async () => {
+    mockFlags(ICON_CHANGE_UNDEFINED_FLAGS)
+    await changeIcon('drive')
+
+    expect(mockedRNChangeIcon).not.toHaveBeenCalled()
   })
 })
