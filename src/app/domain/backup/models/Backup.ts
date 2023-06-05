c2aadd591a6251d56c854ab08ebfa3dc62ae45d4
Théo Poizat <hello@zatteo.com>
2023-06-05 18:51:06 +0200

    feat(backup): Split backup preparation and improve data returned to front end
    prepareBackup method will be in charge
    of everything needed before doing a backup :
    - managing permissions
    - initializing backup
    - geting medias to backup
    - returning data to the user
    
    More data are returned to the front end so that the user can choose
    with full knowledge of facts if he want to do a backup or not.
    
    A backup status has been added to improve tracability for the user.

--- src/app/domain/backup/models/Backup.ts
+++ src/app/domain/backup/models/Backup.ts
@@ -1,15 +1,22 @@
-import { BackupedMedia } from '/app/domain/backup/models'
+import { Media, BackupedMedia } from '/app/domain/backup/models'
+
+type BackupStatus = 'to_do' | 'initializing' | 'ready' | 'running' | 'done'
 
 /**
  * A local backup config representing the current backup of the device
  * @member {string} remotePath
  * @member {number} lastBackupDate
  * @member {BackupedMedia[]} backupedMedias
+ * @member {object} currentBackup
  */
 export interface LocalBackupConfig {
   remotePath: string
   lastBackupDate: number
   backupedMedias: BackupedMedia[]
+  currentBackup: {
+    status: BackupStatus
+    mediasToBackup: Media[]
+  }
 }
 
 /**
@@ -27,8 +34,13 @@ export interface RemoteBackupConfig {
  * An object representing everything a front end need to know about a backup
  * @member {number} lastBackupDate
  * @member {number} backupedMediasCount
+ * @member {object} currentBackup
  */
 export interface BackupInfo {
   lastBackupDate: number
   backupedMediasCount: number
+  currentBackup: {
+    status: BackupStatus
+    mediasToBackupCount: number
+  }
 }
