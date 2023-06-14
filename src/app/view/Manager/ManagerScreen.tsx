6b609e8db8abd96c3ab1f2cbba5a9a7b53042c43
Ldoppea <yannick.chiron@gmail.com>
2023-06-14 17:37:30 +0200

    feat: Add splashscreen and overlay to Manager screen
    In previous commit we implemented a new Manager screen that is
    displayed when the app is opened with a Universal Link that targets the
    Cloudery Manager
    
    When doing so, some flickering may happen until the Manager screen load
    it's internal WebView. To prevent this we want to hide this mechanism
    behind a splashscreen
    
    An additional blue overlay is used to prevent another flickering when
    hiding the splashscreen

--- src/app/view/Manager/ManagerScreen.tsx
+++ src/app/view/Manager/ManagerScreen.tsx
@@ -1,19 +1,51 @@
 import React from 'react'
+import { StyleSheet, View } from 'react-native'
 import { WebView } from 'react-native-webview'
 
 import { ManagerScreenProps } from '/app/view/Manager/ManagerScreenTypes'
 import { useManagerScreenProps } from '/app/view/Manager/useManagerScreen'
+import { getColors } from '/ui/colors'
+
+const colors = getColors()
 
 export const ManagerScreen = (props: ManagerScreenProps): React.ReactNode => {
   const managerProps = useManagerScreenProps(props)
 
   return (
-    managerProps.display && (
-      <WebView
-        source={{ uri: managerProps.managerUrl }}
-        onShouldStartLoadWithRequest={managerProps.onShouldStartLoadWithRequest}
-        onError={managerProps.handleError}
+    <View style={styles.view}>
+      {managerProps.display && (
+        <WebView
+          source={{ uri: managerProps.managerUrl }}
+          onShouldStartLoadWithRequest={
+            managerProps.onShouldStartLoadWithRequest
+          }
+          onError={managerProps.handleError}
+          style={{
+            backgroundColor: colors.primaryColor
+          }}
+        />
+      )}
+      <View
+        style={[
+          styles.loadingOverlay,
+          {
+            backgroundColor: colors.primaryColor
+          }
+        ]}
       />
-    )
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1
+  },
+  loadingOverlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
+})
