79e17188d81838d333cc5e1594330c146e045e4b
Théo Poizat <hello@zatteo.com>
2023-06-21 11:30:45 +0200

    fix(backup): Use query instead of collection.get to get created backup folder

--- src/@types/cozy-client.d.ts
+++ src/@types/cozy-client.d.ts
@@ -93,7 +93,7 @@ declare module 'cozy-client' {
 
   export const useClient = (): CozyClient => CozyClient as CozyClient
 
-  interface FileCollectionGetResult {
+  export interface FileCollectionGetResult {
     data: {
       _id: string
       name: string
@@ -112,7 +112,6 @@ declare module 'cozy-client' {
     addReferencesTo: (references: object, dirs: object[]) => Promise<void>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     launch: (trigger: any) => any
-    get: (id: string) => Promise<FileCollectionGetResult>
   }
 
   export default class CozyClient {
