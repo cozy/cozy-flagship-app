e90e3667757a73ea139411272c00eb679c7a942c
Théo Poizat <hello@zatteo.com>
2023-06-26 15:48:58 +0200

    feat(backup): Add managePermission service
    This service allow to :
    - return what we need on backup front end (if we have permissions and
    if we can ask them)
    - request native permissions needed for backup

--- src/app/domain/backup/services/managePermissions.ts
+++ src/app/domain/backup/services/managePermissions.ts
@@ -1,40 +1,66 @@
-import { Platform, PermissionsAndroid, Permission } from 'react-native'
+import { Platform } from 'react-native'
 
-const PLATFORM_VERSION =
-  typeof Platform.Version === 'number'
-    ? Platform.Version
-    : parseInt(Platform.Version, 10)
+import {
+  checkNativePermission,
+  requestNativePermission,
+  NATIVE_PERMISSIONS,
+  NativePermissionStatus
+} from '/app/domain/nativePermissions'
 
-const ANDROID_PERMISSION =
-  PLATFORM_VERSION >= 33
-    ? ('android.permission.READ_MEDIA_IMAGES' as Permission) // PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES is only available starting RN 0.70
-    : PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE
+export const checkBackupPermissions =
+  async (): Promise<NativePermissionStatus> => {
+    if (Platform.OS === 'ios') {
+      return await checkNativePermission(NATIVE_PERMISSIONS.IOS.PHOTO_LIBRARY)
+    }
 
-export const hasMediaPermissions = async (): Promise<boolean> => {
-  if (Platform.OS === 'android') {
-    const hasPermission = await PermissionsAndroid.check(ANDROID_PERMISSION)
-    return hasPermission
+    if (Platform.OS === 'android' && Platform.Version >= 33) {
+      const imagePermission = await checkNativePermission(
+        NATIVE_PERMISSIONS.ANDROID.READ_MEDIA_IMAGES
+      )
+      const videoPermission = await checkNativePermission(
+        NATIVE_PERMISSIONS.ANDROID.READ_MEDIA_VIDEO
+      )
+
+      return {
+        granted: imagePermission.granted && videoPermission.granted,
+        canRequest: imagePermission.canRequest && videoPermission.canRequest
+      }
+    }
+
+    if (Platform.OS === 'android' && Platform.Version < 33) {
+      return await checkNativePermission(
+        NATIVE_PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE
+      )
+    }
+
+    throw new Error('Platform is not supported for backup')
   }
 
-  return true
-}
+export const requestBackupPermissions =
+  async (): Promise<NativePermissionStatus> => {
+    if (Platform.OS === 'ios') {
+      return await requestNativePermission(NATIVE_PERMISSIONS.IOS.PHOTO_LIBRARY)
+    }
 
-export const requestMediaPermissions = async (): Promise<boolean> => {
-  if (Platform.OS === 'android') {
-    const requestResult = await PermissionsAndroid.request(ANDROID_PERMISSION)
-    return requestResult === PermissionsAndroid.RESULTS.GRANTED
+    if (Platform.OS === 'android' && Platform.Version >= 33) {
+      const imagePermission = await requestNativePermission(
+        NATIVE_PERMISSIONS.ANDROID.READ_MEDIA_IMAGES
+      )
+      const videoPermission = await requestNativePermission(
+        NATIVE_PERMISSIONS.ANDROID.READ_MEDIA_VIDEO
+      )
+
+      return {
+        granted: imagePermission.granted && videoPermission.granted,
+        canRequest: imagePermission.canRequest && videoPermission.canRequest
+      }
+    }
+
+    if (Platform.OS === 'android' && Platform.Version < 33) {
+      return await requestNativePermission(
+        NATIVE_PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE
+      )
+    }
+
+    throw new Error('Platform is not supported for backup')
   }
-
-  return true
-}
-
-export const managePermissions = async (): Promise<boolean> => {
-  const hasPermission = await hasMediaPermissions()
-
-  if (hasPermission) {
-    return true
-  }
-
-  const requestResult = await requestMediaPermissions()
-  return requestResult
-}
