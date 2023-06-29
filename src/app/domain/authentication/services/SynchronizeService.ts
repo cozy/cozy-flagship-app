bbdbcf3eaba2e4a90ee2ace25beed3681674a089
Antonin Cezard <acezard@gmail.com>
2023-06-29 16:01:03 +0200

    feat: save oauthName on app bootstrap if needed
    This will update existing users still using
    the old application name in their oauth client

--- src/app/domain/authentication/services/SynchronizeService.ts
+++ src/app/domain/authentication/services/SynchronizeService.ts
@@ -1,14 +1,55 @@
 import Minilog from '@cozy/minilog'
+import { getDeviceName } from 'react-native-device-info'
 
 import type CozyClient from 'cozy-client'
+import { getErrorMessage } from 'cozy-intent'
 
-const log = Minilog('SynchronizeService')
+import { SOFTWARE_NAME } from '/libs/constants'
+import { saveClient } from '/libs/clientHelpers/persistClient'
+
+export const syncLog = Minilog('📱 SynchronizeService')
 
 export const synchronizeDevice = async (client: CozyClient): Promise<void> => {
   try {
     await client.getStackClient().fetchJSON('POST', '/settings/synchronized')
-    log.info('📱 Device synchronized')
+    syncLog.info('Device synchronized')
   } catch (error) {
-    log.warn('Error while synchronizing device', error)
+    syncLog.warn('Error while synchronizing device', error)
   }
+}
+
+export const getClientName = async (): Promise<string> =>
+  `${SOFTWARE_NAME} (${await getDeviceName()})`
+
+export const checkClientName = async (client: CozyClient): Promise<void> => {
+  try {
+    const { clientName, ...oauthOptions } = client.getStackClient().oauthOptions
+    const newClientName = await getClientName()
+
+    if (!clientName.startsWith(newClientName)) {
+      syncLog.info(`Updating client name...`, {
+        oldClientName: clientName
+      })
+
+      const response = await client.getStackClient().updateInformation({
+        ...oauthOptions,
+        clientName: newClientName
+      })
+
+      await saveClient(client)
+
+      syncLog.info(`Client name updated with "${newClientName}"`, {
+        OAuthOptions: response
+      })
+    } else {
+      syncLog.info(`Client name is up to date: "${clientName}"`)
+    }
+  } catch (error) {
+    syncLog.error('Failed to update clientName', getErrorMessage(error))
+  }
+}
+
+export const synchronizeOnInit = async (client: CozyClient): Promise<void> => {
+  await checkClientName(client)
+  await synchronizeDevice(client)
 }
