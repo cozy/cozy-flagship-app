b7cf4cdc832bb29c83e3daa1a15aed021851f4f2
Antonin Cezard <acezard@gmail.com>
2023-07-12 15:26:41 +0200

    feat: setup i18n at app launch
    Also use it in WelcomePage for smoke test

---
+++ src/locales/i18n.ts
@@ -0,0 +1,48 @@
+import i18n, { Resource } from 'i18next'
+import 'intl-pluralrules'
+import { initReactI18next } from 'react-i18next'
+import { getLocales } from 'react-native-localize'
+
+import en from './en-GB/translation.json'
+import es from './es/translation.json'
+import fr from './fr-FR/translation.json'
+
+const resources: Record<string, Resource> = {
+  en: {
+    translation: en
+  },
+  fr: {
+    translation: fr
+  },
+  es: {
+    translation: es
+  }
+}
+
+const getSupportedLanguageCode = (
+  languageCode: string
+): keyof typeof resources => {
+  return Object.prototype.hasOwnProperty.call(resources, languageCode)
+    ? languageCode
+    : 'fr'
+}
+
+const language = getSupportedLanguageCode(getLocales()[0].languageCode)
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: resources,
+    lng: language,
+    keySeparator: '.',
+    interpolation: {
+      escapeValue: false
+    }
+  })
+  .catch(error => {
+    // eslint-disable-next-line no-console
+    console.log(error)
+  })
+
+export const geti18n = (): typeof i18n => i18n
+export const getT = (): typeof i18n.t => i18n.t
