fbfba8bdf7f89e5fb332dc0b0b15bf92bfa98c68
Antonin Cezard <acezard@gmail.com>
2023-08-18 16:05:58 +0200

    feat: implement webapp routing
    temp

--- src/app/domain/sharing/models/SharingState.ts
+++ src/app/domain/sharing/models/SharingState.ts
@@ -8,14 +8,20 @@ export enum SharingIntentStatus {
 
 export enum SharingActionType {
   SetIntentStatus = 'SET_INTENT_STATUS',
-  SetFilesToUpload = 'SET_FILES_TO_UPLOAD'
+  SetFilesToUpload = 'SET_FILES_TO_UPLOAD',
+  SetRouteToUpload = 'SET_ROUTE_TO_UPLOAD'
 }
 
 export interface SharingState {
   sharingIntentStatus: SharingIntentStatus
   filesToUpload: ReceivedFile[]
+  routeToUpload?: { href: string; slug: string }
 }
 
 export type SharingAction =
   | { type: SharingActionType.SetIntentStatus; payload: SharingIntentStatus }
   | { type: SharingActionType.SetFilesToUpload; payload: ReceivedFile[] }
+  | {
+      type: SharingActionType.SetRouteToUpload
+      payload: { href: string; slug: string }
+    }
