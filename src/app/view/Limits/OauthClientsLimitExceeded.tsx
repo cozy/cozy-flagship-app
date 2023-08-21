b59aae02bfa74ef3f15a65aaf9edcb9b5168cda1
Ldoppea <yannick.chiron@gmail.com>
2023-08-21 17:26:49 +0200

    feat: Stop waiting for cozy-app when opening OAuthClientsLimitExceeded
    When the default app is different than cozy-home, then the default
    cozy-app won't open because it is blocked by the OAuth Client
    limitation mechanism so we have to manually tell the HomeView to stop
    waiting for the never coming cozy-app
    
    Related PR: #650

--- src/app/view/Limits/OauthClientsLimitExceeded.tsx
+++ src/app/view/Limits/OauthClientsLimitExceeded.tsx
@@ -1,9 +1,10 @@
 import { NavigationProp, ParamListBase } from '@react-navigation/native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
 import WebView from 'react-native-webview'
 
 import { useOAuthClientsLimitExceeded } from '/app/view/Limits/hooks/useOAuthClientsLimitExceeded'
+import { useHomeStateContext } from '/screens/home/HomeStateProvider'
 
 interface OauthClientsLimitExceededProps {
   navigation: NavigationProp<ParamListBase>
@@ -12,7 +13,18 @@ interface OauthClientsLimitExceededProps {
 export const OauthClientsLimitExceeded = ({
   navigation
 }: OauthClientsLimitExceededProps): JSX.Element | null => {
-  const { popupUrl, interceptNavigation, interceptOpenWindow } = useOAuthClientsLimitExceeded(navigation)
+  const { popupUrl, interceptNavigation, interceptOpenWindow } =
+    useOAuthClientsLimitExceeded(navigation)
+  const { setShouldWaitCozyApp, shouldWaitCozyApp } = useHomeStateContext()
+
+  useEffect(() => {
+    if (popupUrl && shouldWaitCozyApp) {
+      // When the default app is different than cozy-home, then the default cozy-app won't
+      // open because it is blocked by the OAuth Client limitation mechanism so we have to manually
+      // tell the HomeView to stop waiting for the never coming cozy-app
+      setShouldWaitCozyApp(false)
+    }
+  }, [popupUrl, shouldWaitCozyApp, setShouldWaitCozyApp])
 
   return popupUrl ? (
     <View style={styles.dialog}>
