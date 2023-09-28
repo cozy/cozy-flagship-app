704200d28d04660dcf45264398fa40d5bedf527a
Théo Poizat <hello@zatteo.com>
2023-09-28 14:57:10 +0200

    feat: Add GeolocationTrackingHeadlessTask
    This Headless task improve Android background geolocation tracking.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { AppRegistry } from 'react-native'
+import BackgroundGeolocation from 'react-native-background-geolocation'
 import 'react-native-gesture-handler'
 import 'react-native-url-polyfill/auto'
 
+import { GeolocationTrackingHeadlessTask } from './CozyGPSMemory/geolocation/services'
 import App from './src/App'
 import { name as appName } from './src/app.json'
 
 AppRegistry.registerComponent(appName, () => App)
+
+BackgroundGeolocation.registerHeadlessTask(GeolocationTrackingHeadlessTask)
