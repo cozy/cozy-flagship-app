5f9ed23dedbc80543a647c4c0fb87b7276d892da
Antonin Cezard <acezard@gmail.com>
2023-10-24 09:05:14 +0200

    fix: do not handle files if user is unlogged

--- src/app/view/OsReceive/OsReceiveProvider.spec.tsx
+++ src/app/view/OsReceive/OsReceiveProvider.spec.tsx
@@ -2,13 +2,8 @@ import { render } from '@testing-library/react-native'
 import React from 'react'
 import { Text } from 'react-native'
 
-import { useQuery } from 'cozy-client'
-
 import { OsReceiveProvider } from '/app/view/OsReceive/OsReceiveProvider'
-import { handleReceivedFiles } from '/app/domain/osReceive/services/OsReceiveData'
-import { ReceivedFile } from '/app/domain/osReceive/models/ReceivedFile'
 import { useOsReceiveState } from '/app/view/OsReceive/OsReceiveState'
-import { getRouteToUpload } from '/app/domain/osReceive/services/OsReceiveNetwork'
 
 jest.mock('/app/domain/osReceive/services/OsReceiveData')
 jest.mock('/app/domain/osReceive/services/OsReceiveNetwork')
@@ -33,26 +28,7 @@ jest.mock('@react-navigation/native', () => ({
 }))
 
 describe('OsReceiveProvider', () => {
-  beforeEach(() => {
-    // eslint-disable-next-line @typescript-eslint/no-extra-semi
-    ;(handleReceivedFiles as jest.Mock).mockClear()
-  })
-
   it('calls services and updates state correctly on mount', () => {
-    const mockReceivedFilesCallback = jest.fn()
-    ;(useQuery as jest.Mock).mockReturnValue({ data: [] })
-    ;(handleReceivedFiles as jest.Mock).mockImplementation(callback => {
-      mockReceivedFilesCallback()
-      const mockCallback = callback as (files: ReceivedFile[]) => void
-      mockCallback([{ filePath: 'test-file' } as ReceivedFile])
-    })
-    ;(getRouteToUpload as jest.Mock).mockReturnValue({
-      result: {
-        href: 'test-href',
-        slug: 'test-slug'
-      }
-    })
-
     const TestComponent = (): JSX.Element => {
       const state = useOsReceiveState()
       return <Text>{JSON.stringify(state)}</Text>
@@ -63,23 +39,5 @@ describe('OsReceiveProvider', () => {
         <TestComponent />
       </OsReceiveProvider>
     )
-
-    expect(mockReceivedFilesCallback).toHaveBeenCalledTimes(1)
-  })
-
-  it('calls cleanup functions on unmount', () => {
-    const cleanupReceivedFiles = jest.fn()
-
-    ;(handleReceivedFiles as jest.Mock).mockReturnValue(cleanupReceivedFiles)
-
-    const { unmount } = render(
-      <OsReceiveProvider>
-        <Text>Test</Text>
-      </OsReceiveProvider>
-    )
-
-    unmount()
-
-    expect(cleanupReceivedFiles).toHaveBeenCalledTimes(1)
   })
 })
