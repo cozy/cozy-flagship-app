7a4133b7f98030363baf30f395b4a7eab0bdd9d8
Antonin Cezard <acezard@gmail.com>
2023-11-21 17:40:20 +0100

    fix: remove redundaant platform check
    Will use mimeType for both ios and android
    since the  share lib is patched
    Still include failsafe

--- src/app/domain/osReceive/fixtures/iOSReceivedFile.ts
+++ src/app/domain/osReceive/fixtures/iOSReceivedFile.ts
@@ -5,7 +5,7 @@ export const iOSReceivedFileFixture = [
     fileName: 'Carte_Alan.pdf',
     filePath:
       'file:///Users/anc/Library/Developer/CoreSimulator/Devices/AE73D485-F700-46F2-900B-713E046D6B51/data/Containers/Shared/AppGroup/7B75CC01-AE21-4949-9E2E-0DC1F63D0C15/Carte_Alan.pdf',
-    mimeType: '.pdf',
+    mimeType: 'application/pdf',
     text: null,
     weblink: null
   }
@@ -18,7 +18,7 @@ export const iOSReceivedFileFixtures = [
     fileName: 'Carte_Alan.pdf',
     filePath:
       'file:///Users/toto/Library/Developer/CoreSimulator/Devices/AE73D485-F700-46F2-900B-713E046D6B51/data/Containers/Shared/AppGroup/7B75CC01-AE21-4949-9E2E-0DC1F63D0C15/Carte_Alan.pdf',
-    mimeType: '.pdf',
+    mimeType: 'application/pdf',
     text: null,
     weblink: null
   },
@@ -28,6 +28,16 @@ export const iOSReceivedFileFixtures = [
     fileName: 'Carte_Alan2.jpg',
     filePath:
       'file:///Users/toto/Library/Developer/CoreSimulator/Devices/AE73D485-F700-46F2-900B-713E046D6B51/data/Containers/Shared/AppGroup/7B75CC01-AE21-4949-9E2E-0DC1F63D0C15/Carte_Alan2.jpg',
+    mimeType: 'image/jpeg',
+    text: null,
+    weblink: null
+  },
+  {
+    contentUri: null,
+    extension: 'jpg',
+    fileName: 'Carte_Alan3.jpg',
+    filePath:
+      'file:///Users/toto/Library/Developer/CoreSimulator/Devices/AE73D485-F700-46F2-900B-713E046D6B51/data/Containers/Shared/AppGroup/7B75CC01-AE21-4949-9E2E-0DC1F63D0C15/Carte_Alan3.jpg',
     mimeType: '.jpg',
     text: null,
     weblink: null
