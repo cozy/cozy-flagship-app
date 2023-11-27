20485c740add90ab4cf292b7f5da2630e0889e61
Antonin Cezard <acezard@gmail.com>
2023-11-27 16:28:54 +0100

    feat: save file in temp directory
    Borrows a lot from `ocr`, should be refactored probably. Not sure
    if worth it because not a lot of code

--- src/libs/intents/printBase64Doc.ts
+++ src/libs/intents/printBase64Doc.ts
@@ -1,9 +1,65 @@
+import { format } from 'date-fns'
+import { Platform } from 'react-native'
+import RNFS from 'react-native-fs'
 import RNPrint from 'react-native-print'
 
 import MiniLog from 'cozy-minilog'
 
+import { getErrorMessage } from '/libs/functions/getErrorMessage'
+
 const log = MiniLog('intents:print')
 
-export const printBase64Doc = (base64?: string): void => {
-  log.info('printBase64Doc', base64, RNPrint)
+const safeDeleteFile = async (path: string): Promise<void> => {
+  try {
+    if (await RNFS.exists(path)) {
+      await RNFS.unlink(path)
+    }
+  } catch (err) {
+    log.error(
+      `Something went wront while deleting document to print: ${getErrorMessage(
+        err
+      )}`
+    )
+  }
+}
+
+const getPrintFolderPath = (): string => {
+  return `${RNFS.TemporaryDirectoryPath}/print/`
+}
+
+const getTemporaryPrintFilePath = (): string => {
+  const date = format(new Date(), 'yyyyMMddHHmmssSSS')
+  const tempFileName = `temp_print_${date}.printfile`
+
+  const pathPrefix = Platform.OS === 'android' ? 'file://' : ''
+
+  const path = `${pathPrefix}${getPrintFolderPath()}${tempFileName}`
+
+  return path
+}
+
+const ensurePrintFolder = async (): Promise<void> => {
+  await RNFS.mkdir(getPrintFolderPath())
+}
+
+export const printBase64Doc = async (base64?: string): Promise<void> => {
+  const path = getTemporaryPrintFilePath()
+
+  try {
+    if (!base64) throw new Error('No base64 provided')
+
+    await ensurePrintFolder()
+    await RNFS.writeFile(path, base64, 'base64')
+
+    await RNPrint.print({ filePath: path })
+  } catch (err) {
+    log.error(
+      `Something went wront while trying to print document: ${getErrorMessage(
+        err
+      )}`
+    )
+    return undefined
+  } finally {
+    await safeDeleteFile(path)
+  }
 }
