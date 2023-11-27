6fa335993233e579cc448f2f12bab3b48ce2c946
Antonin Cezard <acezard@gmail.com>
2023-11-27 16:07:56 +0100

    feat: Add react-native-print lib

--- src/libs/intents/printBase64Doc.ts
+++ src/libs/intents/printBase64Doc.ts
@@ -1,7 +1,9 @@
+import RNPrint from 'react-native-print'
+
 import MiniLog from 'cozy-minilog'
 
 const log = MiniLog('intents:print')
 
 export const printBase64Doc = (base64?: string): void => {
-  log.info('printBase64Doc', base64)
+  log.info('printBase64Doc', base64, RNPrint)
 }
