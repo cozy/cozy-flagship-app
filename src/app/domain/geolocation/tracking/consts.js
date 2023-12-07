712187558bc0f836ab284a3fb01abbe3a82494c5
paultranvan <paul@cozycloud.cc>
2023-12-07 12:05:18 +0100

    fix: Force `FORCE_STOP_TRACKING` for large and slow gap
    This transition is very useful to force a trip end and avoid
    to re-use a previous place that might actually be (very) far away.
    It is also useful when the device stopped for a while, and the next
    location point is far away from the previous one (typically because
    the geofence exit took a long time). Without this transition, the
    server will continue the trip, even if the device was still for
    several hours.
    
    Note it also has drawbacks, in particular for trips with pauses,
    typically by train, subway, plane, etc.
    In those cases, this transition can aggressively cut the
    trip, resulting in unexpected results, such as missing plane
    trip, or absurd subway trips because of lack of locations.
    
    That is why we rely on the speed to avoid such situations, and
    also increase what is seen as a large temporal delta, from
    30min to 60min.

--- src/app/domain/geolocation/tracking/consts.js
+++ src/app/domain/geolocation/tracking/consts.js
@@ -24,7 +24,7 @@ export const WAIT_BEFORE_STOP_MOTION_EVENT = 10 // In minutes
  * Tracking processing
  */
 export const LOW_CONFIDENCE_THRESHOLD = 0.5 // Threshold for low activity confidence
-export const LARGE_TEMPORAL_DELTA = 30 * 60 // 30min, in seconds. Shouldn't have longer breaks without siginificant motion
+export const LARGE_TEMPORAL_DELTA = 60 * 60 // 60min, in seconds. Shouldn't have longer breaks without siginificant motion
 export const MAX_TEMPORAL_DELTA = 12 * 60 * 60 // 18h, in seconds. See https://github.com/e-mission/e-mission-server/blob/f6bf89a274e6cd10353da8f17ebb327a998c788a/emission/analysis/intake/segmentation/trip_segmentation_methods/dwell_segmentation_dist_filter.py#L194
 export const MIN_SPEED_BETWEEN_DISTANT_POINTS = 0.1 // In m/s. Note the average walking speed is ~1.4 m/s
 export const MAX_DOCS_PER_BATCH = 30000 // Should be less than 10 MB. Should never reach 15 MB.
