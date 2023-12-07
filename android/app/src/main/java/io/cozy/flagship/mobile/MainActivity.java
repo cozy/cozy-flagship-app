e88ba3e16bfef18dcadf57f5f8b5738fa2bfeb88
Théo Poizat <hello@zatteo.com>
2023-12-07 18:25:41 +0100

    feat(RN0.72): Update MainActivity and MainApplication

--- android/app/src/main/java/io/cozy/flagship/mobile/MainActivity.java
+++ android/app/src/main/java/io/cozy/flagship/mobile/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.content.Intent;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
 import com.zoontek.rnbootsplash.RNBootSplash;
 import com.rnimmersivebars.ImmersiveBars;
 
@@ -21,6 +25,20 @@ public class MainActivity extends ReactActivity {
     return "CozyReactNative";
   }
 
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     boolean isDarkMode = true;
@@ -32,12 +50,12 @@ public class MainActivity extends ReactActivity {
      * We want to prevent the app from opening inside another app's task.
      * Some apps (like Google Files) will open the app inside their own task, even if the app is already running,
      * And ignore the launchMode="singleTask" in the AndroidManifest.xml.
-     * 
+     *
      * See:
      * https://stackoverflow.com/questions/57750124/launchmode-being-overriden-by-google-files-app-when-sharing-new-instance-of-a-s
      * https://github.com/facebook/react-native/issues/39553
      * https://developer.android.com/guide/components/activities/tasks-and-back-stack#IntentFlagsForTasks
-     * */ 
+     * */
     if (!isTaskRoot()) {
         Intent newIntent = new Intent(getIntent());
         newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
