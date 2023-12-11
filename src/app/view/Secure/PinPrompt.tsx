1eb53d511f6076f5f4c2c32477583b01ed706d4c
Ldoppea <yannick.chiron@gmail.com>
2023-12-11 16:50:26 +0100

    feat: Handle CozyTheme in PinPrompt view

--- src/app/view/Secure/PinPrompt.tsx
+++ src/app/view/Secure/PinPrompt.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { PromptingPage } from '/components/templates/PromptingPage'
 import { usePinPrompt } from '/app/view/Secure/hooks/usePinPrompt'
 import { useI18n } from '/locales/i18n'
+import { CozyTheme } from '/ui/CozyTheme/CozyTheme'
 
 type RootStackParamList = Record<string, undefined | { onSuccess: () => void }>
 
@@ -18,17 +19,19 @@ export const PinPrompt = (props: PinPromptProps): JSX.Element => {
   const { t } = useI18n()
 
   return (
-    <PromptingPage
-      title={t('screens.SecureScreen.pinprompt_title')}
-      body={t('screens.SecureScreen.pinprompt_body')}
-      button1={{
-        label: t('screens.SecureScreen.pinprompt_cta'),
-        onPress: handleSetPinCode
-      }}
-      button2={{
-        label: t('screens.SecureScreen.pinprompt_refusal'),
-        onPress: handleIgnorePinCode
-      }}
-    />
+    <CozyTheme variant="inverted">
+      <PromptingPage
+        title={t('screens.SecureScreen.pinprompt_title')}
+        body={t('screens.SecureScreen.pinprompt_body')}
+        button1={{
+          label: t('screens.SecureScreen.pinprompt_cta'),
+          onPress: handleSetPinCode
+        }}
+        button2={{
+          label: t('screens.SecureScreen.pinprompt_refusal'),
+          onPress: handleIgnorePinCode
+        }}
+      />
+    </CozyTheme>
   )
 }
