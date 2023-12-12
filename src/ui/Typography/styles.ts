fcf958ae154aed86cc4830ec6c34bfffc3ad538d
Ldoppea <yannick.chiron@gmail.com>
2023-12-12 15:27:02 +0100

    feat: Handle CozyTheme in Typography component

--- src/ui/Typography/styles.ts
+++ src/ui/Typography/styles.ts
@@ -1,60 +1,86 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, TextStyle } from 'react-native'
 
+import { CozyThemeColors } from '/ui/colors'
 import { palette } from '/ui/palette'
 
-export const styles = StyleSheet.create({
-  base: { fontFamily: 'Lato-Regular' },
-  initial: { color: palette.Primary.ContrastText },
-  inherit: { color: palette.Primary.ContrastText },
-  primary: { color: palette.Primary['600'] },
-  secondary: { color: palette.Primary.ContrastText },
-  textPrimary: { color: palette.Grey['900'] },
-  textSecondary: { color: palette.Common.white },
-  error: { color: palette.Primary.ContrastText },
-  h4: {
-    fontFamily: 'Lato-Bold',
-    fontSize: 20,
-    lineHeight: 23
-  },
-  body2: {
-    fontFamily: 'Lato-Regular',
-    fontSize: 14,
-    lineHeight: 19
-  },
-  underline: {
-    textDecorationLine: 'underline',
-    fontSize: 16,
-    lineHeight: 21
-  },
-  button: {
-    color: palette.Primary['600'],
-    fontFamily: 'Lato-Bold',
-    fontSize: 14,
-    lineHeight: 18,
-    textTransform: 'uppercase',
-    textAlign: 'center'
-  },
-  h1: { fontSize: 16 },
-  h2: { fontSize: 16 },
-  h3: {
-    fontSize: 20,
-    fontFamily: 'Lato-Bold',
-    color: palette.Grey['900']
-  },
-  h5: { fontSize: 18, fontFamily: 'Lato-Bold', lineHeight: 23 },
-  subtitle1: { fontSize: 16 },
-  subtitle2: {
-    fontSize: 12,
-    lineHeight: 16,
-    fontFamily: 'Lato-Bold',
-    textTransform: 'uppercase'
-  },
-  body1: { fontSize: 16 },
-  caption: { fontSize: 12, lineHeight: 16, fontFamily: 'Lato-Regular' },
-  link: {
-    fontFamily: 'Lato-Regular',
-    fontSize: 14,
-    lineHeight: 19,
-    textDecorationLine: 'underline'
-  }
-})
+export interface TypographyStyles {
+  base: TextStyle
+  initial: TextStyle
+  inherit: TextStyle
+  primary: TextStyle
+  secondary: TextStyle
+  textPrimary: TextStyle
+  textSecondary: TextStyle
+  error: TextStyle
+  h4: TextStyle
+  body2: TextStyle
+  underline: TextStyle
+  button: TextStyle
+  h1: TextStyle
+  h2: TextStyle
+  h3: TextStyle
+  h5: TextStyle
+  subtitle1: TextStyle
+  subtitle2: TextStyle
+  body1: TextStyle
+  caption: TextStyle
+  link: TextStyle
+}
+
+export const styles = (colors: CozyThemeColors): TypographyStyles =>
+  StyleSheet.create({
+    base: { fontFamily: 'Lato-Regular' },
+    initial: { color: colors.primaryTextColor }, // should be removed?
+    inherit: { color: colors.primaryTextColor }, // should be removed?
+    primary: { color: colors.primaryColor },
+    secondary: { color: colors.secondaryTextColor },
+    textPrimary: { color: colors.primaryTextColor },
+    textSecondary: { color: colors.secondaryTextColor },
+    error: { color: colors.errorColor },
+    h4: {
+      fontFamily: 'Lato-Bold',
+      fontSize: 20,
+      lineHeight: 23
+    },
+    body2: {
+      fontFamily: 'Lato-Regular',
+      fontSize: 14,
+      lineHeight: 19
+    },
+    underline: {
+      textDecorationLine: 'underline',
+      fontSize: 16,
+      lineHeight: 21
+    },
+    button: {
+      color: palette.Primary['600'],
+      fontFamily: 'Lato-Bold',
+      fontSize: 14,
+      lineHeight: 18,
+      textTransform: 'uppercase',
+      textAlign: 'center'
+    },
+    h1: { fontSize: 16 },
+    h2: { fontSize: 16 },
+    h3: {
+      fontSize: 20,
+      fontFamily: 'Lato-Bold',
+      color: palette.Grey['900']
+    },
+    h5: { fontSize: 18, fontFamily: 'Lato-Bold', lineHeight: 23 },
+    subtitle1: { fontSize: 16 },
+    subtitle2: {
+      fontSize: 12,
+      lineHeight: 16,
+      fontFamily: 'Lato-Bold',
+      textTransform: 'uppercase'
+    },
+    body1: { fontSize: 16 },
+    caption: { fontSize: 12, lineHeight: 16, fontFamily: 'Lato-Regular' },
+    link: {
+      fontFamily: 'Lato-Regular',
+      fontSize: 14,
+      lineHeight: 19,
+      textDecorationLine: 'underline'
+    }
+  })
