0e9e08ab08d0a4259f4fa3779af9f0d8fa6e6bb1
Antonin Cezard <acezard@gmail.com>
2023-12-13 14:52:07 +0100

    feat: implement non-native modal

--- src/ui/CozyDialogs/ConfirmDialog.tsx
+++ src/ui/CozyDialogs/ConfirmDialog.tsx
@@ -8,15 +8,44 @@ import { Typography } from '/ui/Typography'
 import { styles } from '/ui/CozyDialogs/styles'
 
 interface ConfirmDialogProps {
+  /**
+   * Actions to be displayed in the footer of the dialog. This can be a single React element
+   * or an array of elements, typically buttons that the user can interact with.
+   */
   actions:
     | ReactElement<{
         children: ReactElement<{ style: StyleProp<ViewStyle> }>[]
       }>
     | ReactElement<{ style: StyleProp<ViewStyle> }>
+
+  /**
+   * Content of the dialog. This can be a simple string or a React element for more complex content.
+   */
   content: string | ReactElement
+
+  /**
+   * A function that will be called when the dialog is requested to be closed. This does not
+   * handle the closing itself, which should be controlled by the parent component's state.
+   */
   onClose: () => void
+
+  /**
+   * Optional title text to be displayed at the top of the dialog.
+   */
   title?: string
+
+  /**
+   * Optional style or styles to apply to the header component of the dialog. Can be used to
+   * customize the appearance of the dialog's header.
+   */
   headerStyle?: StyleProp<ViewStyle>
+
+  /**
+   * Optional boolean that determines whether the native Modal component should be used.
+   * If true, the native Modal will be used (default behaviour); otherwise if false, a custom non-native overlay will be used.
+   * This allows for greater flexibility in cases where the native Modal may conflict with other libraries.
+   */
+  native?: boolean
 }
 
 const renderActions = (
@@ -41,34 +70,72 @@ const renderActions = (
   return actions
 }
 
+const ModalContent = ({
+  onClose,
+  title,
+  headerStyle,
+  content,
+  actions
+}: Partial<ConfirmDialogProps>): JSX.Element => (
+  <>
+    <Pressable style={styles.dialog}>
+      <View style={[styles.header, headerStyle]}>
+        <Typography variant="h3">{title}</Typography>
+
+        <IconButton onPress={onClose}>
+          <Icon icon={Cross} />
+        </IconButton>
+      </View>
+
+      <View style={styles.content}>
+        {typeof content === 'string' ? (
+          <Typography>{content}</Typography>
+        ) : (
+          content
+        )}
+      </View>
+
+      <View style={styles.footer}>{actions && renderActions(actions)}</View>
+    </Pressable>
+  </>
+)
+
 export const ConfirmDialog = ({
   actions,
   content,
   onClose,
   title,
-  headerStyle
-}: ConfirmDialogProps): JSX.Element => (
-  <Modal onRequestClose={onClose} statusBarTranslucent transparent>
-    <Pressable style={styles.dialogContainer} onPress={onClose}>
-      <View style={styles.dialog}>
-        <View style={[styles.header, headerStyle]}>
-          <Typography variant="h3">{title}</Typography>
-
-          <IconButton onPress={onClose}>
-            <Icon icon={Cross} />
-          </IconButton>
+  headerStyle,
+  native = true
+}: ConfirmDialogProps): JSX.Element => {
+  // The common modal content component
+  const modalContent = (
+    <View style={styles.overlay}>
+      <Pressable style={styles.overlayBackground} onPress={onClose}>
+        <View style={styles.dialogContainer}>
+          <ModalContent
+            onClose={onClose}
+            title={title}
+            headerStyle={headerStyle}
+            content={content}
+            actions={actions}
+          />
         </View>
+      </Pressable>
+    </View>
+  )
 
-        <View style={styles.content}>
-          {typeof content === 'string' ? (
-            <Typography>{content}</Typography>
-          ) : (
-            content
-          )}
-        </View>
+  // Render the native Modal if the `native` prop is true, default behaviour
+  if (native) {
+    return (
+      <Modal onRequestClose={onClose} statusBarTranslucent transparent>
+        {modalContent}
+      </Modal>
+    )
+  }
 
-        <View style={styles.footer}>{renderActions(actions)}</View>
-      </View>
-    </Pressable>
-  </Modal>
-)
+  // Otherwise, render directly the modal content
+  // This should be avoided if possible, because it can't guarantee that the modal will be displayed on top of other components
+  // (use it if the native modal can't be displayed or can't be seen at all)
+  return modalContent
+}
