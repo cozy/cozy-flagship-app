18d4b82a283b0bcd14f6f1c405d5cd2cd096a489
Ldoppea <yannick.chiron@gmail.com>
2023-12-18 19:40:19 +0100

    fix: Remove SplashScreen side effect from old FlagshipUI implementation
    This hack was created to set the correct FlagshipUI when leaving the
    OsReceive screen
    
    With the new API the HomeView's FlagshipUI is automatically restored
    when OsReceiveScreen is unmounted
    
    Related commit: b5b6dd15654f45ebbff3bf76b0df8fa331be3c4c

--- src/app/theme/SplashScreenService.ts
+++ src/app/theme/SplashScreenService.ts
@@ -1,8 +1,6 @@
 import RNBootSplash, { VisibilityStatus } from 'react-native-bootsplash'
 
 import { flagshipUIEventHandler, flagshipUIEvents } from '/app/view/FlagshipUI'
-import { resetUIState } from '/libs/intents/setFlagshipUI'
-import { navigationRef } from '/libs/RootNavigation'
 import { devlog } from '/core/tools/env'
 
 export class SplashScreenService {
@@ -25,23 +23,7 @@ export const showSplashScreen = (bootsplashName?: string): Promise<void> => {
   return RNBootSplash.show({ fade: true, bootsplashName })
 }
 
-export const hideSplashScreen = (
-  bootsplashName?: string,
-  override?: boolean
-): Promise<void> => {
-  // On the default page (home), we want to reset the UI state at first render
-  // so we get correct OS theme and status bar color
-  const isDefaultRoute =
-    navigationRef.current?.getCurrentRoute()?.name === 'default'
-
-  if (isDefaultRoute) {
-    devlog('☁️ hideSplashScreen called with default route, resetting UI')
-    // We don't care about the URI in this case, we don't want to check if a konnector is open
-    if (!override) resetUIState('')
-  } else {
-    devlog('☁️ hideSplashScreen called with non-default route')
-  }
-
+export const hideSplashScreen = (bootsplashName?: string): Promise<void> => {
   flagshipUIEventHandler.emit(
     flagshipUIEvents.SET_COMPONENT_COLORS,
     `Splashscreen`,
