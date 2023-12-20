60aeef5d4f8613e537cc4c16b134a819c9be5fd4
Théo Poizat <hello@zatteo.com>
2023-12-20 16:04:40 +0100

    feat: Disable geolocation tracking at startup when max days to capture reached

---
+++ src/app/domain/geolocation/helpers/quota.ts
@@ -0,0 +1,58 @@
+import { differenceInDays } from 'date-fns'
+
+import { Q } from 'cozy-client'
+import CozyClient from 'cozy-client'
+import flag from 'cozy-flags'
+
+const MAX_DAYS_TO_CAPTURE_UNLIMITED = -1
+const ONE_DAY = 24 * 60 * 60 * 1000
+interface FirstTimeserie {
+  id: string
+  _id: string
+  type: string
+  startDate: string
+  }[]
+}
+
+export const isGeolocationQuotaExceeded = async (
+  client: CozyClient
+): Promise<boolean> => {
+  const maxDaysToCapture = flag('coachco2.max-days-to-capture') as number | null
+
+  if (
+    typeof maxDaysToCapture !== 'number' ||
+    maxDaysToCapture === MAX_DAYS_TO_CAPTURE_UNLIMITED
+  ) {
+    return false
+  }
+
+  let firstTimeserie
+
+  try {
+    const { data } = (await client.fetchQueryAndGetFromState({
+      definition: Q('io.cozy.timeseries.geojson')
+        .where({ _id: { $gt: null } })
+        .select(['_id', 'startDate'])
+        .indexFields(['startDate'])
+        .sortBy([{ startDate: 'asc' }])
+        .limitBy(1),
+      options: {
+        as: 'io.cozy.timeseries.geojson/firstTimeserie',
+        fetchPolicy: CozyClient.fetchPolicies.olderThan(ONE_DAY)
+      }
+    })) as unknown as { data: FirstTimeserie[] }
+
+    if (data.length === 0) return false
+
+    firstTimeserie = data[0]
+  } catch (error) {
+    return false
+  }
+
+  const daysSinceFirstCapture = differenceInDays(
+    new Date(),
+    new Date(firstTimeserie.startDate)
+  )
+
+  return daysSinceFirstCapture > maxDaysToCapture
+}
