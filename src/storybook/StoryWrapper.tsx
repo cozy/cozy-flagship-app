dc90dbf240c22924062a912fe6bf78bef21f5a6c
Antonin Cezard <acezard@gmail.com>
2023-12-26 15:44:18 +0100

    feat: Add Story for LoadingOverlay
    Also update storybooy config a bit for better UI

--- src/storybook/StoryWrapper.tsx
+++ src/storybook/StoryWrapper.tsx
@@ -4,6 +4,7 @@ import RNBootSplash from 'react-native-bootsplash'
 import { Button } from '/ui/Button'
 import { Container } from '/ui/Container'
 import { CozyTheme, CozyThemeVariant } from '/ui/CozyTheme/CozyTheme'
+import { Grid } from '/ui/Grid'
 
 export const StoryWrapper = (Story: () => JSX.Element): JSX.Element => {
   const [variant, setVariant] = React.useState<CozyThemeVariant>('normal')
@@ -17,14 +18,24 @@ export const StoryWrapper = (Story: () => JSX.Element): JSX.Element => {
   return (
     <CozyTheme variant={variant}>
       <Container>
-        <Button
-          variant="primary"
-          onPress={setVariantOpposite}
-          label="Switch theme"
-          style={{ marginBottom: 16 }}
-        />
+        <Grid container direction="column" justifyContent="space-between">
+          <Grid>
+            <Button
+              variant="primary"
+              onPress={setVariantOpposite}
+              label="Switch theme"
+              style={{ marginBottom: 16 }}
+            />
+          </Grid>
 
-        <Story />
+          <Grid
+            alignItems="center"
+            justifyContent="center"
+            style={{ flexGrow: 1 }}
+          >
+            <Story />
+          </Grid>
+        </Grid>
       </Container>
     </CozyTheme>
   )
---
+++ src/ui/LoadingOverlay/LoadingOverlay.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { StoryWrapper } from '/storybook/StoryWrapper'
+import { LoadingOverlay } from '/ui/LoadingOverlay'
+
+const LoadingOverlayMeta = {
+  title: 'LoadingOverlay',
+  component: LoadingOverlay,
+  decorators: [StoryWrapper]
+}
+
+export default LoadingOverlayMeta
+
+export const Default = (): JSX.Element => <LoadingOverlay />
