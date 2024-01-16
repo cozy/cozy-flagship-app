4cc4b5e291cadb5b7e3b7fbc58ff162016dcda4b
Ldoppea <yannick.chiron@gmail.com>
2024-01-16 17:25:52 +0100

    feat: Handle IAP Subscriptions
    In the ClouderyOffer view, when the user click on a plan, we want to
    trigger the corresponding in-app purchase
    
    The IAP request is done by injecting the corresponding productId and
    the cozy's UUID that will be used by the Cloudery in order to link the
    IAP receipt with the corresponding Cozy instance
    
    During the IAP process, the ClouderyOffer view is hidden behind an
    overlay. For now this overlay is filled with primaryColor, but it
    should be replaced by a loading spinner later

--- src/app/domain/iap/services/clouderyOffer.ts
+++ src/app/domain/iap/services/clouderyOffer.ts
@@ -1,6 +1,13 @@
 import { EventEmitter } from 'events'
 
-import { Platform } from 'react-native'
+import { Linking, Platform } from 'react-native'
+import {
+  clearTransactionIOS,
+  requestSubscription,
+  Subscription,
+  SubscriptionAndroid,
+  SubscriptionOffer
+} from 'react-native-iap'
 import type { WebViewNavigation } from 'react-native-webview/lib/WebViewTypes'
 
 import Minilog from 'cozy-minilog'
@@ -36,18 +43,34 @@ export const isClouderyOfferUrl = (url: string): boolean => {
   return url.includes('premium')
 }
 
-export const isStartIapUrl = (url: string): boolean => {
-  return url.includes(START_IAP_URL)
-}
-
 export const interceptNavigation =
-  () =>
+  (
+    instanceInfo: InstanceInfo,
+    subscriptions: Subscription[],
+    setIsBuying: (isBuying: boolean) => void,
+    subscribed: () => void
+  ) =>
   (request: WebViewNavigation): boolean => {
+    log.debug('Navigating to', request.url)
     try {
       if (isStartIapUrl(request.url)) {
         const url = new URL(request.url)
         const productId = url.searchParams.get('productId')
-        log.debug('Should start IAP with productId', productId)
+
+        if (!productId) {
+          log.error('The IAP url do not contain any product ID')
+          return false
+        }
+
+        log.debug('Should start IAP on navigation with productId', productId)
+        void buySubscription(
+          productId,
+          instanceInfo,
+          subscriptions,
+          setIsBuying,
+          subscribed
+        )
+        return false
       }
 
       return true
@@ -60,3 +83,108 @@ export const interceptNavigation =
       return false
     }
   }
+
+const isStartIapUrl = (url: string): boolean => {
+  return url.startsWith(START_IAP_URL)
+}
+
+const buySubscription = async (
+  itemId: string,
+  instanceInfo: InstanceInfo,
+  subscriptions: Subscription[],
+  setIsBuying: (isBuying: boolean) => void,
+  subscribed: () => void
+): Promise<void> => {
+  log.debug('Buy subscription', itemId)
+
+  try {
+    setIsBuying(true)
+    if (Platform.OS === 'ios') {
+      const productId = itemId
+
+      log.debug('Clear iOS transactions')
+      await clearTransactionIOS()
+
+      log.debug('Request iOS subscription')
+      await requestSubscription({
+        sku: productId,
+        appAccountToken: instanceInfo.instance.data.uuid
+      })
+    } else {
+      const { productId, offers } = getSubscriptionOffers(itemId, subscriptions)
+
+      log.debug('Request Android subscription')
+      await requestSubscription({
+        sku: productId,
+        appAccountToken: instanceInfo.instance.data.uuid,
+        obfuscatedAccountIdAndroid: instanceInfo.instance.data.uuid,
+        subscriptionOffers: offers
+      })
+    }
+
+    subscribed()
+  } catch (error: unknown) {
+    if (isUserCanceledError(error)) {
+      log.debug('User canceled purchase')
+      setIsBuying(false)
+      return
+    }
+
+    const errorMessage = getErrorMessage(error)
+    log.error(
+      `Error while analysing WebView navigation. Intercept it anyway to prevent unexpected behavior: ${errorMessage}`
+    )
+    setIsBuying(false)
+  }
+}
+
+interface Offers {
+  productId: string
+  offers: SubscriptionOffer[]
+}
+
+const getSubscriptionOffers = (
+  basePlanId: string,
+  subscriptions: Subscription[]
+): Offers => {
+  log.debug('Get Subscription offers')
+
+  if (Platform.OS !== 'android') {
+    throw new Error('getSubscriptionOffers should not be called on iOS')
+  }
+
+  const subscriptionsAndroid = subscriptions as SubscriptionAndroid[]
+
+  const offer = subscriptionsAndroid
+    .flatMap(subscription => {
+      return subscription.subscriptionOfferDetails.map(
+        subsriptionOfferDetails => {
+          return {
+            sku: subscription.productId,
+            offerToken: subsriptionOfferDetails.offerToken,
+            basePlanId: subsriptionOfferDetails.basePlanId
+          }
+        }
+      )
+    })
+    .find(offer => offer.basePlanId === basePlanId)
+
+  if (!offer) {
+    throw new Error(`No base plan found for ${basePlanId}`)
+  }
+
+  log.debug('Found subscription offer', offer)
+  return {
+    productId: offer.sku,
+    offers: [offer]
+  }
+}
+
+const isUserCanceledError = (error: unknown): boolean => {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'code' in error &&
+    error.code === 'E_USER_CANCELLED'
+  )
+}
--- src/app/view/IAP/hooks/useClouderyOffer.ts
+++ src/app/view/IAP/hooks/useClouderyOffer.ts
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react'
+import { Platform } from 'react-native'
+import { initConnection, useIAP } from 'react-native-iap'
 import type { WebViewNavigation } from 'react-native-webview/lib/WebViewTypes'
 
 import { useClient } from 'cozy-client'
@@ -12,15 +14,31 @@ import {
 
 const log = Minilog('💳 Cloudery Offer')
 
+const IOS_OFFERS = [
+  'price_2024_standard_monthly_01',
+  'price_2024_premium_monthly_01'
+]
+const ANDROID_OFFERS = ['2024_standard_01', '2024_premium_01']
+const SKUS = Platform.OS === 'ios' ? IOS_OFFERS : ANDROID_OFFERS
+
 interface ClouderyOfferState {
   popupUrl: string | null
   interceptNavigation: (request: WebViewNavigation) => boolean
+  isBuying: boolean
+  setIsBuying: (isBuying: boolean) => void
 }
 
 export const useClouderyOffer = (): ClouderyOfferState => {
   const client = useClient()
+  const { subscriptions, getSubscriptions } = useIAP()
 
   const [popupUrl, setPopupUrl] = useState<string | null>(null)
+  const [isBuying, setIsBuying] = useState<boolean>(false)
+
+  const subscribed = (): void => {
+    setPopupUrl(null)
+    setIsBuying(false)
+  }
 
   useEffect(() => {
     const eventCallback = (href: string): void => {
@@ -29,7 +47,13 @@ export const useClouderyOffer = (): ClouderyOfferState => {
         return
       }
 
-      setPopupUrl(current => current ?? href)
+      const doAsync = async (): Promise<void> => {
+        await initConnection()
+        await getSubscriptions({ skus: SKUS })
+        setPopupUrl(current => current ?? href)
+      }
+
+      void doAsync()
     }
 
     const subscription = clouderyOfferEventHandler.addListener(
@@ -40,10 +64,24 @@ export const useClouderyOffer = (): ClouderyOfferState => {
     return (): void => {
       subscription.removeListener(CLOUDERY_OFFER, eventCallback)
     }
-  }, [client])
+  }, [client, getSubscriptions])
+
+  useEffect(
+    function logSubscriptions() {
+      log.debug(`Subscriptions from store = ${JSON.stringify(subscriptions)}`)
+    },
+    [subscriptions]
+  )
 
   return {
     popupUrl,
-    interceptNavigation: interceptNavigation()
+    interceptNavigation: interceptNavigation(
+      instancesInfo,
+      subscriptions,
+      setIsBuying,
+      subscribed
+    ),
+    isBuying,
+    setIsBuying
   }
 }
