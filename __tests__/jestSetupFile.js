f8ed55d3ccb94bf2d810fcf7e600c66fc2787b56
Ldoppea <yannick.chiron@gmail.com>
2024-01-25 11:17:46 +0100

    feat: remove safetynet implementation

--- __tests__/jestSetupFile.js
+++ __tests__/jestSetupFile.js
@@ -23,14 +23,6 @@ jest.mock('@sentry/react-native', () => ({
   setTag: jest.fn()
 }))
 
-jest.mock(
-  '../src/constants/api-keys',
-  () => ({
-    androidSafetyNetApiKey: 'foo'
-  }),
-  { virtual: true }
-)
-
 jest.mock('react-native-fs', () => mockRNFS)
 
 jest.mock('redux-persist', () => {
--- src/constants/api-keys.ts
+++
@@ -1,4 +0,0 @@
-import Config from 'react-native-config'
-
-export const androidSafetyNetApiKey =
-  Config.ANDROID_SAFETY_NET_API_KEY ?? 'testEnvKey'
