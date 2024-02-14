217c3049605636c5a8f9ef7f31224c7d1515b025
Théo Poizat <hello@zatteo.com>
2024-02-14 17:26:27 +0100

    refactor(SettingsService): Use storage wrapper instead of direct AsyncStorage calls

--- src/app/domain/settings/services/SettingsService.spec.ts
+++ src/app/domain/settings/services/SettingsService.spec.ts
@@ -1,72 +1,73 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-
+import {
+  StorageKeys,
+  getData,
+  storeData,
+  clearData
+} from '/libs/localStore/storage'
 import { ensureAutoLockIsEnabled } from '/app/domain/settings/services/SettingsService'
-import { StorageKeys } from '/libs/localStore/storage'
 
 jest.mock('@react-native-cookies/cookies', () => ({
   clearAll: jest.fn()
 }))
 
 describe('ensureAutoLockIsEnabled', () => {
-  beforeEach(async () => {
-    await AsyncStorage.clear()
-  })
+  beforeEach(async () => await clearData())
 
   it('should not tamper with autoLock value when already enabled', async () => {
-    const expected = 'true'
+    const expected = true
 
-    await AsyncStorage.setItem(StorageKeys.AutoLockEnabled, expected)
+    await storeData(StorageKeys.AutoLockEnabled, expected)
 
     await ensureAutoLockIsEnabled()
 
-    const result = await AsyncStorage.getItem(StorageKeys.AutoLockEnabled)
+    const result = await getData(StorageKeys.AutoLockEnabled)
 
     expect(result).toBe(expected)
   })
 
   it('should set autoLock value to true when the key does not exist', async () => {
-    const expected = 'true'
+    const expected = true
 
     await ensureAutoLockIsEnabled()
 
-    const result = await AsyncStorage.getItem(StorageKeys.AutoLockEnabled)
+    const result = await getData(StorageKeys.AutoLockEnabled)
 
     expect(result).toBe(expected)
   })
 
   it('should set autoLock value to true when the key exists with a false string', async () => {
-    const expected = 'true'
+    const expected = true
 
-    await AsyncStorage.setItem(StorageKeys.AutoLockEnabled, 'false')
+    await storeData(StorageKeys.AutoLockEnabled, false)
 
     await ensureAutoLockIsEnabled()
 
-    const result = await AsyncStorage.getItem(StorageKeys.AutoLockEnabled)
+    const result = await getData(StorageKeys.AutoLockEnabled)
 
     expect(result).toBe(expected)
   })
 
   it('should set autoLock value to true when the key exists with a falsy value', async () => {
-    const expected = 'true'
+    const expected = true
 
-    await AsyncStorage.setItem(StorageKeys.AutoLockEnabled, '')
+    await storeData(StorageKeys.AutoLockEnabled, '')
 
     await ensureAutoLockIsEnabled()
 
-    const result = await AsyncStorage.getItem(StorageKeys.AutoLockEnabled)
+    const result = await getData(StorageKeys.AutoLockEnabled)
 
     expect(result).toBe(expected)
   })
 
   /* Improbable case, but still possible */
   it('should not tamper with autoLock value when the key exists with a truthy value', async () => {
-    const expected = '"foobar"'
+    const expected = 'foobar'
 
-    await AsyncStorage.setItem(StorageKeys.AutoLockEnabled, expected)
+    await storeData(StorageKeys.AutoLockEnabled, expected)
 
     await ensureAutoLockIsEnabled()
 
-    const result = await AsyncStorage.getItem(StorageKeys.AutoLockEnabled)
+    const result = await getData(StorageKeys.AutoLockEnabled)
 
     expect(result).toBe(expected)
   })
