7642f0f8013f1ca62786d9d0e9a93f15d0907a61
Ldoppea <yannick.chiron@gmail.com>
2024-03-06 18:01:12 +0100

    fix: Update iOS IAP subscription IDs
    Due to a problem in the review process, we had to recreate our
    subscription offers

--- src/app/domain/iap/services/availableOffers.ts
+++ src/app/domain/iap/services/availableOffers.ts
@@ -8,8 +8,8 @@ import { getErrorMessage } from '/libs/functions/getErrorMessage'
 const log = Minilog('💳 Available IAP Offers')
 
 const IOS_OFFERS = [
-  'price_2024_standard_monthly_01',
-  'price_2024_premium_monthly_01'
+  'price_2024_standard_monthly_03',
+  'price_2024_premium_monthly_03'
 ]
 const ANDROID_OFFERS = ['2024_standard_01', '2024_premium_01']
 export const SKUS = Platform.OS === 'ios' ? IOS_OFFERS : ANDROID_OFFERS
