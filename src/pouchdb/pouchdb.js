68750144e46c743e321b2b62d9339e629bbe75e8
Ldoppea <yannick.chiron@gmail.com>
2024-05-31 16:44:24 +0200

    feat: Add PouchDBFind plugin to the PouchDB configuration
    This plugin is required by cozy-client to process `find` queries
    
    Related PR: cozy/cozy-client#1507

--- src/pouchdb/pouchdb.js
+++ src/pouchdb/pouchdb.js
@@ -4,6 +4,7 @@ import 'react-native-get-random-values'
 import HttpPouch from 'pouchdb-adapter-http'
 import SQLiteAdapterFactory from 'pouchdb-adapter-react-native-sqlite'
 import PouchDB from 'pouchdb-core'
+import PouchDBFind from 'pouchdb-find'
 import mapreduce from 'pouchdb-mapreduce'
 import replication from 'pouchdb-replication'
 import WebSQLite from 'react-native-quick-websql'
@@ -11,6 +12,7 @@ import WebSQLite from 'react-native-quick-websql'
 const SQLiteAdapter = SQLiteAdapterFactory(WebSQLite)
 
 export default PouchDB.plugin(HttpPouch)
+  .plugin(PouchDBFind)
   .plugin(replication)
   .plugin(mapreduce)
   .plugin(SQLiteAdapter)
