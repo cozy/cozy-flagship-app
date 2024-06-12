8e1bdc44e82d459b198bdf3a3e6a162c4c45cd42
Théo Poizat <hello@zatteo.com>
2024-06-12 10:49:58 +0200

    feat: Add support for dark theme in colors
    Added colors from cozy-ui.

--- src/ui/colors.ts
+++ src/ui/colors.ts
@@ -1,3 +1,5 @@
+import { Appearance } from 'react-native'
+
 import { CozyThemeVariant } from '/ui/CozyTheme/CozyTheme'
 import { palette } from '/ui/palette'
 
@@ -9,7 +11,9 @@ const ALPHA_0_48 = '7A'
 const ALPHA_0_64 = 'A3'
 const ALPHA_0_90 = 'E5'
 
-const colors = {
+// Light colors
+
+const lightNormalColors = {
   // ACTIONS
   actionColorDisabled: palette.Grey['900'] + ALPHA_0_24,
   actionColorDisabledBackground: palette.Grey['900'] + ALPHA_0_12,
@@ -36,7 +40,7 @@ const colors = {
   onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
 }
 
-const colorsInverted = {
+const lightInvertedColors = {
   // ACTIONS
   actionColorDisabled: palette.Common.white + ALPHA_0_32,
   actionColorDisabledBackground: palette.Common.white + ALPHA_0_12,
@@ -63,10 +67,72 @@ const colorsInverted = {
   onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
 }
 
-export type CozyThemeColors = typeof colors
+// Dark colors
+
+const darkNormalColors = {
+  // ACTIONS
+  actionColorDisabled: palette.Common.white + ALPHA_0_32,
+  actionColorDisabledBackground: palette.Common.white + ALPHA_0_12,
+
+  // BORDERS
+  borderMainColor: palette.Common.white + ALPHA_0_24,
+
+  // BACKGROUND
+  paperBackgroundColor: palette.Grey['800'],
+
+  // PRIMARY
+  primaryColor: palette.Primary['400'],
+  primaryContrastTextColor: palette.Grey['900'] + ALPHA_0_90,
+
+  // ERROR
+  errorColor: palette.Error['300'],
+
+  // TEXT
+  primaryTextColor: palette.Common.white,
+  secondaryTextColor: palette.Common.white + ALPHA_0_64,
+
+  // FLAGSHIP APP SPECIFIC
+  splashScreenBackgroundColor: palette.FlagshipSpecific.splashScreenBackground,
+  onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
+}
+
+const darkInvertedColors = {
+  // ACTIONS
+  actionColorDisabled: palette.Grey['900'] + ALPHA_0_24,
+  actionColorDisabledBackground: palette.Grey['900'] + ALPHA_0_12,
+
+  // BORDERS
+  borderMainColor: palette.Grey['900'] + ALPHA_0_16,
+
+  // BACKGROUND
+  paperBackgroundColor: palette.Primary['400'],
+
+  // PRIMARY
+  primaryColor: palette.Grey['800'],
+  primaryContrastTextColor: palette.Common.white,
+
+  // ERROR
+  errorColor: palette.Error['800'],
+
+  // TEXT
+  primaryTextColor: palette.Grey['900'] + ALPHA_0_90,
+  secondaryTextColor: palette.Grey['900'] + ALPHA_0_48,
+
+  // FLAGSHIP APP SPECIFIC
+  splashScreenBackgroundColor: palette.FlagshipSpecific.splashScreenBackground,
+  onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
+}
+
+export type CozyThemeColors = typeof lightNormalColors
 
 export const getColors = (
   variant: CozyThemeVariant = 'normal'
 ): CozyThemeColors => {
-  return variant === 'normal' ? colors : colorsInverted
+  const colorScheme = Appearance.getColorScheme()
+
+  if (colorScheme === 'light') {
+    return variant === 'normal' ? lightNormalColors : lightInvertedColors
+  } else {
+    return variant === 'normal' ? darkNormalColors : darkInvertedColors
+  }
 }
