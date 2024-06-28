18c3bfd6c2a34d68d5fb4f1467364aea69de84db
Théo Poizat <hello@zatteo.com>
2024-06-28 09:07:38 +0200

    feat: Fetch and set color scheme at app startup
    I added a LoggedInWrapper which is added in the tree only if there is a client. It :
    - allows to use useSettings which need a CozyProvider
    - make useColorScheme more concise because we do not need to check if client is defined
    
    Added a wrapper method to set color scheme

--- src/app/theme/colorScheme.ts
+++ src/app/theme/colorScheme.ts
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
 import { Appearance, ColorSchemeName } from 'react-native'
 
+import { useSettings } from 'cozy-client'
 import flag from 'cozy-flags'
 
 interface GetColorSchemeOptions {
   useUserColorScheme?: boolean
+}
+
+export const setColorScheme = (
+  colorScheme: ColorSchemeName | string | undefined
+): void => {
+  const formattedColorScheme = formatColorSchemeName(colorScheme)
+  Appearance.setColorScheme(formattedColorScheme)
 }
 
 export const getColorScheme = ({
@@ -19,4 +28,29 @@ export const getColorScheme = ({
 
   // Force light if flag disabled
   return 'light'
+}
+
+const formatColorSchemeName = (
+  colorScheme: ColorSchemeName | string | undefined
+): ColorSchemeName => {
+  switch (colorScheme) {
+    case 'light':
+      return 'light'
+    case 'dark':
+      return 'dark'
+    default:
+      return null
+  }
+}
+
+export const useColorScheme = (): void => {
+  const {
+    values: { colorScheme: instanceColorScheme }
+  } = useSettings('instance', ['colorScheme']) as {
+    values: Record<'colorScheme', string | undefined>
+  }
+
+  useEffect(() => {
+    setColorScheme(instanceColorScheme)
+  }, [instanceColorScheme])
 }
