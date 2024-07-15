ffabcb7b3d06bf7c0f771184e8514d6cc4c2de41
Théo Poizat <hello@zatteo.com>
2024-07-15 15:05:54 +0200

    fix: Add new colors.ts to white_label management
    We add issues with transitioning screens which had a bad color with GL.
    It is because :
    - with cozy, paperBackgroundColor uses palette.Grey[800] : OK
    - with GL
      - palette.Grey[800] changes : OK
      - paperBackgroundColor changes : NOT OK, it is not associated to palette.Grey[800] anymore but to an hardcoded color in GL override
    
    So here I insert directly in colors.ts the GL overrides as we do on cozy-ui.

---
+++ white_label/brands/cozy/js/ui/colors.ts
@@ -0,0 +1,147 @@
+import { getColorScheme } from '/app/theme/colorScheme'
+import { CozyThemeVariant } from '/ui/CozyTheme/CozyTheme'
+import { palette } from '/ui/palette'
+
+const ALPHA_0_12 = '1F'
+const ALPHA_0_16 = '29'
+const ALPHA_0_24 = '3D'
+const ALPHA_0_32 = '52'
+const ALPHA_0_48 = '7A'
+const ALPHA_0_64 = 'A3'
+const ALPHA_0_90 = 'E5'
+
+// Light colors
+
+const lightNormalColors = {
+  // ACTIONS
+  actionColorDisabled: palette.Grey['900'] + ALPHA_0_24,
+  actionColorDisabledBackground: palette.Grey['900'] + ALPHA_0_12,
+
+  // BORDERS
+  borderMainColor: palette.Grey['900'] + ALPHA_0_16,
+
+  // BACKGROUND
+  paperBackgroundColor: palette.Common.white,
+  defaultBackgroundColor: palette.Grey['100'],
+
+  // PRIMARY
+  primaryColor: palette.Primary['600'],
+  primaryContrastTextColor: palette.Common.white,
+
+  // ERROR
+  errorColor: palette.Error['600'],
+
+  // TEXT
+  primaryTextColor: palette.Grey['900'] + ALPHA_0_90,
+  secondaryTextColor: palette.Grey['900'] + ALPHA_0_48,
+
+  // FLAGSHIP APP SPECIFIC
+  splashScreenBackgroundColor: palette.FlagshipSpecific.splashScreenBackground,
+  onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
+}
+
+const lightInvertedColors = {
+  // ACTIONS
+  actionColorDisabled: palette.Common.white + ALPHA_0_32,
+  actionColorDisabledBackground: palette.Common.white + ALPHA_0_12,
+
+  // BORDERS
+  borderMainColor: palette.Common.white + ALPHA_0_24,
+
+  // BACKGROUND
+  paperBackgroundColor: palette.Primary['600'],
+  defaultBackgroundColor: palette.Primary['600'],
+
+  // PRIMARY
+  primaryColor: palette.Common.white,
+  primaryContrastTextColor: palette.Primary['600'],
+
+  // ERROR
+  errorColor: palette.Error['200'],
+
+  // TEXT
+  primaryTextColor: palette.Common.white,
+  secondaryTextColor: palette.Common.white + ALPHA_0_64,
+
+  // FLAGSHIP APP SPECIFIC
+  splashScreenBackgroundColor: palette.FlagshipSpecific.splashScreenBackground,
+  onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
+}
+
+// Dark colors
+
+const darkNormalColors = {
+  // ACTIONS
+  actionColorDisabled: palette.Common.white + ALPHA_0_32,
+  actionColorDisabledBackground: palette.Common.white + ALPHA_0_12,
+
+  // BORDERS
+  borderMainColor: palette.Common.white + ALPHA_0_24,
+
+  // BACKGROUND
+  paperBackgroundColor: palette.Grey['800'],
+  defaultBackgroundColor: palette.Grey.A400,
+
+  // PRIMARY
+  primaryColor: palette.Primary['400'],
+  primaryContrastTextColor: palette.Grey['900'] + ALPHA_0_90,
+
+  // ERROR
+  errorColor: palette.Error['300'],
+
+  // TEXT
+  primaryTextColor: palette.Common.white,
+  secondaryTextColor: palette.Common.white + ALPHA_0_64,
+
+  // FLAGSHIP APP SPECIFIC
+  splashScreenBackgroundColor: palette.FlagshipSpecific.splashScreenBackground,
+  onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
+}
+
+const darkInvertedColors = {
+  // ACTIONS
+  actionColorDisabled: palette.Grey['900'] + ALPHA_0_24,
+  actionColorDisabledBackground: palette.Grey['900'] + ALPHA_0_12,
+
+  // BORDERS
+  borderMainColor: palette.Grey['900'] + ALPHA_0_16,
+
+  // BACKGROUND
+  paperBackgroundColor: palette.Primary['400'],
+  defaultBackgroundColor: palette.Primary['400'],
+
+  // PRIMARY
+  primaryColor: palette.Grey['800'],
+  primaryContrastTextColor: palette.Common.white,
+
+  // ERROR
+  errorColor: palette.Error['800'],
+
+  // TEXT
+  primaryTextColor: palette.Grey['900'] + ALPHA_0_90,
+  secondaryTextColor: palette.Grey['900'] + ALPHA_0_48,
+
+  // FLAGSHIP APP SPECIFIC
+  splashScreenBackgroundColor: palette.FlagshipSpecific.splashScreenBackground,
+  onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
+}
+
+export type CozyThemeColors = typeof lightNormalColors
+
+export interface GetColorsOptions {
+  variant?: CozyThemeVariant
+  useUserColorScheme?: boolean
+}
+
+export const getColors = ({
+  variant = 'normal',
+  useUserColorScheme = false
+}: GetColorsOptions = {}): CozyThemeColors => {
+  const colorScheme = getColorScheme({ useUserColorScheme })
+
+  if (colorScheme === 'light') {
+    return variant === 'normal' ? lightNormalColors : lightInvertedColors
+  } else {
+    return variant === 'normal' ? darkNormalColors : darkInvertedColors
+  }
+}
---
+++ white_label/brands/mabulle/js/ui/colors.ts
@@ -0,0 +1,139 @@
+import { getColorScheme } from '/app/theme/colorScheme'
+import { CozyThemeVariant } from '/ui/CozyTheme/CozyTheme'
+import { palette } from '/ui/palette'
+
+// Light colors
+
+const lightNormalColors = {
+  // ACTIONS
+  actionColorDisabled: 'rgba(29, 46, 85, 0.24)',
+  actionColorDisabledBackground: 'rgba(29, 46, 85, 0.12)',
+
+  // BORDERS
+  borderMainColor: 'rgba(29, 46, 85, 0.51)',
+
+  // BACKGROUND
+  paperBackgroundColor: '#ffffff',
+  defaultBackgroundColor: '#f4f4fa',
+
+  // PRIMARY
+  primaryColor: '#0066ff',
+  primaryContrastTextColor: '#ffffff',
+
+  // ERROR
+  errorColor: palette.Error['600'],
+
+  // TEXT
+  primaryTextColor: 'rgba(29, 46, 85, 1);',
+  secondaryTextColor: 'rgba(29, 46, 85, 0.7)',
+
+  // FLAGSHIP APP SPECIFIC
+  splashScreenBackgroundColor: palette.FlagshipSpecific.splashScreenBackground,
+  onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
+}
+
+const lightInvertedColors = {
+  // ACTIONS
+  actionColorDisabled: 'rgba(255, 255, 255, 0.32)',
+  actionColorDisabledBackground: 'rgba(255, 255, 255, 0.12)',
+
+  // BORDERS
+  borderMainColor: 'rgba(255, 255, 255, 0.51)',
+
+  // BACKGROUND
+  paperBackgroundColor: '#0066ff',
+  defaultBackgroundColor: '#0066ff',
+
+  // PRIMARY
+  primaryColor: 'rgba(255, 255, 255, 1)',
+  primaryContrastTextColor: '#0066ff',
+
+  // ERROR
+  errorColor: '#ffd3d3',
+
+  // TEXT
+  primaryTextColor: 'rgba(255, 255, 255, 1)',
+  secondaryTextColor: 'rgba(255, 255, 255, 0.70)',
+
+  // FLAGSHIP APP SPECIFIC
+  splashScreenBackgroundColor: palette.FlagshipSpecific.splashScreenBackground,
+  onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
+}
+
+// Dark colors
+
+const darkNormalColors = {
+  // ACTIONS
+  actionColorDisabled: 'rgba(255, 255, 255, 0.32)',
+  actionColorDisabledBackground: 'rgba(255, 255, 255, 0.12)',
+
+  // BORDERS
+  borderMainColor: 'rgba(255, 255, 255, 0.51)',
+
+  // BACKGROUND
+  paperBackgroundColor: '#3e424a',
+  defaultBackgroundColor: '#2c3039',
+
+  // PRIMARY
+  primaryColor: '#91d1ff',
+  primaryContrastTextColor: 'rgba(29, 46, 85, 0.9)',
+
+  // ERROR
+  errorColor: '#ff9b9b',
+
+  // TEXT
+  primaryTextColor: 'rgba(255, 255, 255, 1)',
+  secondaryTextColor: 'rgba(255, 255, 255, 0.70)',
+
+  // FLAGSHIP APP SPECIFIC
+  splashScreenBackgroundColor: palette.FlagshipSpecific.splashScreenBackground,
+  onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
+}
+
+const darkInvertedColors = {
+  // ACTIONS
+  actionColorDisabled: 'rgba(29, 46, 85, 0.24)',
+  actionColorDisabledBackground: 'rgba(29, 46, 85, 0.12)',
+
+  // BORDERS
+  borderMainColor: 'rgba(29, 46, 85, 0.51)',
+
+  // BACKGROUND
+  paperBackgroundColor: '#91D1FF',
+  defaultBackgroundColor: '#91D1FF',
+
+  // PRIMARY
+  primaryColor: '#3e424a',
+  primaryContrastTextColor: 'rgba(255, 255, 255, 1)',
+
+  // ERROR
+  errorColor: '#771212',
+
+  // TEXT
+  primaryTextColor: 'rgba(29, 46, 85, 1)',
+  secondaryTextColor: 'rgba(29, 46, 85, 0.70)',
+
+  // FLAGSHIP APP SPECIFIC
+  splashScreenBackgroundColor: palette.FlagshipSpecific.splashScreenBackground,
+  onboardingBackgroundColor: palette.FlagshipSpecific.onboardingBackground
+}
+
+export type CozyThemeColors = typeof lightNormalColors
+
+export interface GetColorsOptions {
+  variant?: CozyThemeVariant
+  useUserColorScheme?: boolean
+}
+
+export const getColors = ({
+  variant = 'normal',
+  useUserColorScheme = false
+}: GetColorsOptions = {}): CozyThemeColors => {
+  const colorScheme = getColorScheme({ useUserColorScheme })
+
+  if (colorScheme === 'light') {
+    return variant === 'normal' ? lightNormalColors : lightInvertedColors
+  } else {
+    return variant === 'normal' ? darkNormalColors : darkInvertedColors
+  }
+}
