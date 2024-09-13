08849625b0caf836f67bab958eb93afb3f58df35
Ldoppea <yannick.chiron@gmail.com>
2024-09-13 16:59:18 +0200

    feat: Open cozy-app when offline only if it supports Offline mode
    When offline, we want to open coz-apps only when they declare to be
    compatible with Offline mode
    
    Cozy-app can declare this by setting `offline_support: true` in their
    manifest.webapp file
    
    Non compatible cozy-app won't be opened when the Flagship app is
    offline, and instead an error message will be displayed

---
+++ src/app/domain/offline/isOfflineCompatible.ts
@@ -0,0 +1,48 @@
+import RNFS from 'react-native-fs'
+
+import { getErrorMessage } from 'cozy-intent'
+import Minilog from 'cozy-minilog'
+
+import { getCurrentAppConfigurationForFqdnAndSlug } from '/libs/cozyAppBundle/cozyAppBundleConfiguration'
+import { getBaseFolderForFqdnAndSlugAndCurrentVersion } from '/libs/httpserver/httpPaths'
+
+export const log = Minilog('IsOfflineCompatible')
+
+export const isOfflineCompatible = async (
+  fqdn: string,
+  slug: string
+): Promise<boolean> => {
+  try {
+    const appConfiguration = await getCurrentAppConfigurationForFqdnAndSlug(
+      fqdn,
+      slug
+    )
+
+    if (!appConfiguration) {
+      return false
+    }
+
+    const basePath = await getBaseFolderForFqdnAndSlugAndCurrentVersion(
+      fqdn,
+      slug
+    )
+
+    const manifestPath = basePath + '/manifest.webapp'
+
+    if (!(await RNFS.exists(manifestPath))) {
+      return false
+    }
+
+    const fileContent = await RNFS.readFile(manifestPath)
+
+    const manifest = JSON.parse(fileContent) as Record<string, unknown>
+
+    return manifest.offline_support === true
+  } catch (error) {
+    log.error(
+      `Could not read manifest for app "${slug}": ${getErrorMessage(error)}.`
+    )
+
+    return false
+  }
+}
