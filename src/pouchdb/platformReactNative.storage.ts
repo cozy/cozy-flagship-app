b00bb13a15a750179f14c1aec27c279aaf99c88f
Ldoppea <yannick.chiron@gmail.com>
2024-12-04 16:16:54 +0100

    feat: Migrate Offline's platform storage from AsyncStorage to MMKV

--- src/pouchdb/platformReactNative.storage.ts
+++ src/pouchdb/platformReactNative.storage.ts
@@ -1,14 +1,14 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { storage as mmkvStorage } from '/libs/localStore'
 
 export const storage = {
   getItem: async (key: string): Promise<string | null> => {
-    return AsyncStorage.getItem(key)
+    return Promise.resolve(mmkvStorage.getString(key) ?? null)
   },
   setItem: async (key: string, value: string | undefined): Promise<void> => {
     if (value === undefined) return
-    return AsyncStorage.setItem(key, value)
+    return Promise.resolve(mmkvStorage.set(key, value))
   },
   removeItem: async (key: string): Promise<void> => {
-    return AsyncStorage.removeItem(key)
+    return Promise.resolve(mmkvStorage.delete(key))
   }
 }
