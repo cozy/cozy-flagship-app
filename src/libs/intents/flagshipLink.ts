81df99df4980b481ff13e24026eb0f7b65344aaf
Ldoppea <yannick.chiron@gmail.com>
2024-12-04 16:22:43 +0100

    MEASURE PERF FLAGSHIP LINK

--- src/libs/intents/flagshipLink.ts
+++ src/libs/intents/flagshipLink.ts
@@ -1,7 +1,10 @@
+import { uniqueId } from 'lodash'
+
 import CozyClient, { QueryDefinition } from 'cozy-client'
 import type { QueryResult } from 'cozy-client/types/types'
 import Minilog from 'cozy-minilog'
 
+import rnperformance from '/app/domain/performances/measure'
 import { getErrorMessage } from '/libs/functions/getErrorMessage'
 
 const log = Minilog('⛳🔗 flagshipLink')
@@ -16,6 +19,9 @@ export const flagshipLinkRequest = async (
         'FlagshipLinkRequest should not be called with undefined client'
       )
     }
+
+    const markName = `FlagshipLinkRequest ${operation.doctype} ${uniqueId()}`
+
 
     // Temporary skip `io.cozy.jobs` queries as they are not mandatory
     // to display cozy-home but are very long to be executed (>20s) and
@@ -33,9 +39,11 @@ export const flagshipLinkRequest = async (
     }
 
     const start = new Date()
+    rnperformance.mark(markName)
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const result = await client.query(operation)
 
+    rnperformance.measure(markName, markName, 'FlagshipLinkRequest')
     const end = new Date()
     const duration = end.getTime() - start.getTime()
     log.debug(`⏰ operation took ${duration}ms -- ${JSON.stringify(operation)}`)
