5d00e7f10383256ff446ed837e10866a0822bb7e
Ldoppea <yannick.chiron@gmail.com>
2024-12-19 16:05:55 +0100

    feat: Add performance measurements on MMKV migration script
    Since we migrated to MMKV, existing users' data should be migrated from
    the old AsyncStorage to MMKV
    
    Code taken from #1097
    
    Co-authored-by: Théo Poizat <hello@zatteo.com>

--- src/libs/localStore/storage.ts
+++ src/libs/localStore/storage.ts
@@ -161,8 +161,7 @@ export const hasMigratedFromAsyncStorage = storage.getBoolean(
 // TODO: Remove `hasMigratedFromAsyncStorage` after a while (when everyone has migrated)
 export async function migrateFromAsyncStorage(): Promise<void> {
   log.info('Migrating from AsyncStorage -> MMKV...')
-  const markName = `migrateFromAsyncStorage`
-  rnperformance.mark(markName)
+  const markName = rnperformance.mark('migrateFromAsyncStorage')
 
   const keys = await AsyncStorage.getAllKeys()
 
@@ -184,6 +183,9 @@ export async function migrateFromAsyncStorage(): Promise<void> {
 
   storage.set('hasMigratedFromAsyncStorage', true)
 
-  rnperformance.measure(markName, markName, 'AsyncStorageMigration')
+  rnperformance.measure({
+    markName: markName,
+    category: 'AsyncStorageMigration'
+  })
   log.info(`Migrated from AsyncStorage -> MMKV!`)
 }
