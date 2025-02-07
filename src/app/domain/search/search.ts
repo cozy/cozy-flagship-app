50388e045ea2d0c210cec94b26b6f92f1999cf14
Ldoppea <yannick.chiron@gmail.com>
2025-02-07 11:03:10 +0100

    feat: Add storage to SearchEngine constructor
    Since cozy/cozy-libs/pull/2729 the SearchEngine requires the consuming
    app to provide a storage provider in order to persist its index
    
    Related PR: cozy/cozy-libs#2729

--- src/app/domain/search/search.ts
+++ src/app/domain/search/search.ts
@@ -1,17 +1,26 @@
 import CozyClient from 'cozy-client'
-import type { SearchOptions } from 'cozy-dataproxy-lib/dist/search/types'
+import type {
+  SearchOptions,
+  StorageInterface
+} from 'cozy-dataproxy-lib/dist/search/types'
 import Minilog from 'cozy-minilog'
 
 import { CozyClientPerformanceApi } from '/app/domain/performances/measure'
 import { SearchEngine } from '/app/domain/search/dataproxy-wrapper'
+import { getData, storeData } from '/libs/localStore/storage'
 
 const log = Minilog('📱🗂️ Search')
 
 let searchEngine: SearchEngine | undefined = undefined
 
+const storage: StorageInterface = {
+  storeData: storeData,
+  getData: getData
+}
+
 export const initSearchEngine = (client: CozyClient): void => {
   log.debug('Init SearchEngine')
-  searchEngine = new SearchEngine(client, null, CozyClientPerformanceApi)
+  searchEngine = new SearchEngine(client, storage, CozyClientPerformanceApi)
 }
 
 export const search = (query: string, options: SearchOptions): unknown => {
