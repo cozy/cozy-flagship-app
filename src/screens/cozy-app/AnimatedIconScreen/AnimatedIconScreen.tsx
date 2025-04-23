ab4fec7648f502e13c21f98fbd0c0d87920bb402
Théo Poizat <hello@zatteo.com>
2025-04-23 15:33:01 +0200

    feat: Change transition between apps
    Twake UI is moving from "icon growing to fullscreen page" animation as code to "fullscreen page with icon doing things" animation as lottie file. There is a default animation with Twake logo for new apps and settings.
    
    Because now it is a fullscreen page, there is no more need to managing the first half or the possible premature end of the transition if the webview loads fast.

---
+++ src/screens/cozy-app/AnimatedIconScreen/AnimatedIconScreen.tsx
@@ -0,0 +1,71 @@
+import LottieView, { AnimationObject } from 'lottie-react-native'
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+import TwakeWorkplaceIcon from './TwakeWorkplaceIcon'
+import contactsAnimation from './animations/contacts.json'
+import defaultAnimation from './animations/default.json'
+import driveAnimation from './animations/drive.json'
+import notesAnimation from './animations/notes.json'
+import storeAnimation from './animations/store.json'
+
+import { getColors } from '/ui/colors'
+
+const AVAILABLE_ANIMATIONS: Record<string, AnimationObject> = {
+  drive: driveAnimation,
+  contacts: contactsAnimation,
+  notes: notesAnimation,
+  store: storeAnimation,
+  default: defaultAnimation
+}
+
+export const AnimatedIconScreen = ({ slug }: { slug: string }): JSX.Element => {
+  const colors = getColors()
+
+  return (
+    <View
+      style={{
+        ...styles.container,
+        backgroundColor: colors.paperBackgroundColor
+      }}
+    >
+      <View style={styles.animationContainer}>
+        <LottieView
+          source={AVAILABLE_ANIMATIONS[slug] ?? AVAILABLE_ANIMATIONS.default}
+          speed={0.75}
+          style={styles.lottieView}
+          autoPlay
+          loop={false}
+        />
+      </View>
+      <View style={styles.svgContainer}>
+        <TwakeWorkplaceIcon />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%'
+  },
+  animationContainer: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  lottieView: {
+    width: '40%',
+    height: '40%'
+  },
+  svgContainer: {
+    position: 'absolute',
+    bottom: 20,
+    width: '100%',
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
--- src/screens/cozy-app/CozyAppScreen.functions.ts
+++ src/screens/cozy-app/CozyAppScreen.functions.ts
@@ -1,90 +1,11 @@
 import { WebViewErrorEvent } from 'react-native-webview/lib/WebViewTypes'
 
-import { styles } from './CozyAppScreen.styles'
-
 import { routes } from '/constants/routes'
-import { getDimensions } from '/libs/dimensions'
 import { NetService } from '/libs/services/NetService'
-import { getColors } from '/ui/colors'
-import { ProgressBarProps } from '/components/Bar'
 
 export const handleError = ({ nativeEvent }: WebViewErrorEvent): void => {
   const { code, description } = nativeEvent
 
   if (code === -2 && description === 'net::ERR_INTERNET_DISCONNECTED')
     NetService.handleOffline(routes.home)
-}
-
-export const config = {
-  duration: 300,
-  width: '44%',
-  height: '44%',
-  driver: true
-}
-
-// Width has to be null at start even if it's not a valid value (was set to undefined and it broke the progress bar)
-// At the time of this fix we want to go back the previously working value but we have to investigate why it has to be null
-export const progressBarConfig = (): ProgressBarProps => {
-  const colors = getColors()
-
-  return {
-    width: null as unknown as number | undefined,
-    indeterminate: true,
-    unfilledColor: colors.defaultBackgroundColor,
-    color: colors.primaryColor,
-    borderWidth: 0,
-    height: 8,
-    borderRadius: 100,
-    indeterminateAnimationDuration: 1500
-  }
-}
-
-export const progressBarAnimConfig = {
-  fadeIn: {
-    toValue: 1,
-    duration: 50,
-    useNativeDriver: config.driver
-  },
-  fadeOut: {
-    toValue: 0,
-    duration: 50,
-    useNativeDriver: config.driver
-  }
-}
-
-export const containerAnimConfig = {
-  fadeOut: {
-    toValue: 0,
-    duration: 300,
-    useNativeDriver: config.driver
-  }
-}
-
-export const getTranslateInput = (params: {
-  x: number
-  y: number
-  height: number
-  width: number
-}): { x: number; y: number } => {
-  const { screenHeight, screenWidth } = getDimensions()
-
-  return {
-    x:
-      params.x -
-      styles.fadingContainer.left -
-      (screenWidth - params.width) * 0.5,
-    y:
-      params.y -
-      styles.fadingContainer.top -
-      (screenHeight - params.height) * 0.5
-  }
-}
-
-export const getScaleInput = (params: {
-  width: number
-  height: number
-}): { x: number; y: number } => {
-  const { screenHeight, screenWidth } = getDimensions()
-
-  return { x: params.width / screenWidth, y: params.height / screenHeight }
 }
