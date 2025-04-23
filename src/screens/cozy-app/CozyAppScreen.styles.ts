318f890e4ca934f7193a8125369b50edbcecae88
Théo Poizat <hello@zatteo.com>
2025-04-23 15:33:01 +0200

    feat: Change transition between apps
    Twake UI is moving from "icon growing to fullscreen page" animation as code to "fullscreen page with icon doing things" animation as lottie file. There is a default animation with Twake logo for new apps and settings.
    
    Because now it is a fullscreen page, there is no more need to managing the first half or the possible premature end of the transition if the webview loads fast.

--- src/screens/cozy-app/CozyAppScreen.styles.ts
+++ src/screens/cozy-app/CozyAppScreen.styles.ts
@@ -15,29 +15,8 @@ export const styles = StyleSheet.create({
     width: '100%',
     height: '100%'
   },
-  fadingContainer: {
-    position: 'absolute',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    zIndex: 1, // Will show up above CozyWebView,
-    borderRadius: 8,
-    boxShadow:
-      '0px 0px 0px 0.5px rgba(29, 33, 42, 0.12), 0px 2px 4px rgba(29, 33, 42, 0.0793047), 0px 4px 16px rgba(29, 33, 42, 0.06)'
-  },
   mainView: {
     flex: 1, // Allows full height for loading animation
     opacity: 1
-  },
-  immersiveHeight: {
-    height: 0
-  },
-  progressBarContainer: {
-    width: '60%',
-    display: 'flex'
   }
 })
