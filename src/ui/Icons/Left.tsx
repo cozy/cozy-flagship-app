59d0972ad41aaf930b2094a0fb7e19cfe7fc229e
Ldoppea <yannick.chiron@gmail.com>
2025-04-24 13:08:25 +0200

    feat: Implement new Welcome screen for Twake SSO
    We want to adapt the onboarding screens in order to use the new
    Twake SSO login and subscription mechanisms
    
    To allow this we rework the Welcome screen so it uses the new Twake UI
    and so its login and signup buttons triggers the SSO process
    
    Contrary to previous implementation that used WebViews, the new SSO
    process is fully based on the InAppBrowser
    
    The side effect is that we now prefer to use Schemes instead of
    UniversalLinks. This is because UniversalLinks would not trigger
    correctly in some scenario like when creating a Twake instance. In that
    scenario, a few seconds can occure between the last user click and the
    final redirection. In some devices (all iOS but also some Android
    devices), the UniversalLink redirection would be prevented by the
    browser's rules as it wouldn't be considered as a user action. So the
    Schemes are prefered here as they would always be triggered. The only
    cons is that Schemes would requires user's approval on Android, so the
    user would have to validate a popup just before the final redirection
    
    The Welcome screen also contains a "Use your company server" that would
    allow old Cozy's not configured with Twake SSO to be still accessible
    
    However we still did not implement the other OIDC scenario (on premise
    instance, or partners). This will be implemented in the following weeks

---
+++ src/ui/Icons/Left.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { CommonPathProps, SvgXml } from 'react-native-svg'
+
+export const Left = (props?: CommonPathProps): JSX.Element => (
+  <SvgXml
+    {...props}
+    xml='&lt;svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"&gt;&lt;g id="icons/16/icon-left"&gt;&lt;path id="Shape" fill-rule="evenodd" clip-rule="evenodd" d="M10.70725,2.70725 C11.09825,2.31625 11.09825,1.68425 10.70725,1.29325 C10.31625,0.90225 9.68425,0.90225 9.29325,1.29325 L3.29325,7.29325 C2.90225,7.68425 2.90225,8.31625 3.29325,8.70725 L9.29325,14.70725 C9.68425,15.09825 10.31625,15.09825 10.70725,14.70725 C11.09825,14.31625 11.09825,13.68425 10.70725,13.29325 L5.41425,8.00025 L10.70725,2.70725 Z" fill-opacity="0.72"/&gt;&lt;/g&gt;&lt;/svg&gt;'
+  />
+)
