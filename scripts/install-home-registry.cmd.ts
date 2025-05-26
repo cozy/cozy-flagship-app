6c8140c78f2395b5902d8241f8f8755316c348aa
Ldoppea <yannick.chiron@gmail.com>
2025-05-26 20:16:56 +0200

    feat: Temporary switch to /beta channel for installing local home bundle
    Latest /stable version is from 8 month ago so we want to switch to the
    /beta channel in order to improve testflight experience

--- scripts/install-home-registry.cmd.ts
+++ scripts/install-home-registry.cmd.ts
@@ -1,7 +1,7 @@
 import { installHomeRegistry } from './install-home-registry'
 
 void installHomeRegistry(
-  'https://apps-registry.cozycloud.cc/registry/home/stable/latest',
+  'https://apps-registry.cozycloud.cc/registry/home/beta/latest',
   [
     'android/app/src/main/assets/cozy-home/build',
     'ios/assets/resources/cozy-home/build'
